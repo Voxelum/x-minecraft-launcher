88b89b83d0d0cb01f152da7c77c47c89fc9a1298
ci010 <cijhn@hotmail.com>
2017-06-28 19:11:04 +0800

    initialize
    Former-commit-id: 5debc1e0f2bb67db7097819f1fbe5838931df2e9

---
+++ src/renderer/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <login-page></login-page>
+  </div>
+</template>
+
+<script>
+import LoginPage from '@/components/LoginPage'
+
+export default {
+  name: 'llauncher',
+  components: {
+    LoginPage
+  }
+}
+</script>
+
+<style>
+/* CSS */
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+import App from './App'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  template: '<App/>'
+}).$mount('#app')
