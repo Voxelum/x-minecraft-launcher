5d8021554a9e052063895a937ffe6cb3ace44dc0
ci010 <cijhn@hotmail.com>
2017-07-03 17:54:49 +0800

    remove useless file; add vuex
    Former-commit-id: 77f23194b046fccbed27099d6051bb62accdceb3

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
 import App from './App'
+import store from './store'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   components: { App },
+  store,
   template: '<App/>'
 }).$mount('#app')
