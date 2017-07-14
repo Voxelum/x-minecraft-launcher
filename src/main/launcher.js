ebeb0653dd2cf8c48a4d23aeb89481fd5e861fc6
ci010 <cijhn@hotmail.com>
2017-07-14 16:49:37 +0800

    fix typo

--- src/main/launcher.js
+++ src/main/launcher.js
@@ -7,12 +7,16 @@ import {
     ipcMain
 } from 'electron'
 const paths = require('path')
+import modules from './module-io'
+import services from './services'
+
 class Lancher extends EventEmitter {
-    constructor(root, app, modules, service) {
+    constructor(root, app, modules, services) {
+        super()
         this.root = root
         this.app = app
         this._modules = modules
-        this._service = service
+        this._services = services
     }
 
     getPath(...path) {
@@ -23,21 +27,25 @@ class Lancher extends EventEmitter {
         //well this is future work 2333 
         //TODO toposort for module with dependencies and build tree 
     }
-    _onServiceInit() {
-
-    }
     _onload() {
+        console.log('load modules')
         let tree = _buildTree()
         for (let branch of tree) {
             branch.batch(this)
         }
     }
 
+    _onServiceInit() {
+        console.log('services init')
+        this._services
+    }
+
+
     require(module) {
         //this need to design...
     }
 }
-const modules = require('./module-io')
-const service = require('./service')
-const launcher = new Launcher(path.join(app.getPath('appData'), '.launcher'), app, modules, service)
+const launcher = new Launcher(path.join(app.getPath('appData'), '.launcher'), app, modules, services)
+launcher._onload()
+launcher._onServiceInit()
 export default launcher
