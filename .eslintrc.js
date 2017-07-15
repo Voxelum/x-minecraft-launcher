a76f80d072d834c835602a7a0e1e2e975c7722a0
Indexyz <jiduye@gmail.com>
2017-07-15 21:43:31 +0800

    [Typo] Init ESLint and EditorConfig
    Former-commit-id: 4b0dd0e97d95b646f981d85953e5151e4ca6ec5f

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "indent": ["error", 4],
+    "space-before-function-paren": ["warn", "never"],
+    "no-console": 0,
+    "no-alert": 0,
+    "func-names": 0,
+    "no-underscore-dangle": 0
+  }
+}
