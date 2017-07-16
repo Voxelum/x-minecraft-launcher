a99b21e6a55064c4528114964c63341aaff5a6aa
ci010 <cijhn@hotmail.com>
2017-07-16 05:27:42 -0700

    impl repo
    Former-commit-id: 4df62f5b973dbb97fb91b83872a7715173cb949e

--- src/main/module-io/utils/repository.js
+++ src/main/module-io/utils/repository.js
@@ -16,7 +16,7 @@ export default class Repository {
         this._store = new Map();
     }
     static _hash(buff) {
-        return crypto.createHash('sha1').update(buff).digest().toString();
+        return crypto.createHash('sha1').update(buff).digest('hex').toString('utf-8');
     }
     static _load(filePath) {
         return new Promise((resolve, reject) => {
@@ -31,13 +31,18 @@ export default class Repository {
     }
     _import(filePath, data, resource) {
         return new Promise((resolve, reject) => {
-            fs.writeFile(path.join(this._root, resource.hash), data, (err) => {
-                if (err) reject(err);
-                else resolve(resource);
-            })
+            const targetPath = path.join(this._root, `${resource.hash}.${resource.type}`);
+            if (!fs.existsSync(targetPath)) {
+                fs.writeFile(targetPath, data, { mod: 'w' }, (err) => {
+                    if (err) reject(err);
+                    else resolve(resource);
+                })
+            } else {
+                resolve(resource)
+            }
         });
     }
-    add(fpath) {
+    import(fpath) {
         return Repository._load(fpath).then(({ filePath, data, resource }) => {
             if (!this._store.has(resource.hash)) {
                 this._store.set(resource.hash, resource);
@@ -45,6 +50,45 @@ export default class Repository {
             }
             return resource;
         })
+    }
+    export(resource, targetDirectory, option) {
+        return new Promise((resolve, reject) => {
+            let res
+            if (typeof resource === 'string') {
+                if (this._store.has(resource)) {
+                    resolve(this._store.get(resource))
+                } else {
+                    reject(new Error('no such resource in cache!'))
+                }
+            } else if (resource instanceof Resource) {
+                resolve(resource)
+            } else {
+                reject(new Error('illegal argument!'));
+            }
+        }).then((res) => { // TODO mkdir
+            if (!option) option = {}
+            const targetPath = path.join(targetDirectory, option.fileName || res.fileName);
+            const mode = option.mode || 0;
+            switch (mode) {
+                case 0:
+                case 1:
+                case 2:
+                    return new Promise((resolve, reject) => {
+                        fs.link(path.join(this._root, `${res.hash}.${res.type}`), targetPath, (err) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve();
+                            }
+                        });
+                    });
+                case 3:
+                case 4:
+                default:
+                    break;
+            }
+            return res
+        });
     }
     all() {
         return this._store;
@@ -63,7 +107,8 @@ export default class Repository {
                             .then((resource) => {
                                 this._store.set(resource.hash, resource);
                                 return resource;
-                            })))
+                            }),
+                        ));
                     }
                 })
             }).then(result => Promise.all(result));
