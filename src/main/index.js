d649a8fcc21ff8511f7989d37694b193a94efa4f
ci010 <cijhn@hotmail.com>
2017-07-16 05:28:13 -0700

    redesign the loading process
    Former-commit-id: dc965f147ecbfb9f23d3c61f406211b24325ee39

--- src/main/index.js
+++ src/main/index.js
@@ -7,8 +7,6 @@ import {
     AuthService,
 } from 'ts-minecraft'
 
-import launcher from './launcher'
-
 const devMod = process.env.NODE_ENV === 'development'
 /**
  * Set `__static` path to static files in production
@@ -42,33 +40,7 @@ function createWindow() {
     })
 }
 
-function init() {
-    ipcMain.on('init', (event, args) => {
-        event.sender.send('init', 'pong')
-    })
-
-    ipcMain.on('login', (event, args) => {
-        const [account, password, mode] = args
-        if (mode === 'offline') {
-            event.sender.send('login', undefined, AuthService.offlineAuth(account))
-        } else {
-            AuthService.newYggdrasilAuthService().login(account, password, 'non').then(
-                result => event.sender.send('login', undefined, result),
-                err => event.sender.send('login', err),
-            )
-        }
-    })
-    ipcMain.on('launch', (event, options) => {
-        switch (options.type) {
-        case 'server':
-        case 'modpack':
-        default:
-        }
-    })
-    ipcMain.on('save', (event, args) => args.type)
-}
 app.on('ready', () => {
-    init()
     createWindow()
 })
 
@@ -105,50 +77,37 @@ ipcMain.once('fetchAll', (event) => {
         console.log('remote force reload! implement later...')
     })
 });
-(function() {
-    const context = {
-        getPath(path) {
-            if (typeof path === 'string') {
-                return paths.join(launcher.rootPath, path)
-            } else if (path instanceof Array) {
-                console.log(`paths  ${path}`)
-                return paths.join(launcher.rootPath, path.join(path))
-            }
-            return launcher.rootPath
-        },
-    }
 
-    const modules = launcher._modulesIO
-    const promises = []
+const launcher = require('./launcher'); // require launcher AFTER modules loaded
+
+console.log('services start init')
+const services = launcher._services
+for (const key in services) {
+    if (services.hasOwnProperty(key)) {
+        const service = services[key];
+        if (service.initialize) {
+            service.initialize();
+        }
+    }
+}
+console.log('services inited');
+(function () {
+    const modules = launcher._modulesIO;
+    const promises = [];
     for (const key in modules) {
         if (modules.hasOwnProperty(key)) {
             const m = modules[key];
-            promises.push(m.load(context).then(mod => ({ id: key, module: mod })))
+            promises.push(m.load(launcher).then(mod => ({ id: key, module: mod })));
         }
     }
-    console.log('loaded modules')
-    return Promise.all(promises)
+    return Promise.all(promises);
 })().then((modules) => {
-    console.log('services start init')
-    const services = launcher._services
-    for (const key in services) {
-        if (services.hasOwnProperty(key)) {
-            const service = services[key];
-            if (service.initialize) {
-                service.initialize();
-            }
-        }
-    }
-    console.log('services inited')
-    return modules
-}).then((modules) => {
-    const tree = {}
+    console.log('loaded module');
+    const tree = {};
     for (const m of modules) { tree[m.id] = m.module; }
-    if (_reqTreeEventHolder) { _reqTreeEventHolder.sender.send('fetchAll', undefined, tree) } else { _reqTreeEventHolder = tree }
+    if (_reqTreeEventHolder) { _reqTreeEventHolder.sender.send('fetchAll', undefined, tree); } else { _reqTreeEventHolder = tree; }
     return tree
 }).catch((e) => {
     console.log(e)
 });
-
-console.log(launcher)
 
