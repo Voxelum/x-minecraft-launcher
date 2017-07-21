d77912387e54edf3a5fb5f978b5b139c5301a81a
ci010 <cijhn@hotmail.com>
2017-07-21 17:17:57 +0800

    make plugin independent

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import dynmodule from './modules/models/dynmodule'
-
+import plugins from './plugins'
 import modules from './modules'
 
 Vue.use(Vuex)
@@ -10,5 +9,5 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     modules,
     strict: process.env.NODE_ENV !== 'production',
-    plugins: [dynmodule],
+    plugins,
 })
