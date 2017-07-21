f5437ec18c273b0594bdd8152d6a3fb44efdf641
ci010 <cijhn@hotmail.com>
2017-07-21 17:58:56 +0800

    organize the project
    Former-commit-id: a7779f090cfb3ef4c3b524cad16f0286b11c9a17

---
+++ src/renderer/semantic-ui/components/ResourcePackList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="ui list">
+        <div class="item">
+            <div class="right floated content">
+                <div class="ui button">
+                    <i class="arrow right icon"></i>
+                </div>
+            </div>
+            <i class="map marker icon"></i>
+            <div class="content">
+                <div class="header">Krolewskie Jadlo</div>
+                <div class="description">A pack that do xxx</div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="right floated content">
+                <div class="ui button">
+                    <i class="arrow right icon"></i>
+                </div>
+            </div>
+            <i class="map marker icon"></i>
+            <div class="content">
+                <div class="header">Xian Famous Foods</div>
+                <div class="description">A pack bring xxx exp to mc.</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/renderer/store/modules/theme.js
@@ -0,0 +1,6 @@
+import { Store } from 'vuex'
+import multiSelect from './models/multi-select'
+
+const obj = Object.assign({}, multiSelect)
+
+export default obj
