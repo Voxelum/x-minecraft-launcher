104bd3000c18ad6dba2dcbbaf14488d6d9930f27
ci010 <cijhn@hotmail.com>
2017-07-25 08:03:10 -0700

    optimize ui
    Former-commit-id: 6588575d77ba1b692ddc02f57ae5a5797411f42e

--- src/renderer/semantic-ui/components/ProfileCard.vue
+++ src/renderer/semantic-ui/components/ProfileCard.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="ui card" v-bind:class="{color}" @click="onclick">
-        <p class="ui top attached label">{{$t(this.source.type)}}</p>
+    <div class="ui card" v-bind:class="{color}">
+        <p class="ui top attached label" :data-tooltip="$t(this.source.type)" data-inverted="">
+            <span v-if="this.source.type=='server'">
+                <i class="server icon"></i>
+            </span>
+            <span v-else>
+                <i class="cubes icon"></i>
+            </span>
+            {{this.source.name}}
+            <i class="right floated large delete icon" :class="{red:hoverDelete}" @mouseover="hoverDelete=true" @mouseout="hoverDelete=false"></i>
+        </p>
         <div class="image">
             <img :src="this.source.image">
         </div>
-        <div class="content">
-            <a class="header">{{this.source.name}}</a>
+        <div class="content" @click="onclick">
             <div class="meta">
+                <span class="ui tiny label">
+                    {{this.source.version?this.source.version.length!=0?"unknown":this.source.version:"unknown"}}
+                </span>
                 <span v-if="this.source.type=='server'">{{this.source.ip}}</span>
                 <span v-if="this.source.type=='modpack'">{{this.source.author}}</span>
                 <span class="date">{{this.source.createdDate}}</span>
             </div>
             <div class="description">
-                {{this.source.description}}
+                {{this.source.description?this.source.description!=0?this.source.description:"No description yet.":"No description yet."}}
             </div>
         </div>
         <div class="extra content">
@@ -27,16 +38,23 @@
 <script>
 
 export default {
+    data() {
+        return {
+            hoverDelete: false,
+        }
+    },
     name: 'profile-card',
     props: ['color', 'source', 'id'],
     methods: {
         onclick(e) {
             this.$emit('select', this.id, this.source)
-        }
+        },
+        del(e) {
+            this.$emit('delete', this.id, this.source)
+        },
     },
     mounted() {
     },
-
 }
 </script>
 
