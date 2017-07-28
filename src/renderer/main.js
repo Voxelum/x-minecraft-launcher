9ec5a8173ac24fcb59478ec138621791ad37db32
ci010 <cijhn@hotmail.com>
2017-07-28 07:29:00 -0700

    merge

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue';
 
-
 import App from './App.vue'
 import store from './store'
 import launcher from './launcher'
 import i18n from './i18n';
 
-<<<<<<< HEAD
-=======
 
->>>>>>> f614f4ef10fc60a377ad38e9104f83ce7992f814
 if (!process.env.IS_WEB) {
     Vue.use(require('vue-electron'))
 }
