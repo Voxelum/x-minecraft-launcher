8d03989c5e23e06b78b717a75ebf0f55a484ac61
ci010 <cijhn@hotmail.com>
2017-07-28 07:27:50 -0700

    cleanup

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue';
-import MuseUI from 'muse-ui';
-import Vuetify from 'vuetify';
-import 'muse-ui/dist/muse-ui.css'
+
 
 import App from './App.vue'
 import store from './store'
 import launcher from './launcher'
 import i18n from './i18n';
 
-Vue.use(MuseUI);
-Vue.use(Vuetify);
 if (!process.env.IS_WEB) {
     Vue.use(require('vue-electron'))
 }
