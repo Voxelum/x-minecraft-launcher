aceac8f7a0bc8691f0a5a745dc662bc5250f0ce2
ci010 <cijhn@hotmail.com>
2017-08-04 16:57:54 +0800

    not force save unsaveable module

--- src/renderer/store/plugins/autosave.js
+++ src/renderer/store/plugins/autosave.js
@@ -3,16 +3,19 @@ export default (store) => {
         const type = mutation.type
         if (type.endsWith('$reload')) return
         const moduleId = type.substring(0, type.indexOf('/'))
-        store.dispatch(`${moduleId}/save`, { mutation: type, object: mutation.payload }).then(() => {
-            console.log(`Module [${moduleId}] saved by ${type}`);
-        },
-            (err) => {
-                console.warn(`Module [${moduleId}] saving occured an error:`)
-                console.warn(err)
-            })
-            .catch((err) => {
-                console.warn(`Module [${moduleId}] saving occured an error:`)
-                console.warn(err)
-            });
+        const action = `${moduleId}/save`
+        if (store._actions[action]) {
+            store.dispatch(action, { mutation: type, object: mutation.payload }).then(() => {
+                console.log(`Module [${moduleId}] saved by ${type}`);
+            },
+                (err) => {
+                    console.warn(`Module [${moduleId}] saving occured an error:`)
+                    console.warn(err)
+                })
+                .catch((err) => {
+                    console.warn(`Module [${moduleId}] saving occured an error:`)
+                    console.warn(err)
+                });
+        }
     });
 }
