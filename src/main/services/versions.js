39d7cce9f6230d67b27d63323b879ddb3196c060
ci010 <cijhn@hotmail.com>
2017-08-04 14:53:16 +0800

    fix a stupid bug...

--- src/main/services/versions.js
+++ src/main/services/versions.js
@@ -9,44 +9,10 @@ const versionProviders = new Map()
 
 export default {
     initialize() {
-        // versionProviders.set('minecraft', (versionmeta) => {
-        // })
-        // const mc = new MinecraftFolder(launcher.rootPath)
-        // fs.readdir(mc.versions, (err, files) => {
-        //     if (err) {
-        //         console.error(err)
-        //     } else {
-        //         for (const file of files) {
-        //             Version.parse(mc, file)
-        //                 .then(version => Version.checkDependency(version, mc))
-        //                 .catch((error) => {
-        //                     console.error(error)
-        //                 })
-        //         }
-        //     }
-        // })
     },
     proxy: {
-        // register(id, versionProvider) {
-        //     versionProviders.set(id, versionProvider)
-        // },
     },
     actions: {
-        // update(versionType) {
-        //     return versionProviders.has(versionType) ? versionProviders.get(versionType).update() : 
-        // Promise.reject(`No such version provider: ${versionType}`)
-        // },
-        // require(version) {
-        //     // TODO handle the version dependent tree
-        //     for (const v in version) {
-        //         if (version.hasOwnProperty(v)) {
-        //             const id = version[v];
-        //             if (versionProviders.has(id)) {
-        //                 versionProviders.get(id).require()
-        //             }
-        //         }
-        //     }
-        // },
         refresh(updateTime) {
             return Version.updateVersionMeta({ date: updateTime })
         },
@@ -58,16 +24,7 @@ export default {
             if (!(location instanceof MinecraftFolder)) return Promise.reject('Require location as string or MinecraftLocation!')
             return Version.downloadVersion('client', meta, location)
                 .then(() => Version.parse(location.root, meta.id))
-                .then((version) => {
-                    this.actions.checkClient({ version, location })
-                        .then((v) => {
-                            console.log('suc!')
-                        })
-                        .catch((err) => {
-                            console.error(err)
-                        });
-                    return version;
-                })
+                .then(version => Version.checkDependency(version, location))
         },
         checkClient({ version, location }) {
             if (typeof location === 'string') location = new MinecraftFolder(location)
