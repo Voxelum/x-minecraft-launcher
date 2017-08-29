8614b346098c23d867896c9e21d6fcdc911a98ea
ci010 <cijhn@hotmail.com>
2017-08-29 17:19:21 +0800

    try to enable the editor, but break

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -23,7 +23,8 @@ let whiteListedModules = ['vue']
 let rendererConfig = {
   devtool: '#cheap-module-eval-source-map',
   entry: {
-    renderer: path.join(__dirname, '../src/renderer/main.js')
+    renderer: path.join(__dirname, '../src/renderer/main.js'),
+    // logger: path.join(__dirname, '../src/renderer/log.js')
   },
   externals: [
     ...Object.keys(dependencies || {}).filter(d => !whiteListedModules.includes(d))
@@ -103,6 +104,18 @@ let rendererConfig = {
         ? path.resolve(__dirname, '../node_modules')
         : false
     }),
+    // new HtmlWebpackPlugin({
+    //   filename: 'log.html',
+    //   template: path.resolve(__dirname, '../src/log.ejs'),
+    //   minify: {
+    //     collapseWhitespace: true,
+    //     removeAttributeQuotes: true,
+    //     removeComments: true
+    //   },
+    //   nodeModules: process.env.NODE_ENV !== 'production'
+    //     ? path.resolve(__dirname, '../node_modules')
+    //     : false
+    // }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jquery: 'jquery',
