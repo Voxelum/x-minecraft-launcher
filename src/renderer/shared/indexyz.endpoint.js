d535b1c308cac7a6a88db660d5b07dcdce3655ec
ci010 <cijhn@hotmail.com>
2017-09-06 08:03:36 -0700

    refactor

--- src/renderer/shared/indexyz.endpoint.js
+++
@@ -1,8 +0,0 @@
-export default {
-    project(projectId) {
-        return `curse.mcdev.se/project/id/${projectId}`
-    },
-    files(projectId) {
-        return `curse.mcdev.se/files/project/${projectId}`
-    },
-}
