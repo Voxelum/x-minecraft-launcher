03df7684dd1835e37b4a588ace19064a66c2903e
ci010 <cijhn@hotmail.com>
2017-09-07 15:36:03 +0800

    login refactor; breaking...
    Former-commit-id: 7fd015b7df7f9b598cd8443ae435837ba2ca89d5

---
+++ src/main/services/profile.js
@@ -0,0 +1,44 @@
+import { v4 } from 'uuid'
+import { ProfileService, GameProfile } from 'ts-minecraft'
+
+const registered = {}
+export default {
+    initialize() {
+        registered.mojang = ProfileService.mojang
+    },
+
+    proxy: {
+        register(id, service) {
+            if (registered[id]) {
+                throw new Error(`duplicated id: ${id}`)
+            }
+            registered[id] = service;
+        },
+    },
+
+    actions: {
+        async fetch({ service, uuid, pubKey, cache }) {
+            if (!registered[service]) throw new Error(`No such auth option ${service}`);
+            const profile = await ProfileService.fetch(uuid, { api: registered[service], pubKey })
+            if (cache) {
+                const tex = await GameProfile.cacheTextures(profile);
+                profile.properties = undefined;
+                profile.textures = tex;
+            }
+            return profile;
+        },
+        async lookup({ service, id, pubKey, cache }) {
+            if (!registered[service]) throw new Error(`No such auth option ${service}`);
+            const profile = await ProfileService.lookup(id, { api: registered[service], pubKey })
+            if (cache) {
+                const tex = await GameProfile.cacheTextures(profile);
+                profile.properties = undefined;
+                profile.textures = tex;
+            }
+            return profile;
+        },
+        services() {
+            return Array.from(registered.keys())
+        },
+    },
+}
--- src/renderer/store/modules/auth.js
+++ src/renderer/store/modules/auth.js
@@ -1,73 +1,108 @@
 import Vue from 'vue'
 
-const state = {
-    modes: ['mojang', 'offline'],
-    mode: 'mojang',
-    history: {},
-    accessToken: '',
-    clientToken: '',
-    authInfo: undefined, // cached
-}
-const getters = {
-    modes: theState => theState.modes,
-    disablePassword: theState => theState.mode === 'offline',
-    mode: theState => theState.mode,
-    info: theState => theState.authInfo || {},
-    username: theState => (theState.authInfo ? theState.authInfo.selectedProfile.name : 'Steve'),
-    skin: theState =>
-        (theState.authInfo ? theState.authInfo.selectedProfile.properties.textures.textures.SKIN : undefined)
+export default {
+    namespaced: true,
+    state: {
+        modes: ['mojang', 'offline'],
+        mode: 'mojang',
+        history: {},
 
-}
+        auth: {
+            id: '',
+            name: '',
+            profiles: [],
+            accessToken: '',
+            clientToken: '',
 
-const mutations = {
-    select(theState, mode) {
-        if (theState.modes.indexOf(mode) !== -1) {
-            theState.mode = mode
-        }
+            skin: { data: undefined, slim: false },
+            cape: undefined,
+        }, // cached
     },
-    record(theState, { // record the state history
-        auth,
-        account,
-    }) {
-        console.log(auth)
-        theState.authInfo = auth
-        theState.clientToken = auth.clientToken
-        theState.accessToken = auth.accessToken
-        if (!theState.history[theState.mode]) Vue.set(theState.history, theState.mode, [])
-        theState.history[theState.mode].push(account)
+    getters: {
+        modes: state => state.modes,
+        disablePassword: state => state.mode === 'offline',
+        mode: state => state.mode,
+        info: state => state.auth,
+        username: state => state.auth.name,
+        id: state => state.auth.id,
+        skin: state => state.auth.skin,
+        cape: state => state.auth.cape,
+        history: state => state.history[state.mode],
     },
-}
-const actions = {
-    save(context, payload) {
-        const { mutation } = payload;
-        if (!mutation.endsWith('/record')) return Promise.resolve()
-        const data = JSON.stringify(context.state, (key, value) => (key === 'modes' ? undefined : value))
-        return context.dispatch('write', { path: 'auth.json', data }, { root: true })
+    mutations: {
+        select(state, mode) {
+            if (state.modes.indexOf(mode) !== -1) state.mode = mode
+        },
+        record(state, { // record the state history
+            auth,
+            account,
+        }) {
+            state.auth.clientToken = auth.clientToken
+            state.auth.accessToken = auth.accessToken
+            state.auth.profiles = auth.profiles
+            state.auth.id = auth.selectedProfile.id;
+            state.auth.name = auth.selectedProfile.name;
+            state.auth.skin.data = auth.skin.data;
+            state.auth.skin.slim = auth.skin.slim;
+            state.auth.cape = auth.cape;
+            if (!state.history[state.mode]) Vue.set(state.history, state.mode, [])
+            const his = state.history[state.mode];
+            const idx = his.indexOf(account);
+            if (idx === -1) his.push(account)
+            else if (idx === 0) return;
+            const first = his[0];
+            Vue.set(his, 0, account);
+            Vue.set(his, idx, first);
+        },
+        clear(state) {
+            state.auth.accessToken = ''
+            state.auth.profiles = []
+            state.auth.id = '';
+            state.auth.name = 'Steve';
+            state.auth.skin.data = undefined;
+            state.auth.cape = undefined;
+        },
     },
-    load(context, payload) {
-        return context.dispatch('read', { path: 'auth.json', fallback: {}, encoding: 'json' }, { root: true })
-            .then(data => context.dispatch('query', { service: 'auth', action: 'modes' }, { root: true }).then(modes => [modes, data]))
-            .then(([modes, data]) => {
-                data.modes = modes
-                return data
-            })
-    },
-    login(context, payload) {
-        return context.dispatch('query', { service: 'auth', action: 'login', payload }, { root: true })
-            .then((result) => {
+    actions: {
+        save(context, payload) {
+            const { mutation } = payload;
+            if (!mutation.endsWith('/record')) return Promise.resolve()
+            const data = JSON.stringify(context.state, (key, value) => (key === 'modes' ? undefined : value))
+            return context.dispatch('write', { path: 'auth.json', data }, { root: true })
+        },
+        async load(context, payload) {
+            const data = await context.dispatch('read', { path: 'auth.json', fallback: {}, encoding: 'json' }, { root: true });
+            data.modes = await context.dispatch('query', { service: 'auth', action: 'modes' }, { root: true });
+            return data;
+        },
+        async logout({ commit, dispatch }) {
+            commit('clear')
+        },
+        async login(context, payload) {
+            const result = await context.dispatch('query', { service: 'auth', action: 'login', payload }, { root: true })
+
+            try {
+                const profile = await context.dispatch('query', {
+                    service: 'profile',
+                    action: 'fetch',
+                    payload: { service: 'mojang', uuid: result.userId, cache: true },
+                }, { root: true });
+                const skin = profile.textures.textures.SKIN
+                if (skin) {
+                    result.skin = {
+                        data: skin.data,
+                        slim: skin.metadata.model === 'slim',
+                    }
+                }
+                if (profile.textures.textures.CAPE) result.cape = profile.textures.textures.CAPE.data;
+                return result;
+            } catch (e) {
                 context.commit('record', {
                     auth: result,
                     account: payload.account,
-                })
-                return result
-            })
+                });
+                return result;
+            }
+        },
     },
-}
-
-export default {
-    namespaced: true,
-    state,
-    getters,
-    mutations,
-    actions,
 }
