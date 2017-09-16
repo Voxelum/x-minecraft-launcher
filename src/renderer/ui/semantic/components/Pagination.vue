2563daa7fa2384883d67e8f46fd21bb66849f2dc
ci010 <cijhn@hotmail.com>
2017-09-16 09:58:17 -0700

    pagitation
    Former-commit-id: 2d97b74d978e1fee4ba902a5d469d1fd049e4a65

---
+++ src/renderer/ui/semantic/components/Pagination.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="ui pagination menu" :class="{secondary:secondary}">
+        <a class="item" :class="{active: p===page, disabled: p==='...'}" v-for="p of pagesCache" :key="p" @click="page = p">
+            {{p}}
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            page: 1,
+            pagesCache: [],
+        }
+    },
+    watch: {
+        page() {
+            this.pagesCache = this.calculatePages(this.page, this.pages)
+            this.$emit('page', this.page)
+        },
+    },
+    mounted() {
+        this.pagesCache = this.calculatePages(this.page, this.pages)
+    },
+    methods: {
+        calculatePages(page, total) {
+            const start = Math.max(page - 2, 1)
+            const end = Math.min(page + 3, total);
+            const pages = []
+            if (start > 1) pages.push(1, '...');
+            for (let i = start; i < end; i += 1) pages.push(i);
+            if (end < total) pages.push('...', total)
+            return pages;
+        }
+    },
+    props: {
+        pages: {
+            type: Number,
+            default: 1,
+        },
+        secondary: {
+            type: Boolean,
+            default: true,
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
