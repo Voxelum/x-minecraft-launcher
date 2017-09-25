16f78bb13c40ae7dbfe1fa51eb74560f2bfcd474
ci010 <cijhn@hotmail.com>
2017-09-25 14:15:36 -0700

    cleanup
    Former-commit-id: d332f8ecf353edd494a99d39840ef29553a005e8

--- src/renderer/store/modules/profiles/profile.js
+++ src/renderer/store/modules/profiles/profile.js
@@ -4,8 +4,8 @@ export default {
     modules: { ...settings },
     state: () => ({
         type: '',
-        name: '', // specific info
-        resolution: { width: 800, height: 400, fullscreen: false }, // client setting
+        name: '', 
+        resolution: { width: 800, height: 400, fullscreen: false },  
         java: '',
         minMemory: 1024,
         maxMemory: 2048,
@@ -13,25 +13,20 @@ export default {
         mcOptions: [],
     }),
     getters: {
-        errors(states) {
+        errors(state) {
             const errors = []
-            if (states.minecraft.version === '' || states.minecraft.version === undefined || states.minecraft.version === null) errors.push('profile.empty.version')
-            if (states.java === '' || states.java === undefined || states.java === null) errors.push('profile.empty.java')
+            if (state.minecraft.version === '' || state.minecraft.version === undefined || state.minecraft.version === null) errors.push('profile.empty.version')
+            if (state.java === '' || state.java === undefined || state.java === null) errors.push('profile.empty.java')
             return errors
         },
-        versoin: states => states.minecraft.version,
-        language: (states, gets) => gets['minecraft/options'].lang,
+        versoin: state => state.minecraft.version,
+        language: (state, gets) => gets['minecraft/options'].lang,
     },
     mutations: {
-        putAll(states, option) {
+        putAll(state, option) {
             Object.keys(option)
-                .filter(key => states.hasOwnProperty(key))
-                .forEach(key => (states[key] = option[key]))
-            // for (const key in option) {
-            //     if (option.hasOwnProperty(key) && states.hasOwnProperty(key)) {
-            //         states[key] = option[key];
-            //     }
-            // }
+                .filter(key => state.hasOwnProperty(key))
+                .forEach((key) => { state[key] = option[key] })
         },
     },
     actions: {},
