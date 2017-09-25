cccf3bc4c4ac35b5e3de38019cb1619bea56c37b
ci010 <cijhn@hotmail.com>
2017-09-25 10:05:04 -0700

    clear up

--- src/renderer/ui/semantic/Semantic.vue
+++ src/renderer/ui/semantic/Semantic.vue
@@ -254,24 +254,33 @@ body ::-webkit-scrollbar-thumb {
 ::-webkit-scrollbar-thumb:window-inactive {
     /* background: rgba(0, 0, 0, 0.2); */
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .3s ease;
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .3s ease;
 }
-.fade-enter, .fade-leave-active {
-  opacity: 0
+
+.fade-enter,
+.fade-leave-active {
+    opacity: 0
 }
+
 .child-view {
-  position: absolute;
-  transition: all .3s cubic-bezier(.55,0,.1,1);
+    position: absolute;
+    transition: all .3s cubic-bezier(.55, 0, .1, 1);
 }
-.slide-left-enter, .slide-right-leave-active {
-  opacity: 0;
-  -webkit-transform: translate(30px, 0);
-  transform: translate(30px, 0);
+
+.slide-left-enter,
+.slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(30px, 0);
+    transform: translate(30px, 0);
 }
-.slide-left-leave-active, .slide-right-enter {
-  opacity: 0;
-  -webkit-transform: translate(-30px, 0);
-  transform: translate(-30px, 0);
+
+.slide-left-leave-active,
+.slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-30px, 0);
+    transform: translate(-30px, 0);
 }
 </style>
