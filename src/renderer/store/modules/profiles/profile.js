60ae6ac84a3bd37ccd4d07a8d3762a96a098ea88
ci010 <cijhn@hotmail.com>
2017-09-24 22:47:47 -0700

    refactor
    Former-commit-id: 36ab6840c9a85434a37197dfb2cba849c687f2be

--- src/renderer/store/modules/profiles/profile.js
+++ src/renderer/store/modules/profiles/profile.js
@@ -1,7 +1,8 @@
 import settings from './settings'
 
-function state() {
-    return {
+export default {
+    modules: { ...settings },
+    state: () => ({
         type: '',
         name: '', // specific info
         resolution: { width: 800, height: 400, fullscreen: false }, // client setting
@@ -10,37 +11,28 @@ function state() {
         maxMemory: 2048,
         vmOptions: [],
         mcOptions: [],
-    }
-}
-const getters = {
-    errors(states) {
-        const errors = []
-        if (states.minecraft.version === '' || states.minecraft.version === undefined || states.minecraft.version === null) errors.push('profile.empty.version')
-        if (states.java === '' || states.java === undefined || states.java === null) errors.push('profile.empty.java')
-        return errors
+    }),
+    getters: {
+        errors(states) {
+            const errors = []
+            if (states.minecraft.version === '' || states.minecraft.version === undefined || states.minecraft.version === null) errors.push('profile.empty.version')
+            if (states.java === '' || states.java === undefined || states.java === null) errors.push('profile.empty.java')
+            return errors
+        },
+        versoin: states => states.minecraft.version,
+        language: (states, gets) => gets['minecraft/options'].lang,
     },
-    versoin: states => states.minecraft.version,
-    language: (states, gets) => gets['minecraft/options'].lang,
-}
-const mutations = {
-    putAll(states, option) {
-        for (const key in option) {
-            if (option.hasOwnProperty(key) && states.hasOwnProperty(key)) {
-                states[key] = option[key];
-            }
-        }
+    mutations: {
+        putAll(states, option) {
+            Object.keys(option)
+                .filter(key => states.hasOwnProperty(key))
+                .forEach(key => (states[key] = option[key]))
+            // for (const key in option) {
+            //     if (option.hasOwnProperty(key) && states.hasOwnProperty(key)) {
+            //         states[key] = option[key];
+            //     }
+            // }
+        },
     },
-}
-
-const actions = {
-}
-
-export default {
-    modules: {
-        ...settings,
-    },
-    state,
-    getters,
-    mutations,
-    actions,
+    actions: {},
 }
--- src/renderer/store/modules/profiles/server.js
+++ src/renderer/store/modules/profiles/server.js
@@ -2,81 +2,74 @@ import { TextComponent, TextFormatting, Style, ServerInfo, ServerStatus } from '
 import protocol from 'shared/protocol'
 import profile from './profile'
 
-function state() {
-    const theState = profile.state()
 
-    theState.type = 'server'
-    theState.host = ''
-    theState.port = 25565
-    theState.isLanServer = false
-    theState.icon = ''
-    theState.status = {}
-    return theState
-}
-const getters = {
-    ...profile.getters,
-    errors(states) {
-        // const err = profile.getters.errors(state)
-        // this probably is a issue.... if i delegate to profile's getter; the responsive will fail.
-        const errors = []
-        if (states.minecraft.version === '' || states.minecraft.version === undefined || states.minecraft.version === null) errors.push('profile.empty.version')
-        if (states.java === '' || states.java === undefined || states.java === null) errors.push('profile.empty.java')
-        if (states.host === '' || states.host === undefined || states.host === null) {
-            errors.push('server.empty.host')
-        }
-        return errors;
-    },
-}
-
-const mutations = profile.mutations
-
-const actions = {
-    serialize(context, payload) {
-        return JSON.stringify(context.state, (key, value) => {
-            if (key === 'settings' || key === 'maps' || key === 'status') return undefined;
-            return value;
-        })
-    },
-    refresh(context, force) {
-        if (context.state.status.pingToServer && !force) return Promise.resolve();
-        context.commit('putAll', { status: ServerStatus.pinging() })
-        if (context.state.host === undefined) return Promise.reject('server.host.empty')
-        return context.dispatch('query', {
-            service: 'servers',
-            action: 'ping',
-            payload: { host: context.state.host, port: context.state.port },
-        }, { root: true })
-            .then((frame) => {
-                const status = ServerStatus.from(frame)
-                status.pingToServer = frame.ping
-                const all = {
-                    icon: status.icon,
-                    status,
-                }
-                const versions = protocol[status.protocolVersion]
-                if (versions) context.commit('minecraft/version', versions[0]);
-                context.commit('putAll', all)
-                return status
-            }, (err) => {
-                if (err.code === 'ETIMEOUT') {
-                    const timeout = TextComponent.str('server.status.timeout');
-                    timeout.style = Style.create({ color: TextFormatting.RED })
-                    context.commit('putAll', { status: new ServerStatus(TextComponent.str('version.unknown'), timeout, -1, -1, -1) })
-                } else if (err.code === 'ECONNREFUSED') {
-                    const nohost = TextComponent.str('server.status.nohost');
-                    nohost.style = Style.create({ color: TextFormatting.RED })
-                    context.commit('putAll', { status: new ServerStatus(TextComponent.str('version.unknown'), nohost, -1, -1, -1) })
-                } else {
-                    context.commit('putAll', { status: new ServerStatus(TextComponent.str('version.unknown'), 'Internet Error', -1, -1, -1) })
-                }
-            })
-    },
-}
 export default {
     namespaced: true,
     modules: { ...profile.modules },
-    state,
-    getters,
-    mutations,
-    actions,
+    state: () => ({
+        ...profile.state(),
+        type: 'server',
+        host: '',
+        port: 25565,
+        isLanServer: false,
+        icon: '',
+        status: {},
+    }),
+    getters: {
+        ...profile.getters,
+        errors(state) {
+            // const err = profile.getters.errors(state)
+            // this probably is a issue.... if i delegate to profile's getter; the responsive will fail.
+            const errors = []
+            if (state.minecraft.version === '' || state.minecraft.version === undefined || state.minecraft.version === null) errors.push('profile.empty.version')
+            if (state.java === '' || state.java === undefined || state.java === null) errors.push('profile.empty.java')
+            if (state.host === '' || state.host === undefined || state.host === null) {
+                errors.push('server.empty.host')
+            }
+            return errors;
+        },
+    },
+    mutations: profile.mutations,
+    actions: {
+        serialize(context, payload) {
+            return JSON.stringify(context.state, (key, value) => {
+                if (key === 'settings' || key === 'maps' || key === 'status') return undefined;
+                return value;
+            })
+        },
+        refresh(context, force) {
+            if (context.state.status.pingToServer && !force) return Promise.resolve();
+            context.commit('putAll', { status: ServerStatus.pinging() })
+            if (context.state.host === undefined) return Promise.reject('server.host.empty')
+            return context.dispatch('query', {
+                service: 'servers',
+                action: 'ping',
+                payload: { host: context.state.host, port: context.state.port },
+            }, { root: true })
+                .then((frame) => {
+                    const status = ServerStatus.from(frame)
+                    status.pingToServer = frame.ping
+                    const all = {
+                        icon: status.icon,
+                        status,
+                    }
+                    const versions = protocol[status.protocolVersion]
+                    if (versions) context.commit('minecraft/version', versions[0]);
+                    context.commit('putAll', all)
+                    return status
+                }, (err) => {
+                    if (err.code === 'ETIMEOUT') {
+                        const timeout = TextComponent.str('server.status.timeout');
+                        timeout.style = Style.create({ color: TextFormatting.RED })
+                        context.commit('putAll', { status: new ServerStatus(TextComponent.str('version.unknown'), timeout, -1, -1, -1) })
+                    } else if (err.code === 'ECONNREFUSED') {
+                        const nohost = TextComponent.str('server.status.nohost');
+                        nohost.style = Style.create({ color: TextFormatting.RED })
+                        context.commit('putAll', { status: new ServerStatus(TextComponent.str('version.unknown'), nohost, -1, -1, -1) })
+                    } else {
+                        context.commit('putAll', { status: new ServerStatus(TextComponent.str('version.unknown'), 'Internet Error', -1, -1, -1) })
+                    }
+                })
+        },
+    },
 }
