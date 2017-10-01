6fd65ed670f77089552ec2c63a1c6db638e0b788
ci010 <cijhn@hotmail.com>
2017-09-30 21:57:31 -0700

    quick fix settings state

--- src/renderer/store/state.js
+++ src/renderer/store/state.js
@@ -5,6 +5,9 @@ export default {
     templates: {
         minecraft: { ...options },
     },
+    theme: 'semantic',
+    themes: [],
+    defaultResolution: { width: 400, height: 400, fullscreen: false },  
     url: '',
     root: '',
 }
