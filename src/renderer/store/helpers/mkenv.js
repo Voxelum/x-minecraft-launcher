cfac1a9e002a86c17e79fb0fc2c6c6ffd01b27be
ci010 <cijhn@hotmail.com>
2017-10-02 16:33:03 -0700

    add  typing
    Former-commit-id: c08688f68400f08f23050f0fa425fc9de497f24d

--- src/renderer/store/helpers/mkenv.js
+++ src/renderer/store/helpers/mkenv.js
@@ -1,22 +1,36 @@
 import fs from 'fs-extra'
+import { ActionContext } from 'vuex'
+import { MinecraftFolder } from 'ts-minecraft'
 
+/**
+ * @param {ActionContext} context 
+ * @param {string} id 
+ * @param {MinecraftFolder} rootLoc 
+ * @param {MinecraftFolder} profileLoc 
+ */
 async function mkRespack(context, id, rootLoc, profileLoc) {
-    const allPacks = context.getters['resourcepacks/allKeys'];
-    const targetDirectory = profileLoc.resourcepacks;
-    await fs.ensureDir(targetDirectory)
+    const allPacks = context.getters['repository/resourcepacks'];
+    await fs.ensureDir(profileLoc.resourcepacks)
 
-    return Promise.all(allPacks.map(key => context.dispatch('resourcepacks/export', { resource: key, targetDirectory })
+    return Promise.all(allPacks.map(key => context.dispatch('link', { resource: key, minecraft: profileLoc.root })
         .catch((e) => {
             console.warn(`Cannot export resourcepack id: ${key}`)
             console.warn(e)
         })))
 }
 
+/**
+ * @param {ActionContext} context 
+ * @param {string} id 
+ * @param {MinecraftFolder} rootFolder 
+ * @param {MinecraftFolder} profileFolder 
+ */
 async function mkMods(context, id, rootFolder, profileFolder) {
     const targetDirectory = profileFolder.mods;
     await fs.remove(targetDirectory)
     await fs.ensureDir(targetDirectory);
-    const getMod = context.getters['mods/get'];
+
+    const getMod = context.getters['repository/mods'];
 }
 
 export default async (context, profileId, rootLoc, profileLoc) => {
