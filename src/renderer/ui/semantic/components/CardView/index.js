3a2805623a2edb509073ebb1977ca950ed12ad8d
ci010 <cijhn@hotmail.com>
2017-10-05 16:53:54 -0700

    minor fix bugsa

--- src/renderer/ui/semantic/components/CardView/index.js
+++ src/renderer/ui/semantic/components/CardView/index.js
@@ -3,7 +3,7 @@ import ModpackCard from './ModpackCard'
 import ServerCard from './ServerCard'
 
 export default {
-    components: { 'modpack-card': ModpackCard, 'server-card': ServerCard },
+    name: 'CardView',  components: { 'modpack-card': ModpackCard, 'server-card': ServerCard },
     computed: {
         ...vuex.mapGetters('profiles', ['allKeys', 'getByKey']),
     },
