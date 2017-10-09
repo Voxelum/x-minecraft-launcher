5369bf60b355ee242488768b44b312b2f6b4a36f
ci010 <cijhn@hotmail.com>
2017-10-08 19:03:44 -0700

    dynamic modals

--- src/renderer/ui/semantic/components/modals/index.js
+++ src/renderer/ui/semantic/components/modals/index.js
@@ -5,4 +5,24 @@ files.keys().forEach((key) => {
     modules[key.replace(/(\.\/|\.vue)/g, '')] = files(key)
 })
 
-export default modules
+export default {
+    components: {
+        ...modules,
+    },
+    render(createElement) {
+        return createElement('div', {},
+            Object.keys(modules).map(name => createElement(name, {
+                ref: name.toLowerCase(),
+            })))
+    },
+    mounted() {
+        this.$bus.$on('modal', this.show)
+    },
+    methods: {
+        show(id, args) {
+            const modal = this.$refs[`${id}modal`.toLowerCase()]
+            if (modal) modal.show(args); // $(modal).modal('show') // eslint-disable-line
+            else console.warn(`No modal named ${id}`)
+        },
+    },
+}
