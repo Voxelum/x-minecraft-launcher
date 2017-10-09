4a53cc7551e58366718131ff52d1327eee137f2f
ci010 <cijhn@hotmail.com>
2017-10-08 19:14:36 -0700

    fix modal ui bugs

---
+++ src/renderer/ui/semantic/components/modals/MissingVersionModal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ui basic modal" style="padding:0 20% 0 20%;">
+        <div class="ui icon small header">
+            <i class="user icon"></i>
+            Missing Version
+        </div>
+        <div>
+        </div>
+    </div>
+</template>
+
+<script>
+import vuex from 'vuex'
+
+export default {
+    computed: {
+        ...vuex.mapGetters('auth', ['username'])
+    },
+    mounted() {
+        $(this.$el).modal({ blurring: true })
+    },
+    methods: {
+        show() {
+            $(this.$el).modal('show')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
