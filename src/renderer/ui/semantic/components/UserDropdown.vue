b555c3f12c21c9a7a81aa82b6102482bab511545
ci010 <cijhn@hotmail.com>
2017-10-09 20:55:36 -0700

    modulaize

---
+++ src/renderer/ui/semantic/components/UserDropdown.vue
@@ -0,0 +1,31 @@
+<template>
+    <h5 class="ui pointing dropdown">
+        <i class="user icon"></i>
+        {{username}}
+        <i class="dropdown icon"></i>
+        <div class="menu">
+            <div class="item" @click="$bus.$emit('modal','profile')">
+                <i class="id card outline icon"></i> {{$t('user.profile')}}
+            </div>
+            <div class="item" @click="$bus.$emit('modal','login')">
+                <i class="sign out icon"></i> {{$t('user.logout')}}
+            </div>
+        </div>
+    </h5>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    computed:{
+        ...mapGetters('auth',['username']),
+    },
+    mounted() {
+        $(this.$el).dropdown({ on: 'hover' })
+    },
+}
+</script>
+
+<style>
+
+</style>
