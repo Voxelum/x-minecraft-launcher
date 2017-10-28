839b3da08527679688737ae4fe9f6eecbe3668d5
Phoebe <Phoebe@Phoebes-MacBook-Pro.local>
2017-10-28 16:33:53 -0700

    Select and display Java path

--- src/renderer/ui/semantic/components/LaunchSettings.vue
+++ src/renderer/ui/semantic/components/LaunchSettings.vue
@@ -1,40 +1,62 @@
 <template>
-    <div>
-        <div class="ui segment">
-            Try to display selected java, maybe here? 
-        </div>
-        <div class="ui segment">
-            Here should display the list of existing javas
-        </div>
-        <div class="ui icon button" @click="popDialog">
-            After this button click, it should popup a dialog
-            <i class="add icon"></i>
+    <div class="ui form">
+        <div class="fields">
+                <!-- <div class="ui twox wide field">
+                    <span>Select</span>
+                </div> -->
+                <div class="ui inline field">
+                    <label>Java</label>
+                    <div ref="path" class="ui selection dropdown" style="min-width:400px">
+                        <input type="hidden" name="java path">
+                            <i class="dropdown icon"></i>
+                        <div class="default text">Java path</div>
+                        <div class="menu">
+                            <li class="item" v-for="value in javas" :key="value">
+                                {{value}}
+                            </li>
+                        </div>
+                    </div>
+                    <div class="ui icon button" @click="popDialog">
+                        <i class="add icon"></i>
+                    </div>
+                </div>
         </div>
     </div>
 </template>
 
 <script>
-import vuex from 'vuex'
+import vuex from "vuex";
 
 export default {
-    methods: {
-        ...vuex.mapActions(['addJavas', 'openDialog']),
-        popDialog(event){
-            // @Assign to phoebe, luca
-            // should popup dialog here then collect user selected java path
-            // suppose you get the java path store here: 
-            // const javapath = ...;
-            // you should store java path into vuex's store by: 
-            // this.addJavas(javapath);
-            this.openDialog({}).then((paths)=>{
-                //after user select, the paths should be the paths...
-            })
-        },
-        selectJava(){
-
-        },
+  mounted() {
+    $(this.$refs.path).dropdown({
+      onChange: function(value, text, $selectedItem) {
+        console.log(value);
+        // custom action
+      }
+    });
+  },
+  computed: {
+    ...vuex.mapGetters(["javas"])
+  },
+  methods: {
+    ...vuex.mapActions(["addJavas", "openDialog"]),
+    popDialog(event) {
+      // @Assign to phoebe, luca
+      // should popup dialog here then collect user selected java path
+      // suppose you get the java path store here:
+      // const javapath = ...;
+      // you should store java path into vuex's store by:
+      // this.addJavas(javapath);
+      this.openDialog({}).then(paths => {
+        this.addJavas(paths[0]);
+        console.log(paths[0]);
+        //after user select, the paths should be the paths...
+      });
     },
-}
+    selectJava() {}
+  }
+};
 </script>
 
 <style>
