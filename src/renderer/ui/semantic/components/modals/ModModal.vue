f3e498708934f96f18e86ddf19fd77eb33af2eba
Phoebe <Phoebe@Phoebes-MacBook-Pro.local>
2017-11-04 16:23:54 -0700

    list

--- src/renderer/ui/semantic/components/modals/ModModal.vue
+++ src/renderer/ui/semantic/components/modals/ModModal.vue
@@ -1,40 +1,40 @@
 <template>
     <div class="ui modal">
-    <i class="close icon"></i>
-    <div class="header">
-        Profile Picture
-    </div>
-    <div class="image content">
-        <!-- <div class="ui medium image">
-        <img src="/images/avatar/large/chris.jpg">
-        </div> -->
-        <!-- <i class="circle icon"></i> -->
-        <!-- <div class="description">
-        <div class="ui header">We've auto-chosen a profile image for you.</div>
-        <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
-        </div> -->
-        <div ref = 'a' class="ui accordion">
-            <div class="title">
-            <i class="dropdown icon"></i>
-            A
-            </div>
-            <div class="content">
-            <p class="transition hidden"> aaa </p>
-            </div>  
+        <i class="close icon"></i>
+        <div class="header">
+            Profile Picture
         </div>
-        
+        <div class="image content">
 
-    </div>
-    <div class="actions">
-        <div class="ui black deny button">
-        Cancel
+            <div class="ui list">
+                <div class="item">comment</div>
+
+                <div ref='a' class="ui accordion">
+                    <div class="title">
+                        <i class="dropdown icon"></i>
+                        properties
+                    </div>
+                    <div class="content">
+                        <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 7 </p>
+                        <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 6 </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <li v-for="cate in Object.keys(categories)" :key="cate">
+                    {{ categories[cate] }}
+                </li> -->
+
         </div>
-        <div class="ui positive right labeled icon button">
-        Save
-        <i class="checkmark icon"></i>
+        <div class="actions">
+            <div class="ui black deny button">
+                Cancel
+            </div>
+            <div class="ui positive right labeled icon button">
+                Save
+                <i class="checkmark icon"></i>
+            </div>
         </div>
-    </div>
     </div>
 </template>
 
@@ -42,13 +42,34 @@
 export default {
     methods: {
         show() {
-            $(this.$el).modal('show')
+            $(this.$el).modal("show");
         }
     },
     mounted() {
-        $(this.$refs.a).accordion({})
-    }
-}
+        $(this.$refs.a).accordion({});
+    },
+    data: () => ({
+        categories: {
+            cateA: {
+                comment: "lll",
+                properties: [
+                    {
+                        name: "hp",
+                        type: "I",
+                        comment: "hp",
+                        value: 7
+                    },
+                    {
+                        name: "hp",
+                        type: "I",
+                        comment: "hp",
+                        value: 6
+                    }
+                ]
+            }
+        }
+    })
+};
 </script>
 
 <style>
