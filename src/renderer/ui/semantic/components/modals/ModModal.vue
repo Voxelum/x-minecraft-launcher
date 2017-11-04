db6a757aa6b92e28cc4301b98ae9a0569d01e504
xinhai xiong <xinhaixiong@xinhais-MacBook-Pro.local>
2017-11-04 15:12:05 -0700

    Move loc
    Former-commit-id: 1baebf5b0b663ca1d30f265c46bae949c0ccb014

---
+++ src/renderer/ui/semantic/components/modals/ModModal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="ui modal">
+    <i class="close icon"></i>
+    <div class="header">
+        Profile Picture
+    </div>
+    <div class="image content">
+        <div class="ui medium image">
+        <img src="/images/avatar/large/chris.jpg">
+        </div>
+        <div class="description">
+        <div class="ui header">We've auto-chosen a profile image for you.</div>
+        <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
+        <p>Is it okay to use this photo?</p>
+        </div>
+    </div>
+    <div class="actions">
+        <div class="ui black deny button">
+        Nope
+        </div>
+        <div class="ui positive right labeled icon button">
+        Yep, that's me
+        <i class="checkmark icon"></i>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        show() {
+            $(this.$el).modal('show')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
