26c0f1a2eb97357c9789d855f30d0435ce093c80
Phoebe <Phoebe@Phoebes-MacBook-Pro.local>
2017-11-04 17:27:49 -0700

    categories

--- src/renderer/ui/semantic/components/modals/ModModal.vue
+++ src/renderer/ui/semantic/components/modals/ModModal.vue
@@ -4,45 +4,68 @@
         <div class="header">
             Profile Picture
         </div>
-            <div class="image content">
+        <div class="image content">
 
-                <div class="ui list">
-                    
-                    <div class="item">comment</div>
-
-                    <div ref='a' class="ui accordion">
-                        <div class="title">
-                            <i class="dropdown icon"></i>
-                            properties
-                        </div>
+            <div class="ui divided items">
+                <li class="item" v-for="cate in Object.keys(categories)" :key="cate">
                         <div class="content">
-                            <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 7 </p>
-                            <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 6 </p>
+                            <h3 class="header">{{cate}}</h3>
+                            <div class="item">comment: </div>
+                            {{categories[cate].comment}}
+                            <div ref='a' class="ui accordion">
+                                <div class="title">
+                                    <i class="dropdown icon"></i>
+                                    properties
+
+                                </div>
+                                <div class="content">
+                                    <li v-for="item in Object.values(categories[cate].properties)" :key="item">
+                                        {{item.name}}
+                                    </li>
+
+                                    <!-- <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 7 </p>
+                                                            <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 6 </p> -->
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <!-- <li v-for="cate in Object.keys(categories)" :key="cate">
-                        {{ categories[cate] }}
-                    </li> -->
+                </li>
+            </div>
 
+            <!-- <div class="item">comment</div> -->
+
+            <!-- <div ref='a' class="ui accordion">
+                                                    <div class="title">
+                                                        <i class="dropdown icon"></i>
+                                                        properties
+
+                                                    </div>
+                                                    <div class="content">
+                                                        <!-- <li v-for="cate in Object.keys(categories)" :key="cate">
+                                                                {{ categories[cate].properties[0].comment }}
+                                                            </li> -->
+            <!-- <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 7 </p>
+                                                                <p class="transition hidden"> name: "hp", type: "I", comment: "hp", value: 6 </p> -->
+            <!-- </div>
+                                                </div> -->
+
+        </div>
+        <div class="actions">
+            <div class="ui black deny button">
+                Cancel
             </div>
-            <div class="actions">
-                <div class="ui black deny button">
-                    Cancel
-                </div>
-                <div class="ui positive right labeled icon button">
-                    Save
-                    <i class="checkmark icon"></i>
-                </div>
+            <div class="ui positive right labeled icon button">
+                Save
+                <i class="checkmark icon"></i>
             </div>
+        </div>
     </div>
 </template>
 
 <script>
 import ModDetail from '../ModDetail'
 export default {
-    components:{
+    components: {
         ModDetail
     },
 
@@ -56,6 +79,23 @@ export default {
     },
     data: () => ({
         categories: {
+            cateB: {
+                comment: "lll",
+                properties: [
+                    {
+                        name: "hp",
+                        type: "I",
+                        comment: "hp",
+                        value: 1
+                    },
+                    {
+                        name: "hp",
+                        type: "I",
+                        comment: "hp",
+                        value: 2
+                    }
+                ]
+            },
             cateA: {
                 comment: "lll",
                 properties: [
