b1ede8c659be72695ad8ffe05310b3f102713bf6
CI010 <cijhn@hotmail.com>
2017-11-09 15:41:05 -0800

    update game setting

--- src/renderer/ui/semantic/components/GameSettings/GameSettings.vue
+++ src/renderer/ui/semantic/components/GameSettings/GameSettings.vue
@@ -2,6 +2,9 @@
     <div class="">
         <h5 class="ui horizontal divider header">Critical graphic</h5>
         <options-group :options="criticalGraphics" :id="id"></options-group>
+        <hot-key-button :initkey="hotKeys.moveForward"  @keychange="onkeychange"></hot-key-button>
+        <hot-key-button initkey="O"></hot-key-button>
+        <hot-key-button initkey="O"></hot-key-button>
     </div>
 </template>
 
@@ -9,16 +12,23 @@
 import vuex from 'vuex'
 import OptionMenu from './OptionMenu.vue'
 import OptionsGroup from './OptionsGroup'
+import HotKeyButton from './HotKeyButton'
 
 const boolOptions = [true, false]
 const numOptions = [0, 1, 2]
 
 export default {
-    components: { OptionsGroup },
+    components: { OptionsGroup, HotKeyButton },
     computed: {
         ...vuex.mapGetters({
             templates: 'options',
         }),
+    },
+    methods: {
+        onkeychange(newKeyValue) {
+            this.hotKeys.moveForward = newKeyValue;
+            console.log(`Get new key ${newKeyValue}`)
+        }
     },
     data() {
         return {
@@ -36,7 +46,10 @@ export default {
             },
             normalGraphics: {
 
-            }
+            },
+            hotKeys: {
+                moveForward: 'w'
+            },
         }
     },
     props: ['id'],
