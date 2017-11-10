e254267d4f2eaff653ae59a31514d795ae480f02
Sumeng Wang <suwang@ucsc.edu>
2017-11-09 18:04:55 -0800

    Mcmod Ui design

--- src/renderer/ui/semantic/components/McmodCn.vue
+++ src/renderer/ui/semantic/components/McmodCn.vue
@@ -1,5 +1,117 @@
 <template>
-  
+    <div class="ui link cards">
+        <div class="card">
+            <div class="image">
+                <img src="/pages/class/images/cover/20170619/1497862552_14115_zevr.jpg">
+            </div>
+            <div class="content">
+                <div class="header">Mod Pack 1</div>
+            </div>
+            <div class="like icons">
+                <span>
+                    <i class="fire icon"></i>
+                    hot
+                </span>
+                <span>
+                    <i class="thumbs up icon"></i>
+                    like
+                </span>
+                <span>
+                    <i class="heart icon"></i>
+                    favorite
+                </span>
+            </div>
+            <div class="ui divider"></div>
+            <div class="item icons">
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="image">
+                <img src="/pages/class/images/cover/20170619/1497862552_14115_zevr.jpg">
+            </div>
+            <div class="content">
+                <div class="header">Mod Pack 2</div>
+            </div>
+            <div class="like icons">
+                <span>
+                    <i class="fire icon"></i>
+                    hot
+                </span>
+                <span>
+                    <i class="thumbs up icon"></i>
+                    like
+                </span>
+                <span>
+                    <i class="heart icon"></i>
+                    favorite
+                </span>
+            </div>
+            <div class="ui divider"></div>
+            <div class="item icons">
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+            </div>
+        </div>
+        <div class="card">
+            <div class="image">
+                <img src="/pages/class/images/cover/20170619/1497862552_14115_zevr.jpg">
+            </div>
+            <div class="content">
+                <div class="header">Mod Pack 3</div>
+            </div>
+            <div class="like icons">
+                <span>
+                    <i class="fire icon"></i>
+                    hot
+                </span>
+                <span>
+                    <i class="thumbs up icon"></i>
+                    like
+                </span>
+                <span>
+                    <i class="heart icon"></i>
+                    favorite
+                </span>
+            </div>
+            <div class="ui divider"></div>
+            <div class="item icons">
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+                <span class="image">
+                    <img src="/images/avatar2/large/matthew.png">
+                </span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
