fc0e66eb7900423ce6f775c4d0954265207bae62
ci010 <cijhn@hotmail.com>
2017-11-11 16:49:32 -0800

    rename

---
+++ src/main/services/mcmod.js
@@ -0,0 +1,12 @@
+import {
+    net,
+} from 'electron'
+import querystring from 'querystring'
+import parser from 'fast-html-parser'
+
+export default {
+    initialize() { },
+    proxy: {},
+    actions: {
+    },
+}
