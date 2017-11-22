355c92b5978dfd68fd4060c382495c4b31fc3d40
ci010 <cijhn@hotmail.com>
2017-11-22 14:55:03 -0800

    i18n eg
    Former-commit-id: a0a99e53f99d1edb14cf10f23c015b25470db6fe

--- src/renderer/ui/semantic/components/modals/MissingVersionModal.vue
+++ src/renderer/ui/semantic/components/modals/MissingVersionModal.vue
@@ -2,7 +2,7 @@
     <div class="ui basic modal" style="padding:0 20% 0 20%;">
         <div class="ui icon small header">
             <i class="user icon"></i>
-            Missing Version
+            {{$t('missingversion')}}
         </div>
         <div>
         </div>
