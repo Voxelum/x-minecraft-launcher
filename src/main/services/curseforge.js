7a0632a48fcd834aefc9f2b15019196d5bad70c1
CI010 <cijhn@hotmail.com>
2017-11-23 00:40:58 -0800

    migrate the cache

--- src/main/services/curseforge.js
+++ src/main/services/curseforge.js
@@ -1,26 +1,12 @@
-import { net, app, webContents } from 'electron'
 import querystring from 'querystring'
 import parser from 'fast-html-parser'
+
+import request from '../html-request'
 
 function localDate(string) {
     const d = new Date(0)
     d.setUTCSeconds(Number.parseInt(string, 10))
     return d.toLocaleDateString()
-}
-
-function request(endpoint) {
-    return new Promise((resolve, reject) => {
-        let s = ''
-        const req = net.request(endpoint)
-        req.on('response', (msg) => {
-            msg.on('data', (b) => { s += b.toString() })
-            msg.on('end', () => {
-                resolve(s)
-            })
-        })
-        req.on('error', e => reject(e))
-        req.end()
-    });
 }
 
 function convert(node) {
@@ -49,23 +35,10 @@ function convert(node) {
     return text
 }
 
-let cached = {}
-
-function cache(url, data) {
-    cached[url] = data;
-    setTimeout(() => {
-        delete cached[url];
-    }, 60000);
-    return data;
-}
-
 export default {
     initialize() {
     },
     actions: {
-        clearCache() {
-            cached = {}
-        },
         /**
          * Get the license in url
          * @param {string} url
@@ -90,31 +63,32 @@ export default {
             const url = `
             https://minecraft.curseforge.com${path}/files?filter-game-version=${version}&page=${page}
             `
-            if (cached[url]) return cached[url];
-            const filespage = parser.parse(await request(url))
-            const pages = filespage.querySelectorAll('.b-pagination-item')
-                .map(pageItem => pageItem.firstChild.rawText)
-                .map(text => Number.parseInt(text, 10))
-                .filter(n => Number.isInteger(n))
-                .reduce((a, b) => (a > b ? a : b))
-            const versions = filespage.querySelector('#filter-game-version').removeWhitespace()
-                .childNodes.map(ver => ({
-                    type: ver.attributes.class,
-                    text: ver.rawText,
-                    value: ver.attributes.value,
-                }))
-            const files = filespage.querySelectorAll('.project-file-list-item')
-                .map(i => i.removeWhitespace())
-                .map(i => ({
-                    type: i.firstChild.firstChild.attributes.title,
-                    name: i.childNodes[1].firstChild.childNodes[1].firstChild.rawText,
-                    href: i.childNodes[1].firstChild.firstChild.firstChild.attributes.href,
-                    size: i.childNodes[2].rawText,
-                    date: localDate(i.childNodes[3].firstChild.attributes['data-epoch']),
-                    version: i.childNodes[4].firstChild.rawText,
-                    downloadCount: i.childNodes[5].rawText,
-                }))
-            return cache(url, { pages, versions, files });
+            const parse = (filespage) => {
+                const pages = filespage.querySelectorAll('.b-pagination-item')
+                    .map(pageItem => pageItem.firstChild.rawText)
+                    .map(text => Number.parseInt(text, 10))
+                    .filter(n => Number.isInteger(n))
+                    .reduce((a, b) => (a > b ? a : b))
+                const versions = filespage.querySelector('#filter-game-version').removeWhitespace()
+                    .childNodes.map(ver => ({
+                        type: ver.attributes.class,
+                        text: ver.rawText,
+                        value: ver.attributes.value,
+                    }))
+                const files = filespage.querySelectorAll('.project-file-list-item')
+                    .map(i => i.removeWhitespace())
+                    .map(i => ({
+                        type: i.firstChild.firstChild.attributes.title,
+                        name: i.childNodes[1].firstChild.childNodes[1].firstChild.rawText,
+                        href: i.childNodes[1].firstChild.firstChild.firstChild.attributes.href,
+                        size: i.childNodes[2].rawText,
+                        date: localDate(i.childNodes[3].firstChild.attributes['data-epoch']),
+                        version: i.childNodes[4].firstChild.rawText,
+                        downloadCount: i.childNodes[5].rawText,
+                    }))
+                return { pages, versions, files };
+            }
+            return request(url, parse);
         },
         /**
          * Query the project from path
@@ -124,49 +98,52 @@ export default {
         async project(context, path) {
             if (!path || path == null) throw new Error('Curseforge path cannot be null')
             const url = `https://minecraft.curseforge.com${path}`
-            if (cached[url]) return cached[url];
-            const root = parser.parse(await request(url));
-            const descontent = root.querySelector('.project-description')
-            const description = convert(descontent)
-            const details = root.querySelector('.project-details').removeWhitespace()
-            const createdDate = localDate(details.childNodes[1].childNodes[1].firstChild.attributes['data-epoch'])
-            const lastFile = localDate(details.childNodes[2].childNodes[1].firstChild.attributes['data-epoch'])
-            const totalDownload = details.childNodes[3].childNodes[1].rawText
-            const license = details.childNodes[4].childNodes[1].firstChild.attributes.href;
 
-            const projWrap = root.querySelector('.project-user').removeWhitespace()
-            const image = projWrap.firstChild.firstChild.attributes.href;
-            const name = projWrap.childNodes[1].firstChild.rawText;
+            const parse = (root) => {
+                const descontent = root.querySelector('.project-description')
+                const description = convert(descontent)
+                const details = root.querySelector('.project-details').removeWhitespace()
+                const createdDate = localDate(details.childNodes[1].childNodes[1].firstChild.attributes['data-epoch'])
+                const lastFile = localDate(details.childNodes[2].childNodes[1].firstChild.attributes['data-epoch'])
+                const totalDownload = details.childNodes[3].childNodes[1].rawText
+                const license = details.childNodes[4].childNodes[1].firstChild.attributes.href;
 
-            const files = root.querySelectorAll('.file-tag')
-                .map((f) => {
-                    f = f.removeWhitespace();
-                    const typeClass = f.firstChild.firstChild.attributes.class;
-                    let type = 'unknonwn'
-                    if (typeClass.includes('release')) type = 'release'
-                    else if (typeClass.includes('alpha')) type = 'alpha'
-                    else if (typeClass.includes('beta')) type = 'beta'
-                    const href = f.childNodes[1].firstChild.attributes.href;
-                    const fname = f.childNodes[1].childNodes[1].rawText;
-                    const date = localDate(f.childNodes[1].childNodes[2].attributes['data-epoch'])
-                    return {
-                        type,
-                        href,
-                        name: fname,
-                        date,
-                    }
-                });
-            return cache(url, {
-                image,
-                name,
-                createdDate,
-                lastFile,
-                totalDownload,
-                license,
-                description,
-                downloads: {},
-                // files,
-            })
+                const projWrap = root.querySelector('.project-user').removeWhitespace()
+                const image = projWrap.firstChild.firstChild.attributes.href;
+                const name = projWrap.childNodes[1].firstChild.rawText;
+
+                const files = root.querySelectorAll('.file-tag')
+                    .map((f) => {
+                        f = f.removeWhitespace();
+                        const typeClass = f.firstChild.firstChild.attributes.class;
+                        let type = 'unknonwn'
+                        if (typeClass.includes('release')) type = 'release'
+                        else if (typeClass.includes('alpha')) type = 'alpha'
+                        else if (typeClass.includes('beta')) type = 'beta'
+                        const href = f.childNodes[1].firstChild.attributes.href;
+                        const fname = f.childNodes[1].childNodes[1].rawText;
+                        const date = localDate(f.childNodes[1].childNodes[2].attributes['data-epoch'])
+                        return {
+                            type,
+                            href,
+                            name: fname,
+                            date,
+                        }
+                    });
+                return {
+                    image,
+                    name,
+                    createdDate,
+                    lastFile,
+                    totalDownload,
+                    license,
+                    description,
+                    downloads: {},
+                    // files,
+                }
+            }
+
+            return request(url, parse);
         },
         /**
          * 
@@ -180,61 +157,63 @@ export default {
                 'filter-sort': sort || 'popularity',
                 'filter-game-version': version || '',
             })}`
-            if (cached[endpoint]) return cached[endpoint];
-            const s = await request(endpoint)
-            const root = parser.parse(s.replace(/\r\n/g, ''));
-            const pages = root.querySelectorAll('.b-pagination-item')
-                .map(pageItem => pageItem.firstChild.rawText)
-                .map(text => Number.parseInt(text, 10))
-                .filter(n => Number.isInteger(n))
-                .reduce((a, b) => (a > b ? a : b))
-            const versions = root.querySelector('#filter-game-version').removeWhitespace()
-                .childNodes.map(ver => ({
-                    type: ver.attributes.class,
-                    text: ver.rawText,
-                    value: ver.attributes.value,
-                }))
-            const filters = root.querySelector('#filter-sort').removeWhitespace()
-                .childNodes.map(f => ({
-                    text: f.rawText,
-                    value: f.attributes.value,
-                }))
-            const all = root.querySelectorAll('.project-list-item').map((item) => {
-                item = item.removeWhitespace();
-                const noText = n => !(n instanceof parser.TextNode)
-                const [avatar, details] = item.childNodes
-                let icon
-                try {
-                    icon = avatar.firstChild.firstChild.attributes.src
-                } catch (e) {
-                    icon = ''
-                }
-                const path = avatar.firstChild.attributes.href;
-                let [name, status, categories, description] = details.childNodes;
-                const author = name.lastChild.lastChild.firstChild.rawText
-                name = name.firstChild.firstChild.rawText
-                const count = status.firstChild.firstChild.rawText
-                const date = localDate(status.lastChild.firstChild.attributes['data-epoch']);
-                status = {}
-                description = description.firstChild.rawText;
-                categories = categories.firstChild.childNodes.map((ico) => {
-                    const ca = {
-                        href: ico.firstChild.attributes.href,
-                        icon: ico.firstChild.firstChild.attributes.src,
+            const parse = (root) => {
+                root = root.removeWhitespace();
+                const pages = root.querySelectorAll('.b-pagination-item')
+                    .map(pageItem => pageItem.firstChild.rawText)
+                    .map(text => Number.parseInt(text, 10))
+                    .filter(n => Number.isInteger(n))
+                    .reduce((a, b) => (a > b ? a : b))
+                const versions = root.querySelector('#filter-game-version').removeWhitespace()
+                    .childNodes.map(ver => ({
+                        type: ver.attributes.class,
+                        text: ver.rawText,
+                        value: ver.attributes.value,
+                    }))
+                const filters = root.querySelector('#filter-sort').removeWhitespace()
+                    .childNodes.map(f => ({
+                        text: f.rawText,
+                        value: f.attributes.value,
+                    }))
+                const all = root.querySelectorAll('.project-list-item').map((item) => {
+                    item = item.removeWhitespace();
+                    const noText = n => !(n instanceof parser.TextNode)
+                    const [avatar, details] = item.childNodes
+                    let icon
+                    try {
+                        icon = avatar.firstChild.firstChild.attributes.src
+                    } catch (e) {
+                        icon = ''
                     }
-                    return ca
+                    const path = avatar.firstChild.attributes.href;
+                    let [name, status, categories, description] = details.childNodes;
+                    const author = name.lastChild.lastChild.firstChild.rawText
+                    name = name.firstChild.firstChild.rawText
+                    const count = status.firstChild.firstChild.rawText
+                    const date = localDate(status.lastChild.firstChild.attributes['data-epoch']);
+                    status = {}
+                    description = description.firstChild.rawText;
+                    categories = categories.firstChild.childNodes.map((ico) => {
+                        const ca = {
+                            href: ico.firstChild.attributes.href,
+                            icon: ico.firstChild.firstChild.attributes.src,
+                        }
+                        return ca
+                    })
+                    return {
+                        path: path.substring(path.lastIndexOf('/') + 1), name, author, description, date, count, categories, icon,
+                    };
                 })
                 return {
-                    path: path.substring(path.lastIndexOf('/') + 1), name, author, description, date, count, categories, icon,
-                };
-            })
+                    mods: all,
+                    pages,
+                    versions,
+                    filters,
+                }
+            }
 
-            return cache(endpoint, {
-                mods: all,
-                pages,
-                versions,
-                filters,
-            })
+
+            return request(endpoint, parse);
         },
     },
 }
