5aaaae51785db2043321d238b66083126986630a
ci010 <cijhn@hotmail.com>
2017-11-28 16:54:18 -0800

    fix $on
    Former-commit-id: 5aef2814d6ad7a15ff2378f7134b434a2dd5b0b3

--- src/renderer/ui/semantic/components/home/CardView/index.js
+++ src/renderer/ui/semantic/components/home/CardView/index.js
@@ -11,7 +11,7 @@ export default {
     },
     created() {
         this.refresh(false)
-        this.$ipc.$on('refresh', this.refresh)
+        this.$ipc.on('refresh', this.refresh)
     },
     methods: {
         refresh(force = true) { this.ids.forEach(id => this.$store.dispatch(`profiles/${id}/refresh`, force)) },
