19628bcdefd06e4fc1fd544933148e9c49c0ce8d
xinhai xiong <xiongxinhai@gmail.com>
2017-12-01 16:26:26 -0800

    another web page unit test for curseforge project

--- test/unit/curseforge.js
+++ test/unit/curseforge.js
@@ -19,3 +19,21 @@ describe('Fetch detail', () => {
             .catch((e) => { done(e) })
     })
 })
+
+describe('Fetch detail', () => {
+    it('should test project.image is not null and a valid string', (done) => {
+        curseforge.actions.project({}, '/mantle')               
+            .then(
+            (data) => {
+                assert.equal(data.image, 'https://media-elerium.cursecdn.com/avatars/thumbnails/5/796/62/62/635351433944342580.png')
+                assert.equal(data.name, 'Mantle')
+                assert.equal(data.createdDate, 'Feb 22, 2014')
+                assert.equal(data.lastFile, 'Sep 1, 2017')
+                assert.equal(data.totalDownload, '23,980,826')
+                assert.equal(data.license, '/projects/mantle/license')
+                assert(typeof description === 'string')
+                done()
+            })
+            .catch((e) => { done(e) })
+    })
+})
