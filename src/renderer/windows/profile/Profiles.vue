d8004a88565013ef0e40f374b39e75086f181c4c
ci010 <cijhn@hotmail.com>
2019-03-29 00:33:01 +0800

    prototype
    Former-commit-id: ea14cdbeeebfac0d0836f79cce7f23360054475b

--- src/renderer/windows/profile/Profiles.vue
+++ src/renderer/windows/profile/Profiles.vue
@@ -1,51 +1,55 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-btn style="position: absolute; right: 10px; bottom: 10px; " flat fab dark large>
-      <v-icon dark>add</v-icon>
-    </v-btn>
-    <v-layout>
-      <v-flex>
-        <v-text-field append-icon="search" label="Search" solo dark color="green darken-1"></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-flex style="height: 100%">
-      <v-layout column style="overflow: scroll;">
-        <v-flex v-for="profile in profiles" :key="profile.id">
-          <v-card class="mx-auto" color="#26c6da" dark @click max-width="400">
-            <v-card-title>
-              <v-icon large left>mdi-twitter</v-icon>
-              <span class="title font-weight-light">{{ profile.name }}</span>
-            </v-card-title>
+	<v-container grid-list-md text-xs-center>
+		<v-layout row>
+			<v-flex xs11>
+				<v-text-field append-icon="search" label="Search" solo dark color="green darken-1"></v-text-field>
+			</v-flex>
+			<v-flex xs1>
+				<v-btn flat fab dark small style="margin-left: 5px; margin-top: 5px;">
+					<v-icon dark style="font-size: 28px">add</v-icon>
+				</v-btn>
+			</v-flex>
+		</v-layout>
+		<!-- <v-flex style="height: 100%"> -->
+		<v-layout column style="overflow: scroll; max-height: 400px;" align-space-around justify-start
+		  fill-height>
+			<v-flex v-for="profile in profiles" :key="profile.id">
+				<v-card class="mx-auto" color="#grey darken-3" dark>
+					<v-card-title>
+						<v-icon large left>layers</v-icon>
+						<span class="title font-weight-light">{{ profile.name }}</span>
+					</v-card-title>
 
-            <v-card-text class="headline font-weight-bold">{{ profile.description }}</v-card-text>
+					<v-card-text class="headline font-weight-bold">{{ profile.description }}</v-card-text>
 
-            <v-card-actions>
-              <v-list-tile class="grow">
-                <v-list-tile-avatar color="grey darken-3">
-                  <v-img
-                    class="elevation-6"
-                    src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                  ></v-img>
-                </v-list-tile-avatar>
+					<v-card-actions style="margin-top: 40px;">
+						<v-list-tile class="grow">
+							<v-list-tile-avatar color="grey darken-3">
+								<v-img class="elevation-6" src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"></v-img>
+							</v-list-tile-avatar>
 
-                <v-list-tile-content>
-                  <v-list-tile-title>Evan You</v-list-tile-title>
-                </v-list-tile-content>
+							<v-list-tile-content>
+								<v-list-tile-title>Evan You</v-list-tile-title>
+							</v-list-tile-content>
 
-                <v-layout align-center justify-end>
-                  <v-icon class="mr-1">mdi-heart</v-icon>
-                  <span class="subheading mr-2">256</span>
-                  <span class="mr-1">·</span>
-                  <v-icon class="mr-1">mdi-share-variant</v-icon>
-                  <span class="subheading">45</span>
-                </v-layout>
-              </v-list-tile>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-flex>
-  </v-container>
+							<v-layout justify-end style="margin-bottom: auto;">
+								<v-btn>
+									<v-icon>delete</v-icon>
+								</v-btn>
+								<v-btn>
+									<v-icon>file_copy</v-icon>
+								</v-btn>
+								<v-btn>
+									<v-icon>check</v-icon>
+								</v-btn>
+							</v-layout>
+						</v-list-tile>
+					</v-card-actions>
+				</v-card>
+			</v-flex>
+		</v-layout>
+		<!-- </v-flex> -->
+	</v-container>
 </template>
 
 <script>
--- src/renderer/windows/profile/Root.vue
+++ src/renderer/windows/profile/Root.vue
@@ -1,118 +1,84 @@
 <template>
-  <v-app
-    style="background: transparent;"
-    @click="mini = !mini"
-  >
-    <v-layout fill-height>
-      <v-navigation-drawer
-        class=""
-        v-model="drawer"
-        :mini-variant.sync="mini"
-        stateless
-        absolute
-        dark
-        style="border-radius: 2px;"
-      >
-        <v-toolbar
-          flat
-          class="transparent"
-        >
-          <v-list class="pa-0">
-            <v-list-tile avatar>
-              <v-list-tile-avatar>
-                <img src="https://randomuser.me/api/portraits/men/100.jpg">
-              </v-list-tile-avatar>
+	<v-app style="background: transparent;">
+		<v-layout fill-height>
+			<v-navigation-drawer width=700px v-model="drawer" :mini-variant.sync="mini" stateless absolute
+			  dark style="border-radius: 2px;" @click="onNaviClicked">
+				<v-toolbar flat class="transparent">
+					<v-list class="pa-0">
+						<v-list-tile avatar>
+							<v-list-tile-avatar>
+								<img src="https://randomuser.me/api/portraits/men/100.jpg">
+							</v-list-tile-avatar>
 
-              <v-list-tile-content>
-                <v-list-tile-title>Username</v-list-tile-title>
-              </v-list-tile-content>
+							<v-list-tile-content>
+								<v-list-tile-title>Username</v-list-tile-title>
+							</v-list-tile-content>
 
-              <v-list-tile-action>
-                <v-btn
-                  icon
-                  @click.stop="mini = !mini"
-                >
-                  <v-icon>chevron_left</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </v-list>
-        </v-toolbar>
-        <v-list>
-          <v-divider></v-divider>
-          <v-list-tile @click="">
-            <v-list-tile-action>
-              <v-icon>home</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile>
+							<v-list-tile-action>
+								<v-btn icon @click.stop="mini = !mini">
+									<v-icon>chevron_left</v-icon>
+								</v-btn>
+							</v-list-tile-action>
+						</v-list-tile>
+					</v-list>
+				</v-toolbar>
+				<v-list>
+					<v-divider></v-divider>
+					<v-list-tile @click="">
+						<v-list-tile-action>
+							<v-icon>home</v-icon>
+						</v-list-tile-action>
+						<v-list-tile-title>Home</v-list-tile-title>
+					</v-list-tile>
 
-          <v-list-group
-            prepend-icon="account_circle"
-            v-model="openBinding[0]"
-          >
-            <template v-slot:activator>
-              <v-list-tile>
-                <v-list-tile-title>Users</v-list-tile-title>
-              </v-list-tile>
-            </template>
-            <v-list-group
-              no-action
-              sub-group
-              v-model="openBinding[1]"
-            >
-              <template v-slot:activator>
-                <v-list-tile>
-                  <v-list-tile-title>Admin</v-list-tile-title>
-                </v-list-tile>
-              </template>
+					<v-list-group prepend-icon="account_circle" v-model="openBinding[0]">
+						<template v-slot:activator>
+							<v-list-tile>
+								<v-list-tile-title>Users</v-list-tile-title>
+							</v-list-tile>
+						</template>
+						<v-list-group no-action sub-group v-model="openBinding[1]">
+							<template v-slot:activator>
+								<v-list-tile>
+									<v-list-tile-title>Admin</v-list-tile-title>
+								</v-list-tile>
+							</template>
 
-              <v-list-tile
-                v-for="(admin, i) in admins"
-                :key="i"
-                @click=""
-              >
-                <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
-                <v-list-tile-action>
-                  <v-icon v-text="admin[1]"></v-icon>
-                </v-list-tile-action>
-              </v-list-tile>
-            </v-list-group>
+							<v-list-tile v-for="(admin, i) in admins" :key="i" @click="">
+								<v-list-tile-title v-text="admin[0]"></v-list-tile-title>
+								<v-list-tile-action>
+									<v-icon v-text="admin[1]"></v-icon>
+								</v-list-tile-action>
+							</v-list-tile>
+						</v-list-group>
 
-            <v-list-group
-              sub-group
-              no-action
-              v-model="openBinding[2]"
-            >
-              <template v-slot:activator>
-                <v-list-tile>
-                  <v-list-tile-title>Actions</v-list-tile-title>
-                </v-list-tile>
-              </template>
-              <v-list-tile
-                v-for="(crud, i) in cruds"
-                :key="i"
-                @click=""
-              >
-                <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
-                <v-list-tile-action>
-                  <v-icon v-text="crud[1]"></v-icon>
-                </v-list-tile-action>
-              </v-list-tile>
-            </v-list-group>
-          </v-list-group>
-        </v-list>
-      </v-navigation-drawer>
-      <v-layout style="padding: 20px 0px 20px 0px; background: transparent; max-height: 100vh;" fill-height>
-        <div style="width: 100px;"></div>
+						<v-list-group sub-group no-action v-model="openBinding[2]">
+							<template v-slot:activator>
+								<v-list-tile>
+									<v-list-tile-title>Actions</v-list-tile-title>
+								</v-list-tile>
+							</template>
+							<v-list-tile v-for="(crud, i) in cruds" :key="i" @click="">
+								<v-list-tile-title v-text="crud[0]"></v-list-tile-title>
+								<v-list-tile-action>
+									<v-icon v-text="crud[1]"></v-icon>
+								</v-list-tile-action>
+							</v-list-tile>
+						</v-list-group>
+					</v-list-group>
+				</v-list>
+			</v-navigation-drawer>
+			<v-layout style="padding: 20px 0px 20px 0px; background: transparent; max-height: 100vh;"
+			  fill-height>
+				<div style="width: 100px;"></div>
 
-        <v-card style="width: 100%;" color="grey darken-4">
-          <router-view></router-view>
-        </v-card>
+				<v-card style="width: 100%;" color="grey darken-4">
+					<router-view></router-view>
+				</v-card>
 
-      </v-layout>
-    </v-layout>
-  </v-app>
+			</v-layout>
+		</v-layout>
+	</v-app>
 </template>
 
 <script>
@@ -156,6 +122,9 @@ export default {
   methods: {
     close() {
       this.$store.dispatch('exit');
+    },
+    onNaviClicked() {
+			console.log('click!')
     },
   },
 }
