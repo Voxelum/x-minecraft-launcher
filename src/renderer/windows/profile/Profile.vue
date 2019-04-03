726b4399222c7dc0527b7039a0cd616ef23bffb2
ci010 <cijhn@hotmail.com>
2019-04-03 18:43:04 +0800

    add version menu and error menu
    Former-commit-id: 3211ff80790e60e7df814ae4953b43bad8abf849

--- src/renderer/windows/profile/Profile.vue
+++ src/renderer/windows/profile/Profile.vue
@@ -8,17 +8,48 @@
 			<v-icon dark>share</v-icon>
 		</v-btn>
 
+		<v-menu top dark full-width>
+			<template v-slot:activator="{ on }">
+				<v-btn style="position: absolute; left: 130px; bottom: 10px; " :flat="problems.length !== 0"
+				  outline dark :color="problems.length !== 0 ? 'red' : 'white' " v-on="on">
+					<v-icon left dark :color="problems.length !== 0 ? 'red': 'white'">{{problems.length !== 0 ?
+						'warning' : 'check_circle'}}</v-icon>
+					{{$tc('diagnosis.problem', problems.length, {count: problems.length})}}
+				</v-btn>
+			</template>
+
+			<v-list>
+				<template v-for="(item, index) in problems">
+					<v-list-tile ripple :key="index" @click="handleError(item)">
+						<v-list-tile-content>
+							<v-list-tile-title>
+								{{ item.title }}
+							</v-list-tile-title>
+							<v-list-tile-sub-title>
+								{{item.autofix ? 'click to fix this problem': 'pleace manually fix this'}}
+							</v-list-tile-sub-title>
+						</v-list-tile-content>
+						<v-list-tile-action>
+							<v-icon> {{item.autofix ? 'build' : 'arrow_right'}} </v-icon>
+						</v-list-tile-action>
+					</v-list-tile>
+				</template>
+			</v-list>
+		</v-menu>
+
 		<div class="display-1 white--text" style="padding-top: 50px; padding-left: 50px">
 			{{profile.name}}
-			<v-chip label color="green" outline small :selected="false"> 
-				{{profile.author}}
-			</v-chip>
 			<v-chip label color="green" outline small :selected="false">
-				{{profile.mcversion}}
+				{{profile.author || 'Unknown'}}
 			</v-chip>
+			<version-menu ref="menu">
+				<v-chip label color="green" outline small :selected="false" @click="$refs.menu.open()">
+					{{profile.mcversion}}
+				</v-chip>
+			</version-menu>
 		</div>
 		<v-btn color="grey darken-1" style="position: absolute; right: 10px; bottom: 10px; " dark large
-		  @click="launch">Launch</v-btn>
+		  :disabled="problems.length !== 0" @click="launch">Launch</v-btn>
 		<export-dialog :dialog="exportDialog"></export-dialog>
 	</v-layout>
 </template>
@@ -29,9 +60,13 @@ export default {
     exportDialog: false,
   }),
   computed: {
-    profile() { return this.$repo.getters['profile/current'] }
+    profile() { return this.$repo.getters['profile/current'] },
+    problems() {
+      return this.profile.errors.map((e) => ({ id: e.id, autofix: e.autofix, title: this.$t(`diagnosis.${e.id}`, e.arguments || {}) }))
+    },
   },
   mounted() {
+    this.$repo.dispatch('profile/diagnose');
   },
   watch: {
   },
@@ -45,9 +80,24 @@ export default {
     goExport() {
       this.exportDialog = true;
     },
+    handleError(error) {
+      if (!error.autofix) {
+        switch (error.id) {
+          case 'missingVersion':
+            this.$router.push('setting');
+            break;
+          case 'missingJava':
+            this.$router.push('setting');
+            break;
+        }
+      } else {
+
+      }
+    },
   },
   components: {
     ExportDialog: () => import('./ExportDialog'),
+    VersionMenu: () => import('./VersionMenu'),
   },
 }
 </script>
