f4846e33bca346b0439e792b943ffd4bf519242d
ci010 <cijhn@hotmail.com>
2019-04-11 00:24:17 +0800

    User prototype
    Former-commit-id: b16c5fc4ed6cd419d901e6a679c57be0fd3e8de6

--- src/renderer/windows/profile/User.vue
+++ src/renderer/windows/profile/User.vue
@@ -1,21 +1,71 @@
 <template>
-	<v-container fill-height>
-		<v-layout align-center justify-center column fill-height>
+	<v-container fluid grid-list-md>
+		<v-layout align-start justify-center row fill-height>
 			<v-flex>
-				<div class="display-1 white--text">
-					{{username}}
-				</div>
+				<v-layout row fill-height>
+					<v-flex>
+						<v-card dark>
+							<skin-view :data="skin"></skin-view>
+						</v-card>
+						<v-flex>
+							<v-btn dark block>
+								Change Skin
+								<v-icon right dark>accessibility_new</v-icon>
+							</v-btn>
+							<v-btn dark block>
+								Reset
+								<v-icon right dark>undo</v-icon>
+							</v-btn>
+							<v-btn dark block>
+								Upload Skin
+								<v-icon right dark>cloud_upload</v-icon>
+							</v-btn>
+						</v-flex>
+					</v-flex>
+				</v-layout>
 			</v-flex>
 			<v-flex>
-				<v-card dark>
-					<skin-view :data="skin"></skin-view>
-				</v-card>
-			</v-flex>
-			<v-flex>
-				<v-btn outline color="primary">Switch Account</v-btn>
-			</v-flex>
-			<v-flex>
-				<v-btn outline color="red">Logout</v-btn>
+				<v-layout align-start column>
+					<v-flex>
+						<v-chip label dark outline color="primary">
+							Username:
+						</v-chip>
+						<span class="white--text text-lg-right">
+							{{username}}
+						</span>
+					</v-flex>
+					<v-flex>
+						<v-chip label dark outline color="primary">
+							AccessToken:
+						</v-chip>
+						<span class="white--text text-lg-right">
+							{{accessToken}}
+						</span>
+					</v-flex>
+					<v-flex>
+						<v-chip label dark outline color="primary">
+							Authorize Service:
+						</v-chip>
+						<span class="white--text text-lg-right">
+							{{authMode}}
+						</span>
+					</v-flex>
+					<v-flex>
+						<v-chip label dark outline color="primary">
+							Profile Service:
+						</v-chip>
+						<span class="white--text text-lg-right">
+							{{profileMode}}
+						</span>
+					</v-flex>
+
+					<v-flex offset-sm3>
+						<v-btn block color="primary">Switch Account</v-btn>
+					</v-flex>
+					<v-flex>
+						<v-btn block dark color="red">Logout</v-btn>
+					</v-flex>
+				</v-layout>
 			</v-flex>
 		</v-layout>
 	</v-container>
