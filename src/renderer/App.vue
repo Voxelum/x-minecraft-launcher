e6542bf05b94b4641a6e6bf3fe623308b26d08a8
ci010 <cijhn@hotmail.com>
2019-04-19 16:30:40 +0800

    support clipboard
    Former-commit-id: 2c60cbbc4039989f69c1a98dfb8a12a9ac30ef22

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,18 +1,36 @@
 <template>
-    <router-view></router-view>
+    <div>
+        <router-view></router-view>
+        <textarea readonly style="position:absolute;left:-9999px;" ref="clipboard"></textarea>
+    </div>
 </template>
 
 <script>
 
 import 'vuetify/dist/vuetify.css';
 import '@/assets/google.font.css';
+import Vue from 'vue';
 
 export default {
     computed: {
     },
     beforeMount() {
     },
+    created(){
+        this.$electron.ipcRenderer.on('copy', (text) =>{
+            const clipboard = this.$refs.clipboard;
+            clipboard.value = text;
+            clipboard.select();
+            document.execCommand('copy');
+        });
+    },
     mounted() {
+        Vue.prototype.$copy = (text) => {
+            const clipboard = this.$refs.clipboard;
+            clipboard.value = text;
+            clipboard.select();
+            document.execCommand('copy');
+        }
     },
     methods: {
     },
