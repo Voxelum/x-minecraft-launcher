3c4a0337f2ea7238bb7c6dc049321ea2369da66f
ci010 <cijhn@hotmail.com>
2019-04-30 18:15:31 +0800

    improve card style;
    Former-commit-id: 680ba7eb946d7a303aed60f757ca8857a44a1c76

--- src/renderer/windows/profile/ResourcePackCard.vue
+++ src/renderer/windows/profile/ResourcePackCard.vue
@@ -1,35 +1,21 @@
 <template>
-	<v-card color="grey darken-1" hover class="resource-pack-card white--text" flat style="margin-top: 10px;" draggable
-	  @dragstart="onDragStart">
-		<v-layout fill-height>
-			<v-flex style="padding: 0;">
-				<img style="user-drag: none; user-select: none; height: 125px;" :src="data.icon" contain>
-			</v-flex>
-			<v-flex style="padding: 10px;">
-				<text-component :source="data.packName" class="title"></text-component>
-				<text-component :source="data.description"></text-component>
-			</v-flex>
-			<!-- <div style="position: absolute; z-index: 100">
-				<v-btn color="green" style="position: absolute; left: 10px;" v-if="isSelected && !noup" @click="moveup"
-				  icon>
-					<v-icon>arrow_drop_up</v-icon>
-				</v-btn>
-				<v-btn color="green" style="position: absolute; left: 10px; top: 80px;" v-if="isSelected & !nodown"
-				  @click="movedown" icon>
-					<v-icon>arrow_drop_down</v-icon>
-				</v-btn>
-				<v-btn color="green" style="position: absolute; left: 70px; top: 40px" @click="trigger" icon
-				  v-if="isSelected">
-					<v-icon>arrow_left</v-icon>
-				</v-btn>
-				<v-btn color="green" style="position: absolute; left: 70px; top: 40px" @click="trigger" icon
-				  v-else>
-					<v-icon>arrow_right</v-icon>
-				</v-btn>
-			</div> -->
-
-		</v-layout>
-	</v-card>
+	<div>
+		<v-card color="darken-1" flat hover class="resource-pack-card white--text"  style="margin-top: 10px;"
+		  draggable @dragstart="onDragStart">
+			<v-layout justify-center align-center fill-height>
+				<v-flex xs6 style="padding: 0;">
+					<v-img style="user-drag: none; user-select: none; height: 125px;" :src="data.icon" contain>
+					</v-img>
+				</v-flex>
+				<v-flex xs6 style="padding-top: 10px;">
+					<text-component style="white-space: normal; word-break: break-word;" :source="data.packName"
+					  class="title"></text-component>
+					<text-component style="white-space: normal; word-break: break-word;" :source="data.description"></text-component>
+				</v-flex>
+			</v-layout>
+		</v-card>
+		<v-divider></v-divider>
+	</div>
 </template>
 
 <script>
@@ -40,7 +26,7 @@ export default {
   },
   methods: {
     onDragStart(e) {
-			e.dataTransfer.setData("Index", `${this.isSelected ? 'R' : 'L'}${this.index}`);
+      e.dataTransfer.setData("Index", `${this.isSelected ? 'R' : 'L'}${this.index}`);
     },
   },
   components: {
@@ -52,5 +38,6 @@ export default {
 <style scoped=true>
 .title {
   max-width: 100%;
+  white-space: nowrap;
 }
 </style>
