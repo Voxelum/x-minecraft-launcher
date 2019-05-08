c1f1f6ef3128e8c999f29f91bde40ff885dd9875
ci010 <cijhn@hotmail.com>
2019-05-08 19:47:23 +0800

    update config module definition

--- src/universal/store/modules/config.d.ts
+++ src/universal/store/modules/config.d.ts
@@ -5,10 +5,12 @@ export declare namespace ConfigModule {
     interface State {
         locale: string,
         locales: string[],
+        settings: { [key: string]: number | string | boolean | object },
     }
 
     interface Commit {
         (type: 'locale', locale: string): void
+        (type: 'settings', settings: { [key: string]: number | string | boolean | object }): void
     }
 }
 export interface ConfigModule extends FullModule<ConfigModule.State, RootState, {}, Commit, {}> { }
