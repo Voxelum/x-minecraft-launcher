c80f72d0dffe3c61b5c5fa9e976c2c87f4a13f24
ci010 <cijhn@hotmail.com>
2019-05-12 23:59:26 +0800

    support crash report in backend;
    Former-commit-id: bd8e39fc9260c2e7e38d7248e9888932b839828f

--- src/main/ipc.d.ts
+++ src/main/ipc.d.ts
@@ -4,9 +4,10 @@ import { Event } from "electron";
 declare module "electron" {
     interface IpcMain {
         on(channel: 'minecraft-start', listener: () => void): this;
-        on(channel: 'minecraft-exit', listener: () => void): this;
+        on(channel: 'minecraft-exit', listener: (exitStatus?: { code?: string, signal?: string, crashReport?: string, crashReportLocation?: string }) => void): this;
         on(channel: 'minecraft-stdout', listener: (out: string) => void): this;
         on(channel: 'minecraft-stderr', listener: (err: string) => void): this;
+        on(channel: 'minecraft-crash-report', listener: (report: { crashReport: string, crashReportLocation: string }) => void): this;
 
         on(channel: 'store-ready', listener: (store: Store<any>) => void): this;
         on(channel: 'reload', listener: () => void): this;
