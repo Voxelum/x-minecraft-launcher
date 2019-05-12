d02250470f0ac5ec670baec4307d4744bd0fda93
ci010 <cijhn@hotmail.com>
2019-05-12 16:11:00 +0800

    add monitor option;
    Former-commit-id: 67dd738b79f230712f7c3f0b3b250466bd748287

--- src/main/trayManager.js
+++ src/main/trayManager.js
@@ -1,4 +1,5 @@
-import { app, Menu, Tray } from 'electron';
+import { app, Menu, Tray, dialog } from 'electron';
+
 
 let tray = null;
 app.on('ready', () => {
@@ -7,8 +8,26 @@ app.on('ready', () => {
         { label: 'Check for Updates', type: 'normal' },
         { type: 'separator' },
         {
-            label: 'Quit Launcher', 
-            type: 'normal', 
+            label: 'Show Diagnosis',
+            type: 'normal',
+            click() {
+                const cpu = process.getCPUUsage();
+                const mem = process.getProcessMemoryInfo();
+                const sysmem = process.getSystemMemoryInfo();
+
+                mem.then((m) => {
+                    dialog.showMessageBox({
+                        type: 'info',
+                        title: 'Diagnosis Info',
+                        message: `CPU: ${JSON.stringify(cpu)}\nMem: ${JSON.stringify(m)}\nSysMem: ${JSON.stringify(sysmem)}`,
+                    });
+                });
+            },
+        },
+        { type: 'separator' },
+        {
+            label: 'Quit Launcher',
+            type: 'normal',
             click(item, window, event) {
                 app.quit();
             },
