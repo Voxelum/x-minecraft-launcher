2e5f61ee529bfb714267475e0822925c39c4846d
ci010 <cijhn@hotmail.com>
2019-05-12 23:59:09 +0800

    add crash report;
    Former-commit-id: 2d5f50558667cd4396ddd49f8cf32862fd47425b

---
+++ src/renderer/windows/profile/CrashReport.vue
@@ -0,0 +1,39 @@
+<template>
+	<v-dialog :value="value" persistent>
+		<v-toolbar dark tabs color="grey darken-3">
+			<v-toolbar-title>{{location}}</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn icon @click="$emit('close')">
+				<v-icon>arrow_drop_down</v-icon>
+			</v-btn>
+		</v-toolbar>
+		<v-card flat style="min-height: 300px; overflow-y: auto;" dark >
+			<!-- <v-card-text> -->
+			<v-textarea hide-details solo no-resize style="max-height: 400px" background-color="grey darken-4" readonly auto-grow :value="content" flat>
+			</v-textarea>
+			<!-- </v-card-text> -->
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: true,
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+    location: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+</style>
