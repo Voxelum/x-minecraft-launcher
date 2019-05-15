7f54066ce4ffa9b5c6819f4b8502d85d05072242
ci010 <cijhn@hotmail.com>
2019-05-15 14:16:34 +0800

    fix strange translation problem;
    Former-commit-id: 7623eba29efe0a13304cd46bdf1196c3d784ff3b

--- src/main/i18n.js
+++ src/main/i18n.js
@@ -15,10 +15,15 @@ const i18n = new VueI18n({
 });
 const { _t, _tc } = i18n;
 i18n._t = function (k, v, l, h, ...args) {
+    if (args[0] instanceof Array) {
+        args = args[0];
+    }
     const result = _t.apply(i18n, [k, v, l, h, ...args]);
     if (typeof result === 'object') {
         const fallback = result[''];
-        if (fallback) { return i18n.formatter.interpolate(fallback, args[0]).join(''); }
+        if (fallback) {
+            return i18n.formatter.interpolate(fallback, args[0]).join('');
+        }
         return k;
     }
     return result;
