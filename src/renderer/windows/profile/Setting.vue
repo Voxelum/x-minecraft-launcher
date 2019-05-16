dea2d36388e55c7b9b27c29f5ca30b9ce1bc235f
ci010 <cijhn@hotmail.com>
2019-05-16 18:23:18 +0800

    support update options;

--- src/renderer/windows/profile/Setting.vue
+++ src/renderer/windows/profile/Setting.vue
@@ -7,24 +7,46 @@
 			<v-flex d-flex xs6 grow>
 				<v-select dark :label="$t('setting.language')" :items="langs" v-model="selectedLang"></v-select>
 			</v-flex>
-
-			<v-flex d-flex xs12 grow>
+			<v-flex d-flex xs6></v-flex>
+			<v-flex d-flex xs6 style="flex-direction: column;">
 				<v-checkbox dark v-model="autoInstallOnAppQuit" :label="$t('setting.autoInstallOnAppQuit')"></v-checkbox>
-			</v-flex>
-			<v-flex d-flex xs12 grow>
 				<v-checkbox dark v-model="autoDownload" :label="$t('setting.autoDownload')"></v-checkbox>
-			</v-flex>
-			<v-flex d-flex xs12 grow>
 				<v-checkbox dark v-model="allowPrerelease" :label="$t('setting.allowPrerelease')"></v-checkbox>
 			</v-flex>
-			<v-flex d-flex xs6 grow>
+
+			<v-flex d-flex xs6 grow style="color: white;">
+				<v-tooltip top>
+					<template v-slot:activator="{ on }">
+						<v-card dark hover v-on="on">
+							<v-card-title>
+								<h3>
+									{{updateInfo.releaseName}}
+								</h3>
+								<span class="grey--text">{{updateInfo.releaseDate}}</span>
+								<v-spacer></v-spacer>
+								<v-chip small>v{{updateInfo.version}}</v-chip>
+							</v-card-title>
+							<v-divider></v-divider>
+							<v-card-text>
+
+								<div v-html="updateInfo.releaseNotes"></div>
+							</v-card-text>
+						</v-card>
+					</template>
+					{{$t('setting.latestVersion')}}
+				</v-tooltip>
+
+			</v-flex>
+			<v-flex d-flex xs6>
 				<v-btn large dark @click="checkUpdate">
 					{{$t('setting.checkUpdate')}}
 				</v-btn>
 			</v-flex>
-			<v-flex d-flex xs6 grow style="color: white;">
-				{{updateInfo}}
-			</v-flex>
+
+			<p class="white--text" style="position: absolute; bottom: 35px; right: 315px;">
+				<a href="https://github.com/ci010/voxelauncher"> Github Repo</a>
+			</p>
+
 			<p class="white--text" style="position: absolute; bottom: 10px; right: 300px;">
 				Present by <a href="https://github.com/ci010"> CI010 </a>
 			</p>
@@ -57,6 +79,7 @@ export default {
     },
   },
   computed: {
+    version() { },
     updateInfo() { return this.$repo.state.config.updateInfo; },
     readyToUpdate() { return this.$repo.state.config.readyToUpdate; },
     langs() { return this.$repo.state.config.locales; }
