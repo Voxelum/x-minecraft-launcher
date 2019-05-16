53a2f09f74c2bb18955ba481fbf3b74b695d135c
ci010 <cijhn@hotmail.com>
2019-05-16 14:19:05 +0800

    handle client log;

--- src/main/logger.js
+++ src/main/logger.js
@@ -3,11 +3,19 @@ import util from 'util';
 import fs from 'fs';
 import path from 'path';
 
+const root = app.getPath('userData');
+try {
+    fs.mkdirSync(path.resolve(root, 'logs'));
+} catch (e) {
+    if (e.code !== 'EEXIST') {
+        throw e;
+    }
+}
+
 function overwrite() {
     const { log, error, warn } = console;
-    const root = app.getPath('userData');
 
-    const outstream = fs.createWriteStream(path.resolve(root, 'main.log'), { encoding: 'utf-8', flags: 'w+' });
+    const outstream = fs.createWriteStream(path.resolve(root, 'logs', 'main.log'), { encoding: 'utf-8', flags: 'w+' });
     console.log = (message, ...options) => {
         const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
         const content = `[INFO] [${new Date().toUTCString()}]: ${raw}`;
@@ -29,3 +37,34 @@ function overwrite() {
 }
 
 ipcMain.on('reload', overwrite);
+
+const senderIdToStreams = {};
+ipcMain.on('renderer-setup', (event, id) => {
+    console.log(`Setup renderer logger for window ${id}.`);
+    senderIdToStreams[event.sender.id] = fs.createWriteStream(path.resolve(root, 'logs', `renderer.${id}.log`), { encoding: 'utf-8', flags: 'w+' });
+});
+
+ipcMain.on('renderer-log', (event, message, ...options) => {
+    const stream = senderIdToStreams[event.sender.id];
+    if (stream) {
+        const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
+        const content = `[INFO] [${new Date().toUTCString()}]: ${raw}`;
+        stream.write(`${content}\n`);
+    }
+});
+ipcMain.on('renderer-warn', (event, message, ...options) => {
+    const stream = senderIdToStreams[event.sender.id];
+    if (stream) {
+        const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
+        const content = `[WARN] [${new Date().toUTCString()}]: ${raw}`;
+        stream.write(`${content}\n`);
+    }
+});
+ipcMain.on('renderer-error', (event, message, ...options) => {
+    const stream = senderIdToStreams[event.sender.id];
+    if (stream) {
+        const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
+        const content = `[ERROR] [${new Date().toUTCString()}]: ${raw}`;
+        stream.write(`${content}\n`);
+    }
+});
