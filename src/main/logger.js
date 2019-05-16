144481b5446b46dad08fd9f7b68293ebbbefb1e8
ci010 <cijhn@hotmail.com>
2019-05-16 15:51:32 +0800

    fix wrong root usage;
    Former-commit-id: 0f0baecb3f74817a1730d2618b97776438e37d43

--- src/main/logger.js
+++ src/main/logger.js
@@ -3,18 +3,19 @@ import util from 'util';
 import fs from 'fs';
 import path from 'path';
 
-const root = app.getPath('userData');
-try {
-    fs.mkdirSync(path.resolve(root, 'logs'));
-} catch (e) {
-    if (e.code !== 'EEXIST') {
-        throw e;
-    }
-}
-
 function overwrite() {
     const { log, error, warn } = console;
 
+    const root = app.getPath('userData');
+    try {
+        fs.mkdirSync(path.resolve(root, 'logs'));
+    } catch (e) {
+        if (e.code !== 'EEXIST') {
+            throw e;
+        }
+    }
+
+    console.log(path.resolve(root, 'logs', 'main.log'));
     const outstream = fs.createWriteStream(path.resolve(root, 'logs', 'main.log'), { encoding: 'utf-8', flags: 'w+' });
     console.log = (message, ...options) => {
         const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
@@ -34,37 +35,38 @@ function overwrite() {
         error(content);
         outstream.write(`${content}\n`);
     };
+
+
+    const senderIdToStreams = {};
+    ipcMain.on('renderer-setup', (event, id) => {
+        console.log(`Setup renderer logger for window ${id}.`);
+        senderIdToStreams[event.sender.id] = fs.createWriteStream(path.resolve(root, 'logs', `renderer.${id}.log`), { encoding: 'utf-8', flags: 'w+' });
+    });
+
+    ipcMain.on('renderer-log', (event, message, ...options) => {
+        const stream = senderIdToStreams[event.sender.id];
+        if (stream) {
+            const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
+            const content = `[INFO] [${new Date().toUTCString()}]: ${raw}`;
+            stream.write(`${content}\n`);
+        }
+    });
+    ipcMain.on('renderer-warn', (event, message, ...options) => {
+        const stream = senderIdToStreams[event.sender.id];
+        if (stream) {
+            const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
+            const content = `[WARN] [${new Date().toUTCString()}]: ${raw}`;
+            stream.write(`${content}\n`);
+        }
+    });
+    ipcMain.on('renderer-error', (event, message, ...options) => {
+        const stream = senderIdToStreams[event.sender.id];
+        if (stream) {
+            const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
+            const content = `[ERROR] [${new Date().toUTCString()}]: ${raw}`;
+            stream.write(`${content}\n`);
+        }
+    });
 }
 
 ipcMain.on('reload', overwrite);
-
-const senderIdToStreams = {};
-ipcMain.on('renderer-setup', (event, id) => {
-    console.log(`Setup renderer logger for window ${id}.`);
-    senderIdToStreams[event.sender.id] = fs.createWriteStream(path.resolve(root, 'logs', `renderer.${id}.log`), { encoding: 'utf-8', flags: 'w+' });
-});
-
-ipcMain.on('renderer-log', (event, message, ...options) => {
-    const stream = senderIdToStreams[event.sender.id];
-    if (stream) {
-        const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
-        const content = `[INFO] [${new Date().toUTCString()}]: ${raw}`;
-        stream.write(`${content}\n`);
-    }
-});
-ipcMain.on('renderer-warn', (event, message, ...options) => {
-    const stream = senderIdToStreams[event.sender.id];
-    if (stream) {
-        const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
-        const content = `[WARN] [${new Date().toUTCString()}]: ${raw}`;
-        stream.write(`${content}\n`);
-    }
-});
-ipcMain.on('renderer-error', (event, message, ...options) => {
-    const stream = senderIdToStreams[event.sender.id];
-    if (stream) {
-        const raw = options.length !== 0 ? util.format(message, options) : util.format(message);
-        const content = `[ERROR] [${new Date().toUTCString()}]: ${raw}`;
-        stream.write(`${content}\n`);
-    }
-});
