589ff8ad1b0b97e41dfd1aeb27b1c01e6a0d2e0d
ci010 <cijhn@hotmail.com>
2019-05-17 17:22:18 +0800

    fix login locale text;
    Former-commit-id: e7c54a907ddca069386842e74cef35e68cb28dd0

--- src/renderer/windows/profile/Login.vue
+++ src/renderer/windows/profile/Login.vue
@@ -17,21 +17,22 @@
 
 				<!-- <v-text-field dark prepend-icon="person" :label="$t(`${selectedMode}.account`)" :rules="accountRules"
 				  v-model="account" :error="accountError" :error-messages="accountErrors" required @keypress="handleKey"></v-text-field> -->
-				<v-text-field dark="" prepend-icon="lock" :label="$t(`${selectedMode}.password`)" type="password"
+				<v-text-field dark="" prepend-icon="lock" :label="$t(`login.${selectedMode}.password`)" type="password"
 				  :rules="passworldRuls" :disabled="selectedMode==='offline'" v-model="password" :error="passwordError"
 				  :error-messages="passwordErrors" required @keypress="handleKey"></v-text-field>
-				<v-checkbox v-model="rememberMe" label="Remember me" style="padding-top: 5px;" dark>
+				<v-checkbox v-model="rememberMe" :label="$t('login.rememberMe')" style="padding-top: 5px;" dark>
 				</v-checkbox>
 			</v-form>
 		</v-card-text>
-		<v-card-actions style="padding-left: 40px; padding-right: 40px; padding-top: 0px">
-			<v-flex text-xs-center>
+		<v-card-actions style="padding-left: 40px; padding-right: 40px; padding-top: 0px;">
+			<v-flex text-xs-center style="z-index: 10;">
 				<v-btn block :loading="logining" color="green" round large style="color: white" dark @click="login">
 					{{$t('login')}}
 				</v-btn>
 				<div style="margin-top: 25px; margin-bottom: 25px;">
-					<a style="padding-right: 10px;">{{$t('user.forgetPassword')}}</a>
-					<a style="">{{$t('user.signup.description')}} {{$t('user.signup')}}</a>
+					<a style="padding-right: 10px; z-index: 20" href="https://my.minecraft.net/en-us/password/forgot/">{{$t('login.forgetPassword')}}</a>
+					<a style="z-index: 20" href="https://my.minecraft.net/en-us/store/minecraft/#register">{{$t('login.signup.description')}}
+						{{$t('login.signup')}}</a>
 				</div>
 			</v-flex>
 		</v-card-actions>
@@ -56,20 +57,20 @@ export default {
       passwordErrors: [],
 
       usernameRules: [
-        v => !!v || this.$t('user.requireUsername'),
+        v => !!v || this.$t('login.requireUsername'),
       ],
       emailRules: [
-        v => !!v || this.$t('user.requireEmail'),
-        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || this.$t('user.illegalEmail'),
+        v => !!v || this.$t('login.requireEmail'),
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || this.$t('login.illegalEmail'),
       ],
       passworldRuls: [
-        v => !!v || this.$t('user.requirePassword'),
+        v => !!v || this.$t('login.requirePassword'),
       ],
       selectedMode: 'mojang',
     }
   },
   computed: {
-    loginModes() { return this.$repo.getters['user/authModes'] },
+    loginModes() { return this.$repo.getters['user/authModes'].map(m => ({ text: this.$t(`login.${m}.name`), value: m })) },
     accountRules() { return this.selectedMode === 'offline' ? this.usernameRules : this.emailRules; },
     history() { return this.$repo.getters['user/history'] },
   },
@@ -104,7 +105,7 @@ export default {
       } catch (e) {
         this.logining = false;
         if (e.type === 'ForbiddenOperationException' && e.message === 'Invalid credentials. Invalid username or password.') {
-          const msg = this.$t('user.invalidCredentials');
+          const msg = this.$t('login.invalidCredentials');
           this.accountError = true;
           this.accountErrors = [msg];
           this.passwordError = true;
