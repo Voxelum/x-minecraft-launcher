3420c9bf95558ab2bd8ac3d1a1c5e10398d647f5
ci010 <cijhn@hotmail.com>
2019-06-01 18:09:37 +0800

    [feat] support check update & download update  status
    Former-commit-id: 50780f906c08f18da5eb330d144e16680cda6369

--- src/universal/store/modules/config.d.ts
+++ src/universal/store/modules/config.d.ts
@@ -1,4 +1,4 @@
-import { Module } from "../store";
+import { Module, Context } from "../store";
 import { UpdateInfo } from "electron-updater";
 
 export declare namespace ConfigModule {
@@ -13,14 +13,24 @@ export declare namespace ConfigModule {
         allowPrerelease: boolean
 
         readyToUpdate: boolean
+        checkingUpdate: boolean
+        downloadingUpdate: boolean
     }
 
     interface Mutations {
         locale(state: State, locale: string): void
+        downloadingUpdate(state: State, prog: boolean): void;
+        checkingUpdate(state: State, prog: boolean): void;
         settings(state: State, settings: { [key: string]: number | string | boolean | object }): void
     }
+    type C = Context<State, {}, Mutations, Actions>;
+    interface Actions {
+        downloadUpdate(context: C): Promise<string>;
+        quitAndInstall(context: C): Promise<void>;
+        checkUpdate(context: C): Promise<string>;
+    }
 }
-export interface ConfigModule extends Module<ConfigModule.State, ConfigModule.Mutations, {}> { }
+export interface ConfigModule extends Module<ConfigModule.State, ConfigModule.Mutations, ConfigModule.Actions> { }
 
 declare const mod: ConfigModule;
 
