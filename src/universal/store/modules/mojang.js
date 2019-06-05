7588d16ff0b0475fe0c047b1570c7030144288c0
ci010 <cijhn@hotmail.com>
2019-06-05 13:40:43 +0800

    refactor: cleanup code
    Former-commit-id: bf21508321510e85756495d7b71bc26439729d22

--- src/universal/store/modules/mojang.js
+++ src/universal/store/modules/mojang.js
@@ -4,23 +4,9 @@ import { MojangService } from 'ts-minecraft';
 export default {
     namespaced: true,
     actions: {
-        fetchUserInfo(context, accessToken) {
-            return MojangService.getAccountInfo(accessToken);
-        },
         fetchNews() {
             return new Promise((resolve, reject) => {
                 const req = net.request('https://launchermeta.mojang.com/mc/news.json');
-                req.on('response', (response) => {
-                    let str = '';
-                    response.on('data', (buf) => { str += buf.toString(); });
-                    response.on('end', (buf) => { resolve(JSON.parse(str)); });
-                });
-                req.end();
-            });
-        },
-        fetchLauncherInfo(context) {
-            return new Promise((resolve, reject) => {
-                const req = net.request('https://launchermeta.mojang.com/mc/launcher.json');
                 req.on('response', (response) => {
                     let str = '';
                     response.on('data', (buf) => { str += buf.toString(); });
