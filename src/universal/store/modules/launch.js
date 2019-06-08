1208b5ad21860293d8e395ea7ac3799082125667
ci010 <cijhn@hotmail.com>
2019-06-08 22:44:56 +0800

    refactor: adapt to new version resolving

--- src/universal/store/modules/launch.js
+++ src/universal/store/modules/launch.js
@@ -1,9 +1,6 @@
-import { promises as fs } from 'fs';
-import { MinecraftFolder, Launcher, Version } from 'ts-minecraft';
+import { MinecraftFolder, Launcher } from 'ts-minecraft';
 import paths from 'path';
 import { ipcMain } from 'electron';
-import { getExpectVersion } from 'universal/utils/versions';
-import { ensureFile } from 'universal/utils/fs';
 
 /**
  * @param {{ message: string; type: string; }} e
@@ -19,176 +16,6 @@ function onerror(e) {
         e.type = 'illegal.version.json';
     }
     return e;
-}
-
-/**
- * 
- * @param {string} id 
- * @param {MinecraftFolder} location 
- * @param {string} mcTemp 
- * @param {string} forgeTemp 
- * @param {string} liteTemp 
- */
-async function mixinVersion(id, location, forgeTemp, liteTemp) {
-    console.log(`mixin version from ${forgeTemp} ${liteTemp}`);
-    /**
-    * @type {Version.Raw}
-    */
-    const forgeJson = await fs.readFile(location.getVersionJson(forgeTemp)).then(b => JSON.parse(b.toString()));
-    /**
-    * @type {Version.Raw}
-    */
-    const liteJson = await fs.readFile(location.getVersionJson(liteTemp)).then(b => JSON.parse(b.toString()));
-
-    const profile = {
-        id,
-        time: liteJson.time,
-        releaseTime: liteJson.releaseTime,
-        type: liteJson.type,
-        inheritsFrom: forgeTemp,
-        libraries: liteJson.libraries,
-        mainClass: liteJson.mainClass,
-        jar: liteJson.jar,
-    };
-    const args = liteJson.arguments ? liteJson.arguments.game : liteJson.minecraftArguments.split(' ');
-    const forgeArgs = forgeJson.arguments ? forgeJson.arguments.game : forgeJson.minecraftArguments.split(' ');
-    let tweakClass;
-    for (let i = 0; i < args.length; i += 1) {
-        const a = args[i];
-        if (a === '--tweakClass') tweakClass = args[i + 1];
-    }
-    if (!tweakClass) {
-        const err = {
-            type: 'CorruptedVersionJson',
-            reason: 'MissingTweakClass',
-            version: liteTemp,
-        };
-        throw err;
-    }
-
-    if (liteJson.arguments) {
-        Reflect.set(profile, 'arguments', {
-            game: ['--tweakClass', tweakClass, ...forgeArgs],
-            jvm: [...liteJson.arguments.jvm],
-        });
-    } else {
-        forgeArgs.unshift(tweakClass);
-        forgeArgs.unshift('--tweakClass');
-        Reflect.set(profile, 'minecraftArguments', forgeArgs.join(' '));
-    }
-
-    const json = location.getVersionJson(id);
-    await ensureFile(json);
-    await fs.writeFile(json, JSON.stringify(profile, undefined, 4));
-}
-
-/**
- * @param {import('./launch').C} context
- * @param {import('./profile').ProfileModule.Profile} profile 
- * @param {MinecraftFolder} minecraftFolder 
- */
-async function resolveVersion(context, profile, minecraftFolder) {
-    const localVersions = context.rootState.version.local;
-    /**
-     * @typedef {import('./version').VersionModule.LocalVersion} LocalVersion
-     * cache the mcversion -> forge/lite/mc versions real id 
-     * @type {{[mcversion: string] : { forge: LocalVersion[], liteloader: LocalVersion[], minecraft: string }}}
-     */
-    const mcverMap = {};
-    /**
-     * cache the map that expected id -> real id
-     * @type {{[expectId: string]: string}}
-     */
-    const expectVersionMap = {};
-    /**
-     * cache local version into map
-     */
-    localVersions.forEach((ver) => {
-        if (!mcverMap[ver.minecraft]) {
-            mcverMap[ver.minecraft] = {
-                forge: [],
-                liteloader: [],
-                minecraft: '',
-            };
-        }
-        const container = mcverMap[ver.minecraft];
-        if (ver.forge) container.forge.push(ver);
-        if (ver.liteloader) container.liteloader.push(ver);
-        if (!ver.forge && ver.liteloader) container.minecraft = ver.id;
-        expectVersionMap[getExpectVersion(ver.minecraft, ver.forge, ver.liteloader)] = ver.id;
-    });
-
-    const mcversion = profile.mcversion;
-    if (!mcversion) {
-        const err = {
-            type: 'NoSelectedVersion',
-        };
-        throw err;
-    }
-
-    const forgeVersion = profile.forge.enabled ? profile.forge.version : undefined;
-    const liteVersion = profile.liteloader.enabled ? profile.liteloader.version : undefined;
-
-    const expectId = getExpectVersion(mcversion, forgeVersion, liteVersion);
-    const targetVersionId = expectVersionMap[expectId];
-    /**
-     * real version name
-     * @type {string}
-     */
-    let version;
-
-    if (!targetVersionId) {
-        console.log(`try to generate version dynamic, ${expectId}`);
-        // if target version not exist, try to generate version dynamicly
-        const versionContainer = mcverMap[mcversion];
-        if (!versionContainer) {
-            const err = {
-                type: 'MissingMinecraftVersion',
-                version: mcversion,
-            };
-            throw err;
-        }
-        let forgeTemplate;
-        let liteTemplate;
-        if (forgeVersion) {
-            const forges = versionContainer.forge;
-            for (const f of forges) {
-                if (f.forge === forgeVersion) {
-                    forgeTemplate = f.id;
-                    break;
-                }
-            }
-            if (!forgeTemplate) {
-                const err = {
-                    type: 'MissingForgeVersion',
-                    version: forgeVersion,
-                };
-                throw err;
-            }
-        }
-        if (liteVersion) {
-            const lites = versionContainer.liteloader;
-            for (const v of lites) {
-                if (v.liteloader === liteVersion) {
-                    liteTemplate = v.id;
-                    break;
-                }
-            }
-            if (!liteTemplate) {
-                const err = {
-                    type: 'MissingLiteloaderVersion',
-                    version: liteVersion,
-                };
-                throw err;
-            }
-        }
-        await mixinVersion(expectId, minecraftFolder, forgeTemplate, liteTemplate);
-        version = expectId;
-    } else {
-        version = targetVersionId;
-    }
-
-    return version;
 }
 
 /**
@@ -211,19 +38,16 @@ const mod = {
 
             /**
              * real version name
-             * @type {string}
              */
-            const version = profile.forceVersion
-                ? profile.version
-                : await resolveVersion(context, profile, minecraftFolder);
+            const version = await context.dispatch('version/resolve', {
+                minecraft: profile.mcversion,
+                folder: '',
+                forge: profile.forge.enabled ? profile.forge.version : undefined,
+                liteloader: profile.liteloader.enabled ? profile.liteloader.version : undefined,
+            }, { root: true });
 
             console.log(`Chooose ${version} version.`);
 
-            /**
-             * Handle profile error
-             */
-            // const errors = context.getters[`profiles/${profileId}/errors`]
-            // if (errors && errors.length !== 0) return Promise.reject(errors[0])
             const java = profile.java || context.rootGetters['java/default'];
             /**
              * Build launch condition
