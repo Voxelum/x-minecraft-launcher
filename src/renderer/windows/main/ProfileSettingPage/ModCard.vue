55701c5d455cadcb0fd7bdc5fee099753d36f564
ci010 <cijhn@hotmail.com>
2019-06-08 22:41:30 +0800

    feat: support compatability on card

--- src/renderer/windows/main/ProfileSettingPage/ModCard.vue
+++ src/renderer/windows/main/ProfileSettingPage/ModCard.vue
@@ -19,25 +19,59 @@
 				</v-layout>
 			</v-card>
 		</template>
-		{{ compatible ? data.modid : $t('mod.incompatible') }}
+		{{ compatible ? $t('mod.compatible') : $t('mod.incompatible', { accept: acceptedRange, actual:
+		mcversion }) }}
 		<v-divider></v-divider>
 	</v-tooltip>
 </template>
 
 <script>
+import { isCompatible } from 'universal/utils/versions';
+import unknownPack from 'static/unknown_pack.png';
+
 export default {
   data() {
     return {
       icon: '',
     };
   },
-  props: ['data', 'isSelected', 'index', 'hash', 'compatible'],
+  props: {
+    data: {
+      required: true,
+      type: Object
+    },
+    isSelected: {
+      required: true,
+      type: Boolean,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+    hash: {
+      required: true,
+      type: String
+    }
+  },
   mounted() {
     this.$repo.dispatch('resource/readForgeLogo', this.hash).then((icon) => {
       if (typeof icon === 'string' && icon !== '') {
         this.icon = `data:image/png;base64, ${icon}`;
+      } else {
+        this.icon = unknownPack;
       }
     })
+  },
+  computed: {
+    mcversion() {
+      return this.$repo.getters['profile/current'].mcversion;
+    },
+    acceptedRange() {
+      return this.data.acceptedMinecraftVersions ? this.data.acceptedMinecraftVersions : `[${this.data.mcversion}]`;
+    },
+    compatible() {
+      return isCompatible(this.acceptedRange, this.mcversion);
+    },
   },
   methods: {
     onDragStart(e) {
@@ -56,6 +90,10 @@ export default {
 .incompatible.draggable-card:hover {
   background-color: #e65100;
 }
+.draggable-card:hover {
+  background-color: #388E3C;
+}
+
 .title {
   max-width: 100%;
   white-space: nowrap;
