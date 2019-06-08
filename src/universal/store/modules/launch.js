1d396bc7082f8a0e1f4d5a1a6337b052051dad4e
ci010 <cijhn@hotmail.com>
2019-06-08 22:44:56 +0800

    refactor: adapt to new version resolving
    Former-commit-id: 769634c2642efe2d4fc43d5cd501e3d2c3c5f625

---
+++ src/universal/store/modules/launch.js
@@ -0,0 +1,144 @@
+import { MinecraftFolder, Launcher } from 'ts-minecraft';
+import paths from 'path';
+import { ipcMain } from 'electron';
+
+/**
+ * @param {{ message: string; type: string; }} e
+ */
+function onerror(e) {
+    if (e.message.startsWith('Cannot find version ') || e.message.startsWith('No version file for ') || e.message.startsWith('No version jar for ')) {
+        e.type = 'missing.version';
+    } else if (e.message === 'Missing library') {
+        e.type = 'missing.libraries';
+    } else if (e.message === 'Missing asset!') {
+        e.type = 'missing.assets';
+    } else if (e.message === 'Missing mainClass' || e.message === 'Missing minecraftArguments') {
+        e.type = 'illegal.version.json';
+    }
+    return e;
+}
+
+/**
+ * @type { import('./launch').LauncherModule }
+ */
+const mod = {
+    actions: {
+        async launch(context) {
+            /**
+             * current selected profile
+             * @type { import('./profile').ProfileModule.Profile }
+             */
+            const profile = context.rootGetters['profile/current'];
+            const user = context.rootState.user;
+            if (!profile) return Promise.reject(new Error('launch.profile.empty'));
+            if (user.accessToken === '' || user.name === '' || user.id === '') return Promise.reject(new Error('launch.auth.illegal'));
+
+            const debug = profile.showLog;
+            const minecraftFolder = new MinecraftFolder(paths.join(context.rootState.root, 'profiles', profile.id));
+
+            /**
+             * real version name
+             */
+            const version = await context.dispatch('version/resolve', {
+                minecraft: profile.mcversion,
+                folder: '',
+                forge: profile.forge.enabled ? profile.forge.version : undefined,
+                liteloader: profile.liteloader.enabled ? profile.liteloader.version : undefined,
+            }, { root: true });
+
+            console.log(`Chooose ${version} version.`);
+
+            const java = profile.java || context.rootGetters['java/default'];
+            /**
+             * Build launch condition
+             * @type {Launcher.Option}
+             */
+            const option = {
+                auth: {
+                    selectedProfile: {
+                        id: user.id,
+                        name: user.name,
+                    },
+                    accessToken: user.accessToken,
+                    userType: user.userType,
+                    properties: user.properties,
+                },
+                gamePath: minecraftFolder.root,
+                resourcePath: context.rootState.root,
+                javaPath: java.path,
+                minMemory: profile.minMemory || 1024,
+                maxMemory: profile.maxMemory || 1024,
+                version,
+            };
+            if (profile.type === 'server') {
+                option.server = { ip: profile.server.host, port: profile.server.port };
+            }
+
+            const { mods, resourcepacks } = await context.dispatch('profile/resolveResources', context.rootState.profile.id);
+
+            try {
+                await context.dispatch('resource/deploy', {
+                    resources: resourcepacks,
+                    minecraft: option.gamePath,
+                });
+            } catch (e) {
+                console.error('Cannot deploy resource packs');
+                console.error(e);
+            }
+            try {
+                await context.dispatch('resource/deploy', {
+                    resources: mods,
+                    minecraft: option.gamePath,
+                });
+            } catch (e) {
+                console.error('Cannot deploy mods');
+                console.error(e);
+            }
+
+            console.log(JSON.stringify(option));
+
+            // Launch
+            return Launcher.launch(option).then((process) => {
+                let crashReport = '';
+                let crashReportLocation = '';
+                ipcMain.emit('minecraft-start', debug);
+                process.on('error', (err) => {
+                    console.log(err);
+                });
+                process.on('exit', (code, signal) => {
+                    console.log(`exit: ${code}, signal: ${signal}`);
+                    if (code !== 0 && (crashReport || crashReportLocation)) {
+                        ipcMain.emit('minecraft-crash-report', {
+                            crashReport,
+                            crashReportLocation,
+                        });
+                        ipcMain.emit('minecraft-exit', {
+                            code,
+                            signal,
+                            crashReport,
+                            crashReportLocation,
+                        });
+                    } else {
+                        ipcMain.emit('minecraft-exit', { code, signal });
+                    }
+                });
+                process.stdout.on('data', (s) => {
+                    const string = s.toString();
+                    if (string.indexOf('---- Minecraft Crash Report ----') !== -1) {
+                        crashReport = string;
+                    } else if (string.indexOf('Crash report saved to:') !== -1) {
+                        crashReportLocation = string.substring(string.indexOf('Crash report saved to:') + 'Crash report saved to: #@!@# '.length);
+                    }
+                    ipcMain.emit('minecraft-stdout', string);
+                });
+                process.stderr.on('data', (s) => {
+                    ipcMain.emit('minecraft-stderr', s.toString());
+                });
+            }).catch((e) => {
+                throw (e);
+            });
+        },
+    },
+};
+
+export default mod;
