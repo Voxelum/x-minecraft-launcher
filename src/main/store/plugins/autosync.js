09b3b47530ead70a732f05b818af6d1fc69fa91b
ci010 <cijhn@hotmail.com>
2019-06-08 22:40:09 +0800

    feat: try explore new way to do the commit

--- src/main/store/plugins/autosync.js
+++ src/main/store/plugins/autosync.js
@@ -4,10 +4,17 @@ import { ipcMain, webContents } from 'electron';
 
 export default
 /**
-     * @param {Vuex.Store<any>} store
-     */
+         * @param {Vuex.Store<any>} store
+         */
 (store) => {
     const mutationHistory = [];
+    ipcMain.on('vuex-dispatch', (event, { action, payload, option, id }) => {
+        store.dispatch(action, payload, option).then((result) => {
+            event.sender.send(`vuex-dispatch-${id}`, { result });
+        }, (error) => {
+            event.sender.send(`vuex-dispatch-${id}`, { error });
+        });
+    });
     ipcMain.on('vuex-sync', (event, currentId) => {
         console.log(`sync on renderer: ${currentId}, main: ${mutationHistory.length}`);
         if (currentId === mutationHistory.length) {
@@ -18,8 +25,8 @@ export default
     });
     store.subscribe(
         /**
-             * @param {{type: string}} mutation 
-             */
+                 * @param {{type: string}} mutation 
+                 */
         (mutation, state) => {
             mutationHistory.push(mutation);
             const id = mutationHistory.length;
