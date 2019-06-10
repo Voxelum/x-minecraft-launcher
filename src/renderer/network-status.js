0eab2ffd5b8aaa102883e75128f52b2e9515aff7
ci010 <cijhn@hotmail.com>
2019-06-11 01:12:20 +0800

    feat: try impl network status
    Former-commit-id: bb3e0c2e0592b50bde1c872ce562176b077a81aa

---
+++ src/renderer/network-status.js
@@ -0,0 +1,6 @@
+// const { ipcRenderer } = require('electron');
+
+// window.updateOnlineStatus = function () {
+//     console.log('update status');
+//     ipcRenderer.send('online-status-changed', navigator.onLine ? 'online' : 'offline');
+// };
