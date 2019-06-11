a12823bf32e65c0ce5e44c82e62400791b6d24eb
ci010 <cijhn@hotmail.com>
2019-06-11 17:37:01 +0800

    feat: try split the setting page by group
    Former-commit-id: 9be3428e68ebf964cd2053c90da292b2f6a98e6d

--- src/renderer/windows/main/ProfileSettingPage/BaseSetting.vue
+++ src/renderer/windows/main/ProfileSettingPage/BaseSetting.vue
@@ -3,13 +3,10 @@
 		<v-container grid-list-xs fill-height style="overflow: auto;">
 			<v-layout row wrap>
 				<v-flex tag="h1" style="margin-bottom: 10px;" class="white--text" xs12>
-					<span class="headline">{{$t('settings')}}</span>
+					<span class="headline">{{$t('profile.setting')}}</span>
 				</v-flex>
 				<v-flex d-flex xs4>
-					<v-text-field outline dark v-model="name" :label="$t('name')" :rules="nameRules" required></v-text-field>
-				</v-flex>
-				<v-flex d-flex xs4>
-					<v-text-field outline dark v-model="author" :label="$t('author')" required></v-text-field>
+					<v-text-field outline dark v-model="name" :label="$t('name')" :placeholder="`Minecraft ${mcversion}`"></v-text-field>
 				</v-flex>
 				<v-flex d-flex xs4>
 					<version-menu @value="mcversion = $event">
@@ -19,37 +16,26 @@
 						</template>
 					</version-menu>
 				</v-flex>
-				<v-flex d-flex xs8>
-					<v-text-field outline dark v-model="description" :label="$t('description')">
-					</v-text-field>
-				</v-flex>
 				<v-flex d-flex xs4 class="local-version">
 					<v-tooltip top>
 						<template v-slot:activator="{ on }">
 							<v-select v-on="on" hide-details dark outline :item-text="(v)=>v.id" :item-value="(v)=>v"
-							  :label="$t('profile.localVersion')" v-model="localVersion" :items="localVersions"></v-select>
+							  :label="$t('profile.localVersion')" :placeholder="$t('profile.noLocalVersion')"
+							  :no-data-text="$t('profile.noLocalVersion')" v-model="localVersion" :items="localVersions"></v-select>
 						</template>
 						{{$t('profile.localVersionHint')}}
 					</v-tooltip>
 				</v-flex>
 				<v-flex d-flex xs6>
-					<v-select :item-text="regularText" :item-value="getJavaValue" outline dark prepend-inner-icon="add"
-					  v-model="java" :label="$t('java.location')" :items="javas" required :menu-props="{ auto: true, overflowY: true }"
-					  @click:prepend-inner="browseFile"></v-select>
-				</v-flex>
-				<v-flex d-flex xs3>
-					<v-text-field outline dark type="number" v-model="minMemory" :label="$t('java.minMemory')"
-					  required></v-text-field>
-				</v-flex>
-				<v-flex d-flex xs3>
-					<v-text-field outline dark type="number" v-model="maxMemory" :label="$t('java.maxMemory')"
-					  required></v-text-field>
+					<forge-version-menu @value="onSelectForge">
+						<template v-slot="{ on }">
+							<v-text-field hide-details outline dark :value="forgeVersion" placeholder="Disabled" :label="$t('forge.version')"
+							  :readonly="true" v-on="on"></v-text-field>
+						</template>
+					</forge-version-menu>
 				</v-flex>
 				<v-flex d-flex xs6>
 					<v-checkbox hide-details dark v-model="hideLauncher" :label="$t('launch.hideLauncher')"></v-checkbox>
-				</v-flex>
-				<v-flex d-flex xs6>
-					<v-checkbox hide-details dark v-model="showLog" :label="$t('launch.showLog')"></v-checkbox>
 				</v-flex>
 			</v-layout>
 		</v-container>
@@ -64,29 +50,18 @@ export default {
   data: function () {
     return {
       valid: true,
-
-      memoryRange: [256, 10240],
-
-      javaValid: true,
-      maxMemory: 1024,
-      minMemory: 1024,
-      java: { path: '', version: '' },
-      memoryRule: [v => Number.isInteger(v)],
       hideLauncher: false,
-      showLog: false,
-
       name: '',
-      nameRules: [
-        v => !!v || this.$t('profile.requireName'),
-      ],
-      author: '',
-      description: '',
     }
   },
   computed: {
     mcversion: {
       get() { return this.$repo.getters['selectedProfile'].mcversion; },
       set(v) { this.$repo.dispatch('editProfile', { mcversion: v }); },
+    },
+    forgeVersion: {
+      get() { return this.$repo.getters['selectedProfile'].forge.version; },
+      set(v) { return this.$repo.dispatch('editProfile', { forge: { version: v } }); }
     },
     localVersion: {
       get() {
@@ -112,66 +87,38 @@ export default {
     localVersions() {
       return this.$repo.state.version.local;
     },
-    javas() {
-      return this.$repo.state.java.all;
-    },
-    ready() {
-      return this.valid && this.javaValid;
-    },
     versions() {
       return Object.keys(this.$repo.state.version.minecraft.versions);
     },
+  },
+  created() {
+    this.$repo.dispatch('refreshForge').catch(e => {
+      console.error(e);
+    });
   },
   methods: {
     save() {
       this.$repo.dispatch('editProfile', {
         name: this.name,
-        author: this.author,
-        description: this.description,
-        mcversion: this.mcversion,
-        minMemory: this.minMemory,
-        maxMemory: this.maxMemory,
-        java: this.java,
-        showLog: this.showLog,
         hideLauncher: this.hideLauncher,
       });
     },
     load() {
       const profile = this.$repo.getters['selectedProfile'];
       this.name = profile.name;
-      this.author = profile.author;
-      this.description = profile.description;
-
-      this.maxMemory = profile.maxMemory;
-      this.minMemory = profile.minMemory;
-      this.java = profile.java;
-
       this.hideLauncher = profile.hideLauncher;
-      this.showLog = profile.showLog;
+    },
+    onSelectForge(version) {
+      if (version) {
+        this.forgeVersion = version.version;
+      } else {
+        this.forgeVersion = '';
+      }
     },
     onNameInput(event) {
       if (!this.editingName) {
         event.preventDefault();
       }
-    },
-    browseFile() {
-      this.$electron.remote.dialog.showOpenDialog({
-        title: this.$t('java.browse'),
-      }, (filePaths, bookmarks) => {
-        filePaths.forEach((p) => {
-          this.$repo.dispatch('resolveJava', p);
-        });
-      });
-    },
-    getJavaValue(java) {
-      return java;
-    },
-    regularText(java) {
-      const text = `v${java.version}: ${java.path}`
-      if (text.length > 25) {
-        return text.substring(0, 25) + '...'
-      }
-      return text;
     },
   }
 }
---
+++ src/renderer/windows/main/ProfileSettingPage/ModPackSetting.vue
@@ -0,0 +1,51 @@
+<template>
+	<v-container>
+		<v-layout row wrap>
+			<v-flex tag="h1" style="margin-bottom: 10px;" class="white--text" xs12>
+				<span class="headline">{{$t('profile.modpackSetting')}}</span>
+			</v-flex>
+			<v-flex d-flex xs4>
+				<v-text-field outline dark v-model="author" :label="$t('author')" :placeholder="$repo.state.user.name"
+				  required></v-text-field>
+			</v-flex>
+			<v-flex d-flex xs4>
+				<v-text-field outline dark v-model="url" :label="$t('url')" :placeholder="$repo.state.user.name"
+				  required></v-text-field>
+			</v-flex>
+			<v-flex d-flex xs8>
+				<v-text-field outline dark v-model="description" :label="$t('description')">
+				</v-text-field>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      author: '',
+      description: '',
+      url: '',
+    };
+  },
+  methods: {
+    save() {
+      this.$repo.dispatch('editProfile', {
+        author: this.author,
+        description: this.description,
+        url: this.url,
+      });
+    },
+    load() {
+      const profile = this.$repo.getters['selectedProfile'];
+      this.url = profile.url;
+      this.author = profile.author;
+      this.description = profile.description;
+    },
+  },
+}
+</script>
+
+<style>
+</style>
