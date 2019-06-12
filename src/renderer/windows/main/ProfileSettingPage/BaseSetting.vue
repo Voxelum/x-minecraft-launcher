68a738599e84cb7d8d2c82d01b216dd1ef69f68c
ci010 <cijhn@hotmail.com>
2019-06-13 00:24:41 +0800

    refactor: investigating new design
    Former-commit-id: c5aeee71d718b098010c6dac2d60277827e5892b

--- src/renderer/windows/main/ProfileSettingPage/BaseSetting.vue
+++ src/renderer/windows/main/ProfileSettingPage/BaseSetting.vue
@@ -2,16 +2,50 @@
 	<v-form ref="form" v-model="valid" lazy-validation style="height: 100%;">
 		<v-container grid-list-xs fill-height style="overflow: auto;">
 			<v-layout row wrap>
+
 				<v-flex tag="h1" style="margin-bottom: 10px;" class="white--text" xs12>
 					<span class="headline">{{$t('profile.setting')}}</span>
 				</v-flex>
 				<v-flex d-flex xs6>
-					<v-text-field outline dark v-model="name" :label="$t('name')" :placeholder="`Minecraft ${mcversion}`"></v-text-field>
+					<v-text-field dark v-model="name" :label="$t('name')" :placeholder="`Minecraft ${mcversion}`"></v-text-field>
 				</v-flex>
 				<v-flex d-flex xs6>
 					<v-checkbox hide-details dark v-model="hideLauncher" :label="$t('launch.hideLauncher')"></v-checkbox>
 				</v-flex>
-				<v-flex d-flex xs6>
+
+				<v-flex xs12 @mousewheel="onMouseWheel">
+					<v-toolbar dark tabs color="transparent" flat>
+						<v-tabs  v-model="active" color="transparent" dark slider-color="primary">
+							<v-tab>
+								Minecraft Versions
+							</v-tab>
+							<v-tab>
+								Forge Versions
+							</v-tab>
+							<v-tab>
+								Liteloader Versions
+							</v-tab>
+						</v-tabs>
+						<template v-slot:extension>
+							<v-text-field append-icon="filter" flat label="Search" solo-inverted></v-text-field>
+						</template>
+					</v-toolbar>
+					<v-tabs-items  v-model="active" color="transparent" dark slider-color="primary" style="max-height: 50vh; overflow-y: auto">
+						<v-tab-item>
+							<minecraft-version-list></minecraft-version-list>
+						</v-tab-item>
+						<v-tab-item>
+							<forge-version-list></forge-version-list>
+						</v-tab-item>
+						<v-tab-item>
+						</v-tab-item>
+					</v-tabs-items>
+				</v-flex>
+				<!-- <v-flex xs6>
+					<forge-version-list style="max-height: 80vh">
+					</forge-version-list>
+				</v-flex> -->
+				<!-- <v-flex d-flex xs6>
 					<version-menu @value="mcversion = $event">
 						<template v-slot="{ on }">
 							<v-text-field style="cursor: pointer !important;" outline dark append-icon="arrow_drop_down"
@@ -19,19 +53,16 @@
 							  v-on="on"></v-text-field>
 						</template>
 					</version-menu>
-				</v-flex>
-				<!-- <v-flex d-flex xs6>
-
 				</v-flex> -->
-				<v-flex d-flex xs6>
+				<!-- <v-flex d-flex xs6>
 					<forge-version-menu @value="onSelectForge">
 						<template v-slot="{ on }">
 							<v-text-field outline dark :value="forgeVersion" append-icon="arrow_drop_down"
 							  placeholder="Disabled" :label="$t('forge.version')" :readonly="true" v-on="on"></v-text-field>
 						</template>
 					</forge-version-menu>
-				</v-flex>
-				<v-flex d-flex xs12 class="local-version">
+				</v-flex> -->
+				<!-- <v-flex d-flex xs12 class="local-version">
 					<v-tooltip top>
 						<template v-slot:activator="{ on }">
 							<v-select style="width: 100%" v-on="on" hide-details dark outline :item-text="(v)=>v.id" :item-value="(v)=>v"
@@ -40,7 +71,7 @@
 						</template>
 						{{$t('profile.localVersionHint')}}
 					</v-tooltip>
-				</v-flex>
+				</v-flex> -->
 				<v-flex d-flex xs6>
 				</v-flex>
 			</v-layout>
@@ -57,6 +88,7 @@ export default {
   components: { ForgeVersionMenu },
   data: function () {
     return {
+      active: 0,
       valid: true,
       hideLauncher: false,
       name: '',
@@ -127,6 +159,10 @@ export default {
         event.preventDefault();
       }
     },
+    onMouseWheel(e) {
+      e.stopPropagation();
+      return true;
+    }
   }
 }
 </script>
