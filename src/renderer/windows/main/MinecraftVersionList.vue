786d97198fbc9ffa870d42bc59c4548266f4ad1e
ci010 <cijhn@hotmail.com>
2019-06-13 00:24:07 +0800

    refactor: extract mc/forge version list
    Former-commit-id: 05d421d9e878fdefb2a75d43359b71beacfa56be

---
+++ src/renderer/windows/main/MinecraftVersionList.vue
@@ -0,0 +1,65 @@
+<template>
+	<v-list dark style="overflow-y: scroll; scrollbar-width: 0; background-color: transparent;"
+	  @mousewheel="onMouseWheel">
+		<template v-for="(item, index) in versions">
+			<!-- <v-divider dark :key="`L${index}`"></v-divider> -->
+			<v-list-tile ripple :key="index" @click="selectVersion(item.id)" style="margin: 0px 0;">
+				<v-list-tile-avatar>
+					<v-icon v-if="statuses[item.id] !== 'loading'">
+						{{ statuses[item.id] === 'remote' ? 'cloud' : 'folder' }}
+					</v-icon>
+					<v-progress-circular v-else :width="2" :size="24" indeterminate></v-progress-circular>
+				</v-list-tile-avatar>
+
+				<v-list-tile-title>
+					{{ item.id }}
+				</v-list-tile-title>
+				<v-list-tile-sub-title v-html="item.releaseTime"></v-list-tile-sub-title>
+
+				<v-list-tile-action style="justify-content: flex-end;">
+					<v-chip :color="item.type === 'release' ? 'primary' : '' " label dark>
+						{{item.type}}
+					</v-chip>
+				</v-list-tile-action>
+			</v-list-tile>
+		</template>
+	</v-list>
+</template>
+
+<script>
+export default {
+  props: {
+    showAlpha: {
+      type: Boolean,
+      default: false,
+    },
+    filterText: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    statuses() {
+      return this.$repo.getters['minecraftStatuses'];
+    },
+    versions() {
+      return this.$repo.state.version.minecraft.versions
+        .filter(version => this.showAlpha || version.type === 'release')
+        .filter(version => version.id.indexOf(this.filterText) !== -1)
+        .map(version => version);
+    },
+  },
+  methods: {
+    onMouseWheel(e) {
+      e.stopPropagation();
+      return true;
+    },
+    selectVersion(v) {
+      this.$emit('value', v);
+    },
+  }
+}
+</script>
+
+<style>
+</style>
