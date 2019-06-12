45f507af984ebb3055f18e23cb9b3391accaf042
ci010 <cijhn@hotmail.com>
2019-06-12 18:59:37 +0800

    docs: add some doc on version module
    Former-commit-id: 2973717b98347585a3c0775f05f9fd79281538f5

--- src/universal/store/modules/version.d.ts
+++ src/universal/store/modules/version.d.ts
@@ -3,12 +3,36 @@ import { Library, VersionMeta } from "ts-minecraft/dest/libs/version";
 import { Context, Module, TaskHandle } from "../store";
 export type Status = 'remote' | 'local';
 
+/**
+ * The module handle the local/remote version related work 
+ */
 export declare namespace VersionModule {
-    interface LocalVersion {
+    /**
+     * An interface to reference a resolved version in 
+     * <minecraft folder>/versions/<version-id>/<version-id>.json
+     * 
+     * This is more lightweight than ts-minecraft's Version by Version.parse.
+     */
+    interface ResolvedVersion {
+        /**
+         * Minecraft version of this version. e.g. 1.7.10
+         */
         minecraft: string,
+        /**
+         * Forge version of this version. e.g. 14.23.5.2838
+         */
         forge: string,
         liteloader: string,
+        /**
+         * The ideal id this version, which is computed by 
+         * function universal/utils/versions.js#getExpectVersion
+         */
         id: string,
+        /**
+         * The real folder id of the version, which is the <verison-id> in
+         * 
+         * <minecraft folder>/versions/<version-id>/<version-id>.json
+         */
         folder: string
     }
 
@@ -18,7 +42,7 @@ export declare namespace VersionModule {
     }
 
     interface State {
-        local: LocalVersion[]
+        local: ResolvedVersion[]
         minecraft: Version.MetaContainer
         refreshingMinecraft: boolean
         forge: { [mcversion: string]: ForgeWebPage }
@@ -33,6 +57,9 @@ export declare namespace VersionModule {
         minecraftVersion: (mcversion: string) => VersionMeta | undefined,
         minecraftStatuses: { [minecraftVersion: string]: Status };
 
+        /**
+         * Get the forge webpage info by a minecraft version
+         */
         forgeVersionsOf: (mcversion: string) => ForgeWebPage | undefined;
         forgeLatestOf: (mcversion: string) => ForgeWebPage.Version | undefined;
         forgeRecommendedOf: (mcversion: string) => ForgeWebPage.Version | undefined;
@@ -49,7 +76,7 @@ export declare namespace VersionModule {
         refreshingForge(state: State, refreshing: boolean): void;
         refreshingLiteloader(state: State, refreshing: boolean): void;
 
-        localVersions(state: State, local: LocalVersion[]): void;
+        localVersions(state: State, local: ResolvedVersion[]): void;
         minecraftMetadata(state: State, metadatas: Version.MetaContainer): void;
         forgeMetadata(state: State, metadatas: ForgeWebPage): void;
         liteloaderMetadata(state: State, metadatas: LiteLoader.VersionMetaList): void;
@@ -60,11 +87,14 @@ export declare namespace VersionModule {
         refresh(context: C): Promise<void>
 
         refreshVersions(context: C): Promise<void>
+        /**
+         * Request minecraft version list and cache in to store and disk.
+         */
         refreshMinecraft(context: C): Promise<void>
         refreshForge(context: C): Promise<void>
         refreshLiteloader(context: C): Promise<void>
 
-        resolveVersion(context: C, version: Pick<LocalVersion, 'minecraft' | 'forge' | 'liteloader' | 'folder'>): Promise<string>
+        resolveVersion(context: C, version: Pick<ResolvedVersion, 'minecraft' | 'forge' | 'liteloader' | 'folder'>): Promise<string>
 
         installLibraries(context: C, payload: { libraries: Library[] }): Promise<TaskHandle>;
         installAssets(context: C, version: string): Promise<TaskHandle>
