620f277c4f121fe4bbbdf6563e00f2489c0b1080
ci010 <cijhn@hotmail.com>
2019-06-14 19:19:31 +0800

    style: finalize the setting pages design
    it still needs to fix detail problem
    
    
    Former-commit-id: 288aa54150fe1c5a5aeb084376fd23cdde2a69e1

--- src/renderer/windows/main/ForgeVersionList.vue
+++ src/renderer/windows/main/ForgeVersionList.vue
@@ -1,28 +1,50 @@
 <template>
-	<v-list dark style="overflow-y: scroll; scrollbar-width: 0;">
+	<v-list v-if="fullVersionList.length !== 0" dark style="overflow-y: scroll; scrollbar-width: 0; background-color: transparent;">
+		<v-list-tile>
+			<v-checkbox v-model="recommendedAndLatestOnly" :label="$t('forge.recommendedAndLatestOnly')"></v-checkbox>
+			<v-spacer></v-spacer>
+			<v-checkbox v-model="showBuggy" :label="$t('forge.showBuggy')"></v-checkbox>
+		</v-list-tile>
+		<v-divider dark></v-divider>
 		<v-list-tile ripple @click="selectVersion(null)">
-			<v-list-tile-title>
-				{{$t('forge.disable')}}
-			</v-list-tile-title>
+			<v-list-tile-avatar>
+				<v-icon> close </v-icon>
+			</v-list-tile-avatar>
+			{{$t('forge.disable')}}
 		</v-list-tile>
 		<template v-for="(item, index) in versions">
 			<v-list-tile ripple :key="index" @click="selectVersion(item)">
-				<v-list-tile-title>
-					<v-icon v-if="iconMapping[item.type]">{{iconMapping[item.type]}}</v-icon>
-					{{ item.version }}
-					<span style="color: grey; font-size: 14px; font-style: italic;">
-						{{item.date}}
-					</span>
-				</v-list-tile-title>
-				<v-list-tile-action style="justify-content: flex-end;">
-					<v-icon v-if="statuses[item.version] !== 'loading'">
-						{{ statuses[item.version] === 'remote' ? 'cloud_download' : 'folder' }}
+				<v-list-tile-avatar>
+					<v-icon v-if="statuses[index] !== 'loading'">
+						{{ statuses[index] === 'remote' ? 'cloud_download' : 'folder' }}
 					</v-icon>
 					<v-progress-circular v-else :width="2" :size="24" indeterminate></v-progress-circular>
+				</v-list-tile-avatar>
+
+				<v-list-tile-title>
+					{{ item.version }}
+				</v-list-tile-title>
+				<v-list-tile-sub-title v-html="item.date"></v-list-tile-sub-title>
+
+				<v-list-tile-action style="justify-content: flex-end;">
+					<v-chip label v-if="item.type !== 'common'" :color="item.type === 'recommended'?'green': ''">
+						{{item.type}}
+					</v-chip>
+					<!-- <v-icon v-if="iconMapping[item.type]">{{iconMapping[item.type]}}</v-icon> -->
 				</v-list-tile-action>
 			</v-list-tile>
 		</template>
 	</v-list>
+	<v-container v-else fill-height>
+		<v-layout align-center justify-center row fill-height>
+			<v-flex shrink xs5 tag="h1" class="white--text">
+				<v-btn large>
+					<v-icon left> refresh </v-icon>
+					{{$t('forge.noVersion', mcversion)}}
+				</v-btn>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
@@ -30,34 +52,36 @@ export default {
   data: () => ({
     iconMapping: {
       buggy: 'bug_report',
-      recommended: 'star_rate',
+      recommended: 'star',
       latest: 'fiber_new'
-    }
+    },
+    showBuggy: false,
+    recommendedAndLatestOnly: true,
   }),
   props: {
-    showBuggy: {
-      type: Boolean,
-      default: false,
-    },
     filterText: {
       type: String,
       default: '',
+    },
+    mcversion: {
+      type: String,
+      default: undefined
     },
   },
   computed: {
     statuses() {
       return this.$repo.getters['forgeStatuses'];
     },
+    fullVersionList() {
+      const mcversion = this.mcversion || this.$repo.getters['currentVersion'].minecraft;
+      const ver = this.$repo.state.version.forge[mcversion];
+      return ver ? ver.versions : [];
+    },
     versions() {
-      const mcversion = this.$repo.getters['currentVersion'].minecraft;
-      const ver = this.$repo.getters.forgeVersionsOf(mcversion);
-      let result = [];
-      if (ver) {
-        result = ver.versions
-          .filter(version => this.showBuggy || version.type !== 'buggy')
-          .filter(version => version.version.indexOf(this.filterText) !== -1);
-      }
-      return result;
+      return this.fullVersionList
+        .filter(version => !this.recommendedAndLatestOnly || version.type === 'recommended' || version.type === 'latest')
+        .filter(version => this.showBuggy || version.type !== 'buggy')
+        .filter(version => version.version.indexOf(this.filterText) !== -1);
     },
   },
   methods: {
