12ef091a0cd34cbd35906f81e6c85409d10731a8
ci010 <cijhn@hotmail.com>
2019-06-14 12:46:25 +0800

    fix: wrong import from renderer side
    Former-commit-id: 533192470d0772584e183fbdbff5116697541665

--- src/main/store/plugins/index.js
+++ src/main/store/plugins/index.js
@@ -1,5 +1,5 @@
 /**
- * The file enables `@/store/index.js` to import all vuex modules
+ * The file enables `store/index.js` to import all vuex modules
  * in a one-shot manner. There should not be any reason to edit this file.
  */
 
