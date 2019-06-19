ba17c63884c3118802a63ac4c8c6814aa8184743
ci010 <cijhn@hotmail.com>
2019-06-19 23:27:32 +0800

    feat: support bangbangAPI jre download

--- src/main/utils/jre.js
+++ src/main/utils/jre.js
@@ -1,18 +1,20 @@
 import { net, app } from 'electron';
 import os from 'os';
-import { promises as fs, createReadStream, existsSync } from 'fs';
+import { promises as fs, createReadStream, existsSync, mkdirSync } from 'fs';
 import { Utils } from 'ts-minecraft';
-import path from 'path';
+import path, { join } from 'path';
 import Task from 'treelike-task';
 import { createExtractStream } from 'yauzlw';
 import { createDecompressor } from 'lzma-native';
 import { createHash } from 'crypto';
+import { downloadFile, downloadFileWork } from 'ts-minecraft/dest/libs/utils/network';
+import { exec } from 'child_process';
 import { ensureDir, ensureFile } from './fs';
 
 /**
  * @param {Task.Context} context 
  */
-export default async function officialEndpoint(context) {
+export async function officialEndpoint(context) {
     const root = app.getPath('userData');
     function resolveArch() {
         switch (os.arch()) {
@@ -83,4 +85,57 @@ export default async function officialEndpoint(context) {
         await fs.unlink(dest);
     });
     return version;
+}
+
+
+/**
+ * @param {Task.Context} context 
+ */
+export async function bangbangAPI(context) {
+    const x64 = os.arch() === 'x64';
+    const platform = os.platform();
+    const isWin = platform === 'win32';
+    const isMac = platform === 'darwin';
+    const isLinux = platform === 'linux';
+    function resolveJava() {
+        switch (platform) {
+            case 'darwin': return 'jre_mac.dmg';
+            case 'win32': return x64 ? 'jre_x64.exe' : 'jre_x86.exe';
+            case 'linux': return x64 ? 'jre_x64.tar.gz' : 'jre_x86.tar.gz';
+            default: return '';
+        }
+    }
+    const filename = resolveJava();
+    const root = app.getPath('userData');
+    const javaRoot = path.resolve(root, 'jre');
+    const destination = path.resolve(root, 'temp', filename);
+    await context.execute('download', downloadFileWork({
+        url: `http://bmclapi2.bangbang93.com/java/${filename}`,
+        destination,
+    }));
+
+    function exec_(cmd, option = {}) {
+        return new Promise((resolve, reject) => {
+            exec(cmd, option, (err, stdout, stderr) => {
+                if (err) { reject(err); } else {
+                    resolve(stdout);
+                }
+            });
+        });
+    }
+    switch (platform) {
+        case 'darwin':
+            await fs.copyFile(join(__static, 'mac-jre-installer.sh'), join(root, 'temp', 'mac-jre-installer.sh'));
+            mkdirSync(join(root, 'jre'));
+            await exec_(join(root, 'temp', 'mac-jre-installer.sh'), { cwd: root });
+            break;
+        case 'win32':
+            await exec_([destination, `INSTALLDIR=${javaRoot}`, 'STATIC=1', 'INSTALL_SILENT=1', 'SPONSORS=0'].join(' '));
+            break;
+        case 'linux':
+            await exec_(`tar xvzf ${destination} -C ${join(root, 'jre')}`, { cwd: root });
+            break;
+        default:
+            break;
+    }
 }
