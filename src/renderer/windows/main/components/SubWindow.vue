804c164e25d6e59ff6dd67616faeee3f9fc5e2b2
ci010 <cijhn@hotmail.com>
2019-06-20 20:52:47 +0800

    refactor(ui): make bottom bar doesnt follow page

--- src/renderer/windows/main/components/SubWindow.vue
+++ src/renderer/windows/main/components/SubWindow.vue
@@ -1,20 +1,9 @@
 <template>
-	<div style="height: 100%">
-		<v-window :value="value" style="height: 100%">
-			<v-window-item ref="items" v-for="(c, i) in components" :key="i" style="height: 100%">
-				<component :selected="i===value && selected" :is="c" @goto="$emit('goto', $event)"></component>
-			</v-window-item>
-		</v-window>
-
-		<v-layout style="position: absolute; z-index: 2; bottom: 10px; width: 100%" align-center
-		  justify-center>
-			<v-item-group class="shrink" mandatory tag="v-flex" :value="value" @value="$emit('value', $event)">
-				<v-item v-for="(c, i) in components" :key="i">
-					<v-icon dark slot-scope="{ active, toggle }" :color="active ? 'primary': ''" @click="toggle">minimize</v-icon>
-				</v-item>
-			</v-item-group>
-		</v-layout>
-	</div>
+	<v-window :value="value" style="height: 100%">
+		<v-window-item ref="items" v-for="(c, i) in components" :key="i" style="height: 100%">
+			<component :selected="i===value && selected" :is="c" @goto="$emit('goto', $event)"></component>
+		</v-window-item>
+	</v-window>
 </template>
 
 <script>
