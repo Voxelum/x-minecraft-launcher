4915c6b2d34c67b85aac227b832fcc5580175003
ci010 <cijhn@hotmail.com>
2019-07-03 13:50:18 +0800

    fix(lint): fix code linter error

--- src/main/setup.d.ts
+++ src/main/setup.d.ts
@@ -2,17 +2,17 @@ import { CustomEvents } from './ipc';
 import { BrowserWindow, BrowserWindowConstructorOptions, Tray, Dock } from 'electron';
 import { Repo } from '../universal/store/store';
 interface Instance {
-    requestFocus(): void
-    dispose(): void
-    listeners: { [channel: string]: Function[] }
+    requestFocus(): void;
+    dispose(): void;
+    listeners: { [channel: string]: Function[] };
 }
 interface Hook {
-    requestFocus(): void
-    dispose(): void
+    requestFocus(): void;
+    dispose(): void;
 }
 interface Context {
     createWindow(url: string, option: BrowserWindowConstructorOptions): BrowserWindow;
-    ipcMain: CustomEvents,
+    ipcMain: CustomEvents;
     configTray(func: (tray: Tray) => void): this;
     configDock(func: (dock: Dock) => void): this;
 }
