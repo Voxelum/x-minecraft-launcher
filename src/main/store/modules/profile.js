1dd3b978b38579537e4a1269d131703e9cf4afa7
ci010 <cijhn@hotmail.com>
2019-07-14 22:57:43 +0800

    fix: don't save server status

--- src/main/store/modules/profile.js
+++ src/main/store/modules/profile.js
@@ -161,6 +161,7 @@ const mod = {
                             worlds: undefined,
                             problems: undefined,
                             refreshing: undefined,
+                            status: undefined,
                         },
                     });
                     break;
