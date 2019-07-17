95610811588f2c6c453cb92d2f89646887febaea
ci010 <cijhn@hotmail.com>
2019-07-18 02:07:04 +0800

    feat(ui): beautify the curseforge entry page

--- src/renderer/windows/main/CurseforgePage.vue
+++ src/renderer/windows/main/CurseforgePage.vue
@@ -1,43 +1,55 @@
 <template>
-  <v-container grid-list-xs fill-height>
+  <v-container grid-list-md fill-height>
     <v-layout row wrap>
       <v-flex tag="h1" class="white--text" xs12>
         <span class="headline">{{ $tc('curseforge.name', 2) }}</span>
       </v-flex>
-      <v-flex>
-        <v-card v-ripple draggable hover replace to="/curseforge/mc-mods"> 
+      <v-flex xs6>
+        <v-card v-ripple hover replace to="/curseforge/mc-mods">
+          <v-card-media>
+            <v-img :src="mods" />
+          </v-card-media>
           <v-card-title>
-            {{ $t('curseforge.mc-mods.name') }}
+            <h2>
+              {{ $t('curseforge.mc-mods.name') }}
+            </h2>
           </v-card-title>
           <v-card-text>
             {{ $t('curseforge.mc-mods.description') }}
           </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex>
-        <v-card v-ripple draggable hover replace to="/curseforge/texture-packs">
+      <v-flex xs6>
+        <v-card v-ripple hover replace to="/curseforge/texture-packs">
+          <v-card-media>
+            <v-img :src="resourcepack" />
+          </v-card-media>
           <v-card-title>
-            {{ $t('curseforge.texture-packs.name') }}
+            <h2>
+              {{ $t('curseforge.texture-packs.name') }}
+            </h2>
           </v-card-title>
           <v-card-text>
             {{ $t('curseforge.texture-packs.description') }}
           </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex>
-        <v-card v-ripple draggable hover replace to="/curseforge/worlds">
+      <v-flex xs6>
+        <v-card disabled>
           <v-card-title>
             {{ $t('curseforge.worlds.name') }}
+            敬请期待
           </v-card-title>
           <v-card-text>
             {{ $t('curseforge.worlds.description') }}
           </v-card-text>
         </v-card>
       </v-flex>
-      <v-flex>
-        <v-card v-ripple draggable hover replace to="/curseforge/modpacks">
+      <v-flex xs6>
+        <v-card disabled>
           <v-card-title>
             {{ $t('curseforge.modpacks.name') }}
+            敬请期待
           </v-card-title>
           <v-card-text>
             {{ $t('curseforge.modpacks.description') }}
@@ -49,10 +61,16 @@
 </template>
 
 <script>
-export default {
+import resourcepack from 'renderer/assets/curseforge.resourcepack.png';
+import mods from 'renderer/assets/curseforge.mods.png';
 
+export default {
+  data: () => ({
+    resourcepack,
+    mods,
+  }),
 };
 </script>
 
-<style>
+<style scoped=true>
 </style>
