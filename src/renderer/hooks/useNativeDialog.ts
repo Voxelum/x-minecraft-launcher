b09979edce3eb442ed1c995503a61c8664add1d6
ci010 <cijhn@hotmail.com>
2019-10-10 20:26:21 +0800

    refactor: extract native dialog

--- src/renderer/hooks/useNativeDialog.ts
+++ src/renderer/hooks/useNativeDialog.ts
@@ -1,5 +1,22 @@
-import { remote } from "electron";
+import { remote, OpenDialogOptions, SaveDialogOptions } from "electron";
+
+const dialog = {
+    showOpenDialog(options: OpenDialogOptions) {
+        return new Promise<{ filePaths: string[], bookmarks?: string[] }>((resolve, reject) => {
+            remote.dialog.showOpenDialog(options, (filePaths, bookmarks) => {
+                resolve({ filePaths: filePaths || [], bookmarks });
+            })
+        });
+    },
+    showSaveDialog(options: SaveDialogOptions) {
+        return new Promise<{ filename?: string, bookmark?: string }>((resolve, reject) => {
+            remote.dialog.showSaveDialog(options, (filename, bookmark) => {
+                resolve({ filename, bookmark });
+            })
+        });
+    }
+}
 
 export function useNativeDialog() {
-    return remote.dialog;
+    return dialog;
 }
