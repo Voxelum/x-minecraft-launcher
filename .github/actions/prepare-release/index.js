cf4b8ecf1977ce03239e965b7d04a5742923fa72
ci010 <cijhn@hotmail.com>
2020-05-08 19:31:43 +0800

    chore: restore to master

--- .github/actions/prepare-release/index.js
+++ .github/actions/prepare-release/index.js
@@ -1,26 +1,25 @@
 const fs = require('fs');
 const core = require('@actions/core');
 
-function genBody(b) {
-    let body = ``;
-    body += b;
-    return body;
-}
 async function main(output) {
     const { version } = JSON.parse(fs.readFileSync(`package.json`).toString());
     const changelog = fs.readFileSync('CHANGELOG.md').toString();
     const changelogLines = changelog.split('\n')
 
-    const start = changelogLines.findIndex(l => l.startsWith('## '));
-    const end = changelogLines.slice(start + 1).findIndex(l => l.startsWith('## '))
-    const body = changelogLines.slice(start, end).join('\n') + '\n';
+    const start = changelogLines.findIndex(l => l.startsWith(`## [${version}]`));
+
+    let body = 'Manual Release';
+    if (start !== -1) {
+        const end = changelogLines.slice(start + 1).findIndex(l => l.startsWith('## '))
+        body = changelogLines.slice(start, end).join('\n') + '\n';
+    }
 
     console.log(body);
 
-    output('release', `Release ${version}`);
-    output('body', genBody(body));
+    output('release', `v${version}`);
+    output('body', body);
     output('tag', `v${version}`);
-    output('prerelease', false);
+    output('prerelease', true);
     output('draft', false);
 }
 
