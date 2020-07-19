9f3210fc23fa8a8a8e4e7993b17786170a82ecbb
ci010 <cijhn@hotmail.com>
2020-07-19 18:12:14 +0800

    fix: diagnose reactivity

--- src/renderer/hooks/useSemaphore.ts
+++ src/renderer/hooks/useSemaphore.ts
@@ -1,9 +1,21 @@
 import { SERVICES_SEMAPHORE_KEY } from '@/constant';
-import { inject, computed } from '@vue/composition-api';
+import { inject, computed, set } from '@vue/composition-api';
 
 export function useBusy(semaphore: string | Function) {
+    const sem = useSemaphore(semaphore);
+    return computed(() => sem.value > 0);
+}
+
+export function useSemaphore(semaphore: string | Function) {
     const sems = inject(SERVICES_SEMAPHORE_KEY);
     if (!sems) throw new Error();
     const key = typeof semaphore === 'function' ? semaphore.name : semaphore;
-    return computed(() => sems[key] > 0);
+    return computed(() => {
+        let value = sems[key];
+        if (typeof value === 'undefined') {
+            set(sems, key, 0);
+        }
+        value = sems[key];
+        return value;
+    });
 }
