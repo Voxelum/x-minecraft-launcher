99a309ff9e484305232e1ee907bf5a158ecb6b54
ci010 <cijhn@hotmail.com>
2020-07-20 16:23:46 +0800

    refactor: min the bundle size

--- src/renderer/windows/logger/index.ts
+++ src/renderer/windows/logger/index.ts
@@ -1,11 +1,22 @@
-import VueCompositionApi, { createElement as h } from '@vue/composition-api';
+import VueCompositionApi, { h } from '@vue/composition-api';
 
-import Vuetify from 'vuetify';
 import colors from 'vuetify/es5/util/colors';
-import 'vuetify/dist/vuetify.css';
+// import 'vuetify/dist/vuetify.css';
 import '@/assets/google.font.css';
 
 import Vue from 'vue';
+
+import 'vuetify/dist/vuetify.min.css';
+import Vuetify from 'vuetify/es5/components/Vuetify';
+import VCard from 'vuetify/es5/components/VCard';
+import VList from 'vuetify/es5/components/VList';
+import VChip from 'vuetify/es5/components/VChip';
+import VApp from 'vuetify/es5/components/VApp';
+import VBtn from 'vuetify/es5/components/VBtn';
+import VIcon from 'vuetify/es5/components/VIcon';
+import VGrid from 'vuetify/es5/components/VGrid';
+import VToolbar from 'vuetify/es5/components/VToolbar';
+
 import provideElectron from '@/providers/provideElectron';
 import Logger from './Logger.vue';
 
@@ -17,6 +28,16 @@ Vue.use(Vuetify, {
         primary: colors.green,
         // secondary: colors.green,
         accent: colors.green.accent3,
+    },
+    components: {
+        ...VCard.$_vuetify_subcomponents,
+        ...VList.$_vuetify_subcomponents,
+        ...VGrid.$_vuetify_subcomponents,
+        VChip,
+        VApp,
+        VBtn,
+        VIcon,
+        ...VToolbar.$_vuetify_subcomponents,
     },
 });
 
