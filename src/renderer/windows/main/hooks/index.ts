51b2852743d29a27f8773a95339a7a7a11716c19
ci010 <cijhn@hotmail.com>
2020-11-01 20:01:48 +0800

    refactor: move the routing things out

--- src/renderer/windows/main/hooks/index.ts
+++ src/renderer/windows/main/hooks/index.ts
@@ -2,6 +2,5 @@ export * from './useNotifier';
 export * from './useDialog';
 export * from './useSearch';
 export * from './useIssues';
-export * from './useAsyncRoute';
 export * from './useContextMenu';
 export * from './useCurseforgeRoute';
