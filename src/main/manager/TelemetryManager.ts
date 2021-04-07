76af4e8b26fa7eee70819d2cb613f138f3c0cdd3
ci010 <cijhn@hotmail.com>
2021-04-07 18:29:59 +0800

    fix: correct assign telemetry user/session

--- src/main/manager/TelemetryManager.ts
+++ src/main/manager/TelemetryManager.ts
@@ -1,12 +1,41 @@
-import { APP_INSIGHT_KEY, IS_DEV } from '/@main/constant'
-import { defaultClient, DistributedTracingModes, setup, TelemetryClient } from 'applicationinsights'
+import { Contracts, defaultClient, DistributedTracingModes, setup } from 'applicationinsights'
+import { readFile, writeFile } from 'fs-extra'
+import { join } from 'path'
+import { v4 } from 'uuid'
 import { Manager } from '.'
+import { APP_INSIGHT_KEY, IS_DEV } from '/@main/constant'
 
 export default class TelemetryManager extends Manager {
-  setup() {
+  private contract = new Contracts.ContextTagKeys()
+
+  private sessionId: string = v4()
+
+  async setup() {
     if (IS_DEV) {
       return
     }
+
+    const clientSessionFile = join(this.app.appDataPath, 'client_session')
+    try {
+      const session = await readFile(clientSessionFile).then(b => b.toString())
+      this.sessionId = session
+    } catch {
+      this.sessionId = v4()
+      await writeFile(clientSessionFile, this.sessionId)
+    }
+
+    const tags = defaultClient.context.tags
+    tags[this.contract.sessionId] = this.sessionId
+    tags[this.contract.userId] = this.sessionId
+    tags[this.contract.applicationVersion] = `${this.app.version}#${process.env.BUILD_NUMBER}`
+
+    process.on('uncaughtException', (e) => {
+      defaultClient.trackException({ exception: e })
+    })
+    process.on('unhandledRejection', (e) => {
+      defaultClient.trackException({ exception: e as any }) // the applicationinsights will convert it to error automatically
+    })
+
     setup(APP_INSIGHT_KEY)
       .setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C)
       .setAutoCollectExceptions(true)
