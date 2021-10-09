3ea376e4ad26d5e22ffa946520f0d7f0a8f0525c
ci010 <cijhn@hotmail.com>
2021-10-09 20:23:45 +0800

    fix: add typing to vue template

--- src/renderer/component.d.ts
+++ src/renderer/component.d.ts
@@ -3,12 +3,18 @@
 //   }
 // }
 
-// declare module '@vue/runtime-dom' {
-// 	export * from '@vue/runtime-dom/dist/runtime-dom'
-// 	export { defineComponent, PropType } from '@vue/composition-api'
-// }
+declare module '@vue/runtime-dom' {
+// export * from '@vue/runtime-dom/dist/runtime-dom'
+// export { defineComponent, PropType } from '@vue/composition-api'
+  export interface ComponentPublicInstance {
+    $t(key: string, ...args: any[]): string
+    $tc(key: string, count: number): string
+  }
+}
 
 declare module '@vue/composition-api' {
   export * from '@vue/runtime-dom'
   export * from '@vue/composition-api/dist/index'
 }
+
+export {}
