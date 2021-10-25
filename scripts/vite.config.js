b8094aca7f2216e6118e2831792aa775cf7bb232
ci010 <cijhn@hotmail.com>
2021-10-26 01:31:27 +0800

    chore: update dependencies

--- scripts/vite.config.js
+++ scripts/vite.config.js
@@ -1,36 +1,46 @@
-const { join, resolve } = require('path')
-const { external } = require('../package.json')
+const { join, resolve } = require("path");
+const { external } = require("../package.json");
 // const { default: vue } = require('@vitejs/plugin-vue')
-const { createVuePlugin } = require('vite-plugin-vue2')
-const { readdirSync } = require('fs')
+const { createVuePlugin } = require("vite-plugin-vue2");
+const { readdirSync } = require("fs");
+const { VitePWA } = require("vite-plugin-pwa");
 
-const entries = readdirSync(join(__dirname, '../src/renderer')).filter(f => f.endsWith('.html'))
-  .map(f => join(__dirname, '../src/renderer', f))
+const entries = readdirSync(join(__dirname, "../src/renderer"))
+    .filter((f) => f.endsWith(".html"))
+    .map((f) => join(__dirname, "../src/renderer", f));
 
 /**
  * Vite shared config, assign alias and root dir
  * @type {import('vite').UserConfig}
  */
 const config = {
-  root: join(__dirname, '../src/renderer'),
-  base: '', // has to set to empty string so the html assets path will be relative
-  build: {
-    rollupOptions: {
-      input: entries
+    root: join(__dirname, "../src/renderer"),
+    base: "", // has to set to empty string so the html assets path will be relative
+    build: {
+        rollupOptions: {
+            input: entries,
+        },
+        outDir: resolve(__dirname, "../dist/renderer"),
+        assetsInlineLimit: 0,
     },
-    outDir: resolve(__dirname, '../dist/renderer'),
-    assetsInlineLimit: 0
-  },
-  resolve: {
-    alias: {
-      '/@shared': join(__dirname, '../src/shared'),
-      '/@': join(__dirname, '../src/renderer')
-    }
-  },
-  optimizeDeps: {
-    exclude: external
-  },
-  plugins: [createVuePlugin()]
-}
+    resolve: {
+        alias: {
+            "/@shared": join(__dirname, "../src/shared"),
+            "/@": join(__dirname, "../src/renderer"),
+        },
+    },
+    optimizeDeps: {
+        exclude: external,
+    },
+    plugins: [
+        createVuePlugin(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "x-minecraft-launcher",
+            },
+        }),
+    ],
+};
 
-module.exports = config
+module.exports = config;
