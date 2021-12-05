ba3f2058df11b41536408d36febffd3f58df4a00
ci010 <cijhn@hotmail.com>
2021-12-05 13:51:15 +0800

    refactor: add missing import/assets & fix type

--- src/renderer/hooks/useParticle.ts
+++
@@ -1,9 +0,0 @@
-import { useLocalStorageCacheBool, useLocalStorageCacheStringValue } from './useCache'
-
-
-export function useParticle() {
-  const particleMode = useLocalStorageCacheStringValue<ParticleMode>('particleMode', ParticleMode.PUSH)
-  return {
-    particleMode,
-  }
-}
