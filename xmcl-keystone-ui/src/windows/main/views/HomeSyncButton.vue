980458feee3578c2abebb8687f153476256e9e3c
CI010 <cijhn@hotmail.com>
2022-04-13 18:44:52 +0800

    feat: add instance sync from server

---
+++ xmcl-keystone-ui/src/windows/main/views/HomeSyncButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-tooltip
+    v-if="state.instance.fileApi"
+    top
+  >
+    <template #activator="{ on }">
+      <v-badge
+        right
+        color="primary"
+        overlap
+        :value="true"
+      >
+        <template #badge>
+          <span>{{ 1 }}</span>
+        </template>
+        <v-btn
+          icon
+          :loading="checkingUpdate"
+          @click="show()"
+          v-on="on"
+        >
+          <v-icon>refresh</v-icon>
+        </v-btn>
+      </v-badge>
+    </template>
+    更新实例
+  </v-tooltip>
+</template>
+<script lang="ts" setup>
+import { InstanceIOServiceKey, InstanceServiceKey } from '@xmcl/runtime-api'
+import { useDialog } from '../composables/dialog'
+import { useBusy, useService } from '/@/composables'
+
+const { show } = useDialog('instance-sync')
+const checkingUpdate = useBusy('getInstanceUpdate()')
+const { state } = useService(InstanceServiceKey)
+
+</script>
