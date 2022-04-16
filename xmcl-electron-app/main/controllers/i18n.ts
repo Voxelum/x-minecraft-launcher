7ac6dea32c733b9ea3637de7f4a0adb4ad33b98b
ci010 <cijhn@hotmail.com>
2022-04-16 17:11:09 +0800

    fix: i18n list sometime not show

--- xmcl-electron-app/main/controllers/i18n.ts
+++ xmcl-electron-app/main/controllers/i18n.ts
@@ -1,24 +1,16 @@
 import Controller from '@/Controller'
-import { BaseService, BaseServiceKey } from '@xmcl/runtime-api'
+import { BaseService } from '@xmcl/runtime'
 import { ControllerPlugin } from './plugin'
 
 export const i18n: ControllerPlugin = function (this: Controller) {
   this.app.once('engine-ready', () => {
+    const baseService = this.app.serviceManager.getOrCreateService(BaseService)
+    baseService.state.localesSet(['en', 'zh-CN', 'zh-TW', 'ru'])
+    this.app.log(`Set locale for the app ${baseService.state.locales}`)
+    this.i18n.use(baseService.state.locale)
     this.app.serviceStateManager.subscribe('localeSet', (l) => {
       this.i18n.use(l)
       this.app.log(`Set locale for the app ${l}`)
     })
-  })
-  this.app.on('service-ready', (serv) => {
-    if (serv.name === BaseServiceKey) {
-      const baseService = serv as any as BaseService
-      if (baseService) {
-        baseService.state.localesSet(['en', 'zh-CN', 'zh-TW', 'ru'])
-        this.app.log(`Set locale for the app ${baseService.state.locales}`)
-        this.i18n.use(baseService.state.locale)
-      } else {
-        this.app.log(`Cannot find base service via ${BaseServiceKey}`)
-      }
-    }
   })
 }
