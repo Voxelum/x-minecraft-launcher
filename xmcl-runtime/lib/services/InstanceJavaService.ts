0d822ab13808fe710b9255b88ef1c116a27b9163
CI010 <cijhn@hotmail.com>
2022-05-01 14:33:29 +0800

    refactor: java & version selection

--- xmcl-runtime/lib/services/InstanceJavaService.ts
+++ xmcl-runtime/lib/services/InstanceJavaService.ts
@@ -1,20 +1,18 @@
-import { ResolvedVersion } from '@xmcl/core'
-import { IncompatibleJavaIssueKey, InstanceJavaService as IInstanceJavaService, InstanceJavaServiceKey, InstanceJavaState, InvalidJavaIssueKey, IssueReport, Java, JavaRecord, MissingJavaIssueKey, parseVersion, IssueReportBuilder } from '@xmcl/runtime-api'
+import { IncompatibleJavaIssueKey, InstanceJavaService as IInstanceJavaService, InstanceJavaServiceKey, InstanceJavaState, InvalidJavaIssueKey, IssueReportBuilder, Java, JavaCompatibleState, JavaRecord, MissingJavaIssueKey, parseVersion } from '@xmcl/runtime-api'
 import { relative } from 'path'
 import LauncherApp from '../app/LauncherApp'
-import { missing } from '../util/fs'
 import { DiagnoseService } from './DiagnoseService'
 import { InstanceService } from './InstanceService'
 import { InstanceVersionService } from './InstanceVersionService'
 import { JavaService } from './JavaService'
-import { AbstractService, Inject, Singleton, StatefulService } from './Service'
-import { VersionService } from './VersionService'
+import { Inject, Singleton, StatefulService } from './Service'
 
 interface VersionPreference {
   match: (v: Java) => boolean
   okay: (v: Java) => boolean
   requirement: string
 }
+
 export class InstanceJavaService extends StatefulService<InstanceJavaState> implements IInstanceJavaService {
   constructor(app: LauncherApp,
     @Inject(InstanceService) private instanceService: InstanceService,
@@ -41,8 +39,8 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
         if (matchingJava) {
           await this.instanceService.editInstance({ java: matchingJava })
         } else {
-          if (issue.targetVersion) {
-            await this.javaService.installDefaultJava(issue.targetVersion)
+          if (issue.recommendedDownload) {
+            await this.javaService.installDefaultJava(issue.recommendedDownload)
           }
         }
       },
@@ -52,36 +50,47 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
       id: MissingJavaIssueKey,
       fix: async (issue) => {
         const missingJavaIssue = issue
-        if (missingJavaIssue.targetVersion) {
-          this.javaService.installDefaultJava(missingJavaIssue.targetVersion)
+        if (missingJavaIssue.recommendedDownload) {
+          this.javaService.installDefaultJava(missingJavaIssue.recommendedDownload)
         }
       },
     })
 
-    new Promise<void>((resolve) => {
-      let state = 0
-      const listener = (serv: AbstractService) => {
-        if (serv instanceof InstanceService || serv instanceof JavaService || serv instanceof VersionService) {
-          state += 1
-          if (state === 3) {
-            this.app.removeListener('service-ready', listener)
-            resolve()
+    Promise.all([instanceService.initialize(), javaService.initialize(), instanceVersionService.initialize()]).then(() => {
+      this.storeManager
+        .subscribe('instanceVersion', (ver) => {
+          this.updateJava()
+        })
+        .subscribe('javaRemove', (j) => {
+          if (j.path === this.state.java?.path) {
+            this.updateJava()
           }
-        }
-      }
-      this.app.on('service-ready', listener)
-    }).then(() => {
-      this.storeManager.subscribeAll(['javaUpdate', 'javaRemove', 'instanceSelect'], async () => {
-        await this.updateJava()
-      })
+        })
+        .subscribe('javaUpdate', (j) => {
+          if (this.state.java) {
+            if (j instanceof Array) {
+              if (j.some(java => java.path === this.state.java?.path)) {
+                this.updateJava()
+              }
+            } else if (j.path === this.state.java?.path) {
+              this.updateJava()
+            }
+          } else {
+            this.updateJava()
+          }
+        })
+        .subscribe('instanceEdit', async (payload) => {
+          if (payload.path === this.instanceService.state.path) {
+            if ('runtime' in payload) {
+              // wait instanceVersion change
+              return
+            }
+            if ('java' in payload) {
+              await this.updateJava()
+            }
+          }
+        })
       this.updateJava()
-    })
-    this.storeManager.subscribe('instanceEdit', async (payload) => {
-      if (payload.path === this.instanceService.state.path) {
-        if ('java' in payload || 'runtime' in payload) {
-          await this.updateJava()
-        }
-      }
     })
   }
 
@@ -104,18 +113,17 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
     bestMatched.sort((a, b) => isUnderPath(a.path) ? -1 : 1)
     fine.sort((a, b) => isUnderPath(a.path) ? -1 : 1)
     if (bestMatched.length > 0) {
-      return [bestMatched[0], 0] as const
+      return [bestMatched[0], JavaCompatibleState.Matched] as const
     }
     if (fine.length > 0) {
-      return [fine[0], 1] as const
+      return [fine[0], JavaCompatibleState.MayIncompatible] as const
     }
-    return [bad[0], 2] as const
+    return [bad[0], JavaCompatibleState.VeryLikelyIncompatible] as const
   }
 
   /**
    * Get java for current active instance.
    * It can only return the java with the version matched with launching requirement.
-   * @param validOnly The param to determine if this will only return a valid java
    */
   @Singleton()
   async updateJava() {
@@ -125,9 +133,10 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
       this.log('Diagnose java')
       // instance related info
       const instance = this.instanceService.state.instance
-      const { minecraft, forge, java: javaPath } = instance.runtime
+      const { minecraft, forge } = instance.runtime
+      const javaPath = instance.java
       const selectedVersion = this.instanceVersionService.state.version
-      const javaVersion = selectedVersion?.javaVersion
+      let javaVersion = selectedVersion?.javaVersion
       const resolvedMcVersion = parseVersion(minecraft)
       const minecraftMinor = resolvedMcVersion.minorVersion!
 
@@ -135,71 +144,18 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
 
       const allJava = this.javaService.state.all
 
-      let instanceJava: JavaRecord | undefined
-
       const getBuilderNumber = (v: string) => {
         const [, build] = v.split('_')
         const buildNumber = Number(build)
         return buildNumber
       }
 
-      // user assigned java
-      // if (javaPath && javaPath !== '') {
-      //   instanceJava = allJava.find(j => j.path === javaPath)
-
-      //   // pre-check if the user selected path is not in our cache
-      //   if (instanceJava && (!instanceJava.path || !instanceJava.valid || await this.javaService.validateJavaPath(instanceJava.path))) {
-      //     builder.set(InvalidJavaIssueKey, { path: instanceJava.path })
-      //     this.diagnoseService.report(builder.build())
-      //     return
-      //   }
-
-      //   if (!instanceJava) {
-      //     // no java
-      //     builder.set(MissingJavaIssueKey, { targetVersion: undefined })
-      //     this.diagnoseService.report(builder.build())
-      //     return
-      //   }
-
-      //   if (instanceJava.majorVersion > 8 && javaVersion.majorVersion === 8) {
-      //     if (!resolvedMcVersion.minorVersion || resolvedMcVersion.minorVersion < 13) {
-      //       // 1.13 below does not support higher java
-      //       builder.set(IncompatibleJavaIssueKey, { java: instanceJava.version, version: minecraft, cause: 'Minecraft', targetVersion: javaVersion })
-      //     } else if (resolvedMcVersion.minorVersion >= 13 && instance.runtime.forge && instanceJava.majorVersion > 10) {
-      //       if (resolvedMcVersion.minorVersion < 17) {
-      //         builder.set(IncompatibleJavaIssueKey, { java: instanceJava.version, version: instance.runtime.forge, cause: 'MinecraftForge', targetVersion: javaVersion })
-      //       }
-      //     }
-      //   }
-
-      //   if (instanceJava && instanceJava?.majorVersion === 8) {
-      //     const [, build] = instanceJava.version.split('_')
-      //     const buildNumber = Number(build)
-      //     if (buildNumber >= 321 && forge) {
-      //       // forge not compatible with 321 jre
-      //       builder.set(IncompatibleJavaIssueKey, { java: instanceJava.version, version: forge, cause: 'MinecraftForge', targetVersion: javaVersion })
-      //     }
-      //   }
-
-      //   if (javaVersion.majorVersion > 8 && instanceJava) {
-      //     if (javaVersion.majorVersion !== instanceJava.majorVersion) {
-      //       builder.set(IncompatibleJavaIssueKey, { java: instanceJava.version, version: minecraft, cause: 'MinecraftForge', targetVersion: javaVersion })
-      //     }
-      //   }
-      // } else {
-      // user not assigned, calculate from our side
-      let status = 0
-      if (allJava.length === 0) {
-        builder.set(MissingJavaIssueKey, { targetVersion: undefined })
-        this.diagnoseService.report(builder.build())
-        return
-      }
-
       let preferredMatchedVersion: undefined | ((j: Java) => boolean)
 
       if (javaVersion) {
+        const v = javaVersion
         // if it assign version officially, we need to
-        preferredMatchedVersion = (j) => j.majorVersion === javaVersion.majorVersion
+        preferredMatchedVersion = (j) => j.majorVersion === v.majorVersion
       }
       let versionPref: VersionPreference
       // instance version is not installed
@@ -210,6 +166,12 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
           okay: j => j.majorVersion < 8 || j.majorVersion < 11,
           requirement: javaVersion ? `=${javaVersion.majorVersion.toString()}` : '=8',
         }
+        if (!javaVersion) {
+          javaVersion = {
+            component: 'jre-legacy',
+            majorVersion: 8,
+          }
+        }
       } else if (minecraftMinor >= 13 && minecraftMinor < 17) {
         if (instance.runtime.forge) {
           // use java 8 if forge as forge only compatible with jre8
@@ -218,12 +180,24 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
             okay: (j) => j.majorVersion === 8,
             requirement: '<8_321',
           }
+          if (!javaVersion) {
+            javaVersion = {
+              component: 'jre-legacy',
+              majorVersion: 8,
+            }
+          }
         } else {
           // use greater java if no forge
           versionPref = {
             match: preferredMatchedVersion || (j => j.majorVersion >= 8 && j.majorVersion <= 16),
             okay: _ => true,
             requirement: javaVersion ? `=${javaVersion.majorVersion.toString()}` : '>=8,<=16',
+          }
+          if (!javaVersion) {
+            javaVersion = {
+              component: 'jre-legacy',
+              majorVersion: 8,
+            }
           }
         }
       } else {
@@ -233,48 +207,91 @@ export class InstanceJavaService extends StatefulService<InstanceJavaState> impl
           okay: _ => true,
           requirement: javaVersion ? `=${javaVersion.majorVersion.toString()}` : '>=16',
         }
-      }
-
-      if (javaPath && javaPath !== '') {
-        const record = await this.javaService.resolveJava(javaPath)
-        if (!record) {
-          // invalid
-          builder.set(InvalidJavaIssueKey, { path: javaPath })
-        } else {
-          if (versionPref.match(record)) {
-            status = 0
-          } else if (versionPref.okay(record)) {
-            status = 1
-          } else {
-            status = 2
+        if (!javaVersion) {
+          javaVersion = {
+            component: 'java-runtime-alpha',
+            majorVersion: 16,
           }
         }
+      }
+
+      if (allJava.length === 0) {
+        builder.set(MissingJavaIssueKey, {
+          recommendedDownload: javaVersion,
+          requirement: versionPref.requirement,
+          version: selectedVersion?.id || '',
+          minecraft,
+          forge: forge ?? '',
+        })
+        this.state.instanceJava(undefined)
+        this.diagnoseService.report(builder.build())
+        return
+      }
+
+      let resultQuality = JavaCompatibleState.Matched
+      let resultJava: Java
+      const [computedJava, computedQuality] = this.getSortedJava(versionPref)
+
+      const userAssigned = javaPath && javaPath !== ''
+      if (userAssigned) {
+        const record = await this.javaService.resolveJava(javaPath)
+
+        if (!record) {
+          // invalid java
+          builder.set(InvalidJavaIssueKey, {
+            selectedJavaPath: javaPath,
+
+            recommendedDownload: javaVersion,
+            recommendedVersion: computedJava,
+            recommendedLevel: resultQuality,
+
+            requirement: versionPref.requirement,
+            version: selectedVersion?.id || '',
+            minecraft,
+            forge: forge ?? '',
+          })
+
+          this.state.instanceJava({
+            valid: false,
+            path: javaPath,
+            version: '',
+            majorVersion: -1,
+          })
+          this.diagnoseService.report(builder.build())
+          return
+        }
+
+        // check if this version matched
+        if (versionPref.match(record)) {
+          resultQuality = JavaCompatibleState.Matched
+        } else if (versionPref.okay(record)) {
+          resultQuality = JavaCompatibleState.MayIncompatible
+        } else {
+          resultQuality = JavaCompatibleState.VeryLikelyIncompatible
+        }
+        resultJava = record
       } else {
-        [instanceJava, status] = this.getSortedJava(versionPref)
+        resultQuality = computedQuality
+        resultJava = computedJava
       }
 
-      if (status === 1) {
-        // not best match
+      if (resultQuality !== JavaCompatibleState.Matched) {
+        // not the best match
         builder.set(IncompatibleJavaIssueKey, {
+          selectedJava: resultJava,
+
+          recommendedDownload: javaVersion,
+          recommendedVersion: computedJava,
+          recommendedLevel: computedQuality,
+
+          version: selectedVersion?.id || '',
           minecraft,
           forge: instance.runtime.forge || '',
-          java: instanceJava?.path || '',
-          version: selectedVersion?.id || '',
           requirement: versionPref.requirement,
-          targetVersion: javaVersion,
-        })
-      } else if (status === 2) {
-        // might not be compatible
-        builder.set(IncompatibleJavaIssueKey, {
-          minecraft,
-          forge: instance.runtime.forge || '',
-          java: instanceJava?.path || '',
-          version: selectedVersion?.id || '',
-          requirement: versionPref.requirement,
-          targetVersion: javaVersion,
         })
       }
 
+      this.state.instanceJava({ ...resultJava, valid: true })
       this.diagnoseService.report(builder.build())
     } finally {
       this.down('diagnose')
