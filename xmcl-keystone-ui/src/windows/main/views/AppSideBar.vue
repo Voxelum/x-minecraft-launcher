6a7867117822ec847fd0ed76d60fa236189c02ee
CI010 <cijhn@hotmail.com>
2022-05-17 12:47:53 +0800

    feat: add option to hide sidebar button

--- xmcl-keystone-ui/src/windows/main/views/AppSideBar.vue
+++ xmcl-keystone-ui/src/windows/main/views/AppSideBar.vue
@@ -1,5 +1,6 @@
 <template>
   <v-navigation-drawer
+    v-context-menu="items"
     :value="true"
     permanent
     width="200"
@@ -163,6 +164,7 @@
       </v-tooltip>
 
       <v-tooltip
+        v-if="sideBarShowCurseforge"
         :close-delay="0"
         right
       >
@@ -186,6 +188,7 @@
       </v-tooltip>
 
       <v-tooltip
+        v-if="sideBarShowModrinth"
         :close-delay="0"
         right
       >
@@ -206,6 +209,30 @@
           </v-list-item>
         </template>
         Modrinth
+      </v-tooltip>
+
+      <v-tooltip
+        v-if="sideBarShowFtb"
+        :close-delay="0"
+        right
+      >
+        <template #activator="{ on: tooltip }">
+          <v-list-item
+            push
+            link
+            class="non-moveable"
+            to="/ftb"
+            v-on="tooltip"
+          >
+            <v-list-item-icon>
+              <v-icon>
+                $vuetify.icons.ftb
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>FTB</v-list-item-title>
+          </v-list-item>
+        </template>
+        Feed the Beast
       </v-tooltip>
 
       <v-spacer />
@@ -242,52 +269,71 @@
         {{ t('multiplayer.name') }}
       </v-tooltip>
 
-      <v-list-item
-        link
-        class="non-moveable"
-        @click="show()"
+      <v-tooltip
+        :close-delay="0"
+        right
       >
-        <v-list-item-icon>
-          <v-badge
-            right
-            overlap
-            :value="count !== 0"
+        <template #activator="{ on: tooltip }">
+          <v-list-item
+            link
+            class="non-moveable"
+            v-on="tooltip"
+            @click="show()"
           >
-            <template #badge>
-              <span>{{ count }}</span>
-            </template>
-            <v-icon>
-              assignment
-            </v-icon>
-          </v-badge>
-        </v-list-item-icon>
-        <v-list-item-title>Tasks</v-list-item-title>
-      </v-list-item>
+            <v-list-item-icon>
+              <v-badge
+                right
+                overlap
+                :value="count !== 0"
+              >
+                <template #badge>
+                  <span>{{ count }}</span>
+                </template>
+                <v-icon>
+                  assignment
+                </v-icon>
+              </v-badge>
+            </v-list-item-icon>
+            <v-list-item-title>Tasks</v-list-item-title>
+          </v-list-item>
+        </template>
+        {{ t('task.manager') }}
+      </v-tooltip>
+
       <v-divider
         style="display: block !important;"
       />
-      <v-list-item
-        link
-        push
-        to="/setting"
-        class="non-moveable"
+      <v-tooltip
+        :close-delay="0"
+        right
       >
-        <v-list-item-icon>
-          <v-badge
-            right
-            overlap
-            :value="state.updateStatus !== 'none'"
+        <template #activator="{ on: tooltip }">
+          <v-list-item
+            link
+            push
+            to="/setting"
+            class="non-moveable"
+            v-on="tooltip"
           >
-            <template #badge>
-              <span>{{ 1 }}</span>
-            </template>
-            <v-icon>
-              settings
-            </v-icon>
-          </v-badge>
-        </v-list-item-icon>
-        <v-list-item-title>Settings</v-list-item-title>
-      </v-list-item>
+            <v-list-item-icon>
+              <v-badge
+                right
+                overlap
+                :value="state.updateStatus !== 'none'"
+              >
+                <template #badge>
+                  <span>{{ 1 }}</span>
+                </template>
+                <v-icon>
+                  settings
+                </v-icon>
+              </v-badge>
+            </v-list-item-icon>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item>
+        </template>
+        {{ tc('setting.name', 2) }}
+      </v-tooltip>
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -296,12 +342,18 @@
 import { BaseServiceKey } from '@xmcl/runtime-api'
 import PlayerAvatar from '../components/PlayerAvatar.vue'
 import { useColorTheme } from '../composables/colorTheme'
+import { ContextMenuItem } from '../composables/contextMenu'
 import { useDialog } from '../composables/dialog'
 import { useTaskCount } from '../composables/task'
 import { useCurrentUser } from '../composables/user'
+import { vContextMenu } from '../directives/contextMenu'
 import { useI18n, useRouter, useService } from '/@/composables'
+import { useLocalStorageCacheBool } from '/@/composables/cache'
 
 const { count } = useTaskCount()
+const sideBarShowCurseforge = useLocalStorageCacheBool('sideBarShowCurseforge', true)
+const sideBarShowModrinth = useLocalStorageCacheBool('sideBarShowModrinth', true)
+const sideBarShowFtb = useLocalStorageCacheBool('sideBarShowFtb', true)
 const { show } = useDialog('task')
 const { state } = useService(BaseServiceKey)
 const { gameProfile } = useCurrentUser()
@@ -314,6 +366,36 @@ const subRoutes = new Set([
   '/resource-pack-setting',
   '/shader-pack-setting',
 ])
+
+const items = computed(() => {
+  const result: ContextMenuItem[] = [
+    {
+      text: 'Curseforge',
+      icon: sideBarShowCurseforge.value ? 'check' : '',
+      onClick() {
+        sideBarShowCurseforge.value = !sideBarShowCurseforge.value
+      },
+      children: [],
+    },
+    {
+      text: 'Modrinth',
+      icon: sideBarShowModrinth.value ? 'check' : '',
+      onClick() {
+        sideBarShowModrinth.value = !sideBarShowModrinth.value
+      },
+      children: [],
+    },
+    {
+      text: 'Feed The Beast',
+      icon: sideBarShowFtb.value ? 'check' : '',
+      onClick() {
+        sideBarShowFtb.value = !sideBarShowFtb.value
+      },
+      children: [],
+    },
+  ]
+  return result
+})
 
 const { t, tc } = useI18n()
 const { sideBarColor } = useColorTheme()
