c5b6631ef01476b789f3ac16cc2f28a2173f4805
CI010 <cijhn@hotmail.com>
2022-06-29 23:49:32 +0800

    chore: Move the release note to page repo

---
+++ .github/actions/generate-release-note/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const {  writeFileSync } = require('fs');
+
+async function main(output) {
+    const body = core.getInput('body')
+    const version = core.getInput('version')
+    const locales = ['en', 'zh']
+
+    console.log(body)
+    for (const l of locales) {
+        let content = body
+        if (l === 'zh') {
+            content = content.replace(/BREAKING CHANGES/g, '破坏性改动').replace(/### Features/, '### 新特性').replace('### Bug Fixes', '### 修复与补丁')
+                .replace('### Dependencies Updates', '依赖更新')
+        }
+        try {
+            writeFileSync(`xmcl-page/src/pages/${l}/${version}.md`, content)
+        } catch (e) {
+            console.error(`Fail to update ${l} release note:`)
+            console.error(e)
+        }
+    }
+}
+
+main(core ? core.setOutput : (k, v) => {
+    console.log(k)
+    console.log(v)
+});
