e445541c027a1961ded2583b400a1ce555544116
CI010 <cijhn@hotmail.com>
2022-07-07 14:58:19 +0800

    fix: Prevent guess url failed from missing edge cdn

--- xmcl-runtime/lib/util/curseforge.ts
+++ xmcl-runtime/lib/util/curseforge.ts
@@ -1,4 +1,4 @@
 export function guessCurseforgeFileUrl(id: number, name: string) {
   const fileId = id.toString()
-  return `https://mediafiles.forgecdn.net/files/${fileId.slice(0, 4)}/${fileId.slice(4)}/${name}`
+  return [`https://edge.forgecdn.net/files/${fileId.slice(0, 4)}/${fileId.slice(4)}/${name}`, `https://mediafiles.forgecdn.net/files/${fileId.slice(0, 4)}/${fileId.slice(4)}/${name}`]
 }
