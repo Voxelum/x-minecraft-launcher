8aadec18a8330e20450bd5c7b0fb7ae3fe065314
CI010 <cijhn@hotmail.com>
2022-10-17 22:29:11 +0800

    fix: Set notification title to XMCL

--- xmcl-electron-app/main/controllers/notification.ts
+++ xmcl-electron-app/main/controllers/notification.ts
@@ -3,7 +3,7 @@ import { app } from 'electron'
 import { ControllerPlugin } from './plugin'
 
 export const notificationSetupPlugin: ControllerPlugin = function (this: Controller) {
-  this.app.on('service-ready', () => {
+  this.app.once('engine-ready', () => {
     if (this.app.platform.name === 'windows') {
       app.setAppUserModelId('X Minecraft Launcher')
     }
