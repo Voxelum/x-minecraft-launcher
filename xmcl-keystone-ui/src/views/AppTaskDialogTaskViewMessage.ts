af5d10fc10c2a8eab94755d73ca7c713670bf75b
CI010 <cijhn@hotmail.com>
2023-10-27 22:28:47 +0800

    fix: Should not crash task ui if the task

--- xmcl-keystone-ui/src/views/AppTaskDialogTaskViewMessage.ts
+++ xmcl-keystone-ui/src/views/AppTaskDialogTaskViewMessage.ts
@@ -14,6 +14,7 @@ export default defineComponent({
     const tError = useLocaleError()
     return () => {
       const resolve = (m: any) => {
+        if (!m) return h('div')
         markRaw(m)
         if (m.name === 'DownloadAggregateError') {
           return h('div', [
