28e394ae16862b502c891527de9c25fa225a8721
CI010 <cijhn@hotmail.com>
2023-11-15 20:13:14 +0800

    feat: Support MMC parsing

--- xmcl-runtime/lib/entities/mmcModpack.ts
+++ xmcl-runtime/lib/entities/mmcModpack.ts
@@ -7,5 +7,31 @@ export interface MultiMCManifest {
 }
 
 export interface MuultiMCManifestComponent {
+  cachedName: string
+  cachedVersion: string
+  cachedRequires: [
+    {
+      equals?: string
+      uid: string
+    },
+  ]
+  important?: boolean
+  uid: string
+  version: string
+}
 
+export interface MultiMCConfig {
+  JavaPath: string
+  name: string
+  JvmArgs: string
+  MaxMemAlloc: string
+  MinMemAlloc: string
+  ShowConsole: string
+  lastTimePlayed: string
+  totalTimePlayed: string
+  notes: string
+  MinecraftWinWidth: string
+  MinecraftWinHeight: string
+  JoinServerOnLaunch: string
+  JoinServerOnLaunchAddress: string
 }
