9e048c2ef664de469acb4763214e895a94f30e55
CI010 <cijhn@hotmail.com>
2023-11-27 20:27:57 +0800

    feat: Add i18n modrinth pipeline

--- xmcl-keystone-ui/src/composables/modrinthProject.ts
+++ xmcl-keystone-ui/src/composables/modrinthProject.ts
@@ -3,17 +3,11 @@ import useSWRV from 'swrv'
 import { Ref } from 'vue'
 import { kSWRVConfig } from './swrvConfig'
 import { getModrinthProjectKey } from '@/util/modrinth'
+import { kFlights } from './flights'
 
 export function useModrinthProject(id: Ref<string>) {
-  const { locale } = useI18n()
   const config = inject(kSWRVConfig)
-  const {
-    data: iProject,
-    isValidating: iRefreshing,
-    error: iError,
-    mutate: iMutate,
-  } = useSWRV(computed(() => getModrinthProjectKey(id.value) + '?locale=' + locale.value),
-    () => clientModrinchV2Locale.getProject(id.value), inject(kSWRVConfig))
+
   const {
     data,
     isValidating,
@@ -22,35 +16,56 @@ export function useModrinthProject(id: Ref<string>) {
   } = useSWRV(computed(() => getModrinthProjectKey(id.value)),
     async () => clientModrinthV2.getProject(id.value), inject(kSWRVConfig))
 
-  const project = computed(() => (!iRefreshing.value ? iProject.value : undefined) ?? data.value)
-  const refreshing = computed(() => isValidating.value)
-  const refreshError = computed(() => iError.value || error.value)
-  const refresh = () => {
-    iMutate()
-    mutate()
+  const flights = inject(kFlights, {})
+
+  if (flights.i18nSearch) {
+    const { locale } = useI18n()
+
+    const {
+      data: iProject,
+      isValidating: iRefreshing,
+      error: iError,
+      mutate: iMutate,
+    } = useSWRV(computed(() => getModrinthProjectKey(id.value) + '?locale=' + locale.value),
+      () => clientModrinchV2Locale.getProject(id.value), inject(kSWRVConfig))
+
+    const project = computed(() => (!iRefreshing.value ? iProject.value : undefined) ?? data.value)
+    const refreshing = computed(() => isValidating.value)
+    const refreshError = computed(() => iError.value || error.value)
+    const refresh = () => {
+      iMutate()
+      mutate()
+    }
+
+    watch(data, () => {
+      const _id = id.value
+      const key = getModrinthProjectKey(_id)
+      const item = config?.cache.get(key)
+      if (item?.data.data.id !== _id) {
+        config?.cache.delete(key)
+      }
+    })
+    watch(iProject, () => {
+      const _id = id.value
+      const key = getModrinthProjectKey(_id) + '?locale=' + locale.value
+      const item = config?.cache.get(key)
+      if (item?.data.data.id !== _id) {
+        config?.cache.delete(key)
+      }
+    })
+
+    return {
+      project,
+      refreshing,
+      refreshError,
+      refresh,
+    }
   }
 
-  watch(data, () => {
-    const _id = id.value
-    const key = getModrinthProjectKey(_id)
-    const item = config?.cache.get(key)
-    if (item?.data.data.id !== _id) {
-      config?.cache.delete(key)
-    }
-  })
-  watch(iProject, () => {
-    const _id = id.value
-    const key = getModrinthProjectKey(_id) + '?locale=' + locale.value
-    const item = config?.cache.get(key)
-    if (item?.data.data.id !== _id) {
-      config?.cache.delete(key)
-    }
-  })
-
   return {
-    project,
-    refreshing,
-    refreshError,
-    refresh,
+    project: data,
+    refreshing: isValidating,
+    refreshError: error,
+    refresh: mutate,
   }
 }
