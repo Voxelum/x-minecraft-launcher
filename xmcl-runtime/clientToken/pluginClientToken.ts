f4808ce388294ccdee21557fb4de4d3baeb6f402
CI010 <cijhn@hotmail.com>
2023-12-09 21:42:48 +0800

    refactor: Collect new client count

--- xmcl-runtime/clientToken/pluginClientToken.ts
+++ xmcl-runtime/clientToken/pluginClientToken.ts
@@ -2,18 +2,21 @@ import { randomUUID } from 'crypto'
 import { readFile, writeFile } from 'fs/promises'
 import { join } from 'path'
 import { LauncherAppPlugin } from '~/app'
-import { kClientToken } from '~/clientToken'
+import { kClientToken, kIsNewClient } from '~/clientToken'
 
 export const pluginClientToken: LauncherAppPlugin = async (app) => {
   const clientSessionFile = join(app.appDataPath, 'client_session')
   let clientSession = ''
+  let isNew = false
   try {
     const session = await readFile(clientSessionFile).then(b => b.toString())
     clientSession = session
   } catch {
     clientSession = randomUUID()
     await writeFile(clientSessionFile, clientSession)
+    isNew = true
   }
 
   app.registry.register(kClientToken, clientSession)
+  app.registry.register(kIsNewClient, isNew)
 }
