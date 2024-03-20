ae943d60dada2baad97319392fef671f7b262a9c
ci010 <cijhn@hotmail.com>
2024-03-20 14:33:55 -0700

    fix: Cannot install version due to the version list is empty

--- xmcl-keystone-ui/src/composables/instanceVersionInstall.ts
+++ xmcl-keystone-ui/src/composables/instanceVersionInstall.ts
@@ -27,7 +27,7 @@ export function useInstanceVersionInstall(versions: Ref<LocalVersionHeader[]>) {
 
   const getCacheOrFetch = async <T>(key: string, fetcher: () => Promise<T>) => {
     const cached = cache.get(key)
-    if (cached) {
+    if (cached && cached.data?.data) {
       return cached.data.data as T
     }
     const data = await fetcher()
