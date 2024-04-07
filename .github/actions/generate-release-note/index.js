76a9ce423c5796eac8080c3339abffb41f2ab815
CI010 <cijhn@hotmail.com>
2024-04-07 19:09:44 +0800

    chore: Update the release note generation

--- .github/actions/generate-release-note/index.js
+++ .github/actions/generate-release-note/index.js
@@ -19,6 +19,24 @@ async function main() {
         }
         const today = new Date()
         content = `---\nversion: ${version}\ndate: ${today.getUTCFullYear()}-${(today.getUTCMonth() + 1).toString().padStart(2, '0')}-${today.getUTCDate().toString().padStart(2, '0')}\nlayout: changelog\n---\n${content}`
+
+        const rpmUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-x86_64.rpm`
+        const rpmArmUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-aarch64.rpm`
+        const tarUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-x64.tar.xz`
+        const tarArmUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-arm64.tar.xz`
+        const debUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-amd64.deb`
+        const debArmUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-arm64.deb`
+        const appImageUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-x86_64.AppImage`
+        const appImageArmUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-arm64.AppImage`
+        const darwinZipUrl = `https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-darwin-x64.zip`
+
+        content += `## Downloads\n\n`
+        content += `Windows (x64): [zip](https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-win32-x64.zip)\n\n`
+        content += `Linux (x64): [AppImage](${appImageUrl}) [deb](${debUrl}) [tar.xz](${tarUrl}) [rpm](${rpmUrl})\n\n`
+        content += `Linux (arm64): [AppImage](${appImageArmUrl}) [deb](${debArmUrl}) [tar.xz](${tarArmUrl}) [rpm](${rpmArmUrl})\n\n`
+        content += `Mac (x64): [dmg](https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}.dmg) [zip](${darwinZipUrl})\n\n`
+        content += `Mac (arm64): [zip](https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}/xmcl-${version}-darwin-arm64.zip)\n\n`
+
         try {
             if (existsSync(`xmcl-page/src/${l}/changelogs`)) {
                 writeFileSync(`xmcl-page/src/${l}/changelogs/${version}.md`, content)
