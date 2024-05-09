e4b1db688757c1e581a6c6886acbeaadd6444598
CI010 <cijhn@hotmail.com>
2024-05-09 22:22:09 +0800

    Add translation for changelog

--- .github/actions/generate-release-note/index.js
+++ .github/actions/generate-release-note/index.js
@@ -4,6 +4,53 @@ const getInput = (name) => {
     const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
     return val
 }
+
+async function translate(body, locale) {
+    const key = process.env.DEEPSEEK_API_KEY
+    if (!key) {
+        return body
+    }
+    const res = await fetch("https://api.deepseek.com/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${key}`,
+        },
+        body: JSON.stringify({
+            model: "deepseek-chat",
+            messages: [
+                {
+                    role: "system",
+                    content: "You are an asistant of a Minecraft Launcher developer. You are asked to translate the mod description into different languages by locale code. I'm going to give you markdown text. You should give me translated markdown text. Do not wrap extra markdown code block (```) to the output, and do not add locale prefix to output."
+                },
+                {
+                    role: "user",
+                    content: "Translate following text into zh-CN:\nHello World"
+                },
+                {
+                    role: "assistant",
+                    content: "你好世界"
+                },
+                {
+                    role: "user",
+                    content: `Translate following text into ${locale}:\n${body}`
+                }
+            ],
+        }),
+    })
+
+    const resp = await res.json()
+    let content = resp.choices[0].message.content;
+    if (content.startsWith('```' + locale)) {
+        content = content.substring(('```' + locale).length);
+        content = content.substring(0, content.length - 3);
+    }
+    if (content.startsWith(locale)) {
+        content = content.substring(locale.length);
+    }
+    return content;
+}
+
 async function main() {
     const body = getInput('body')
     const version = getInput('version')
@@ -16,6 +63,7 @@ async function main() {
             content = content.replace(/BREAKING CHANGES/g, '破坏性改动').replace(/Features/, '新特性').replace('Bug Fixes & Patches', '修复与补丁')
                 .replace('Refactors', '重构')
                 .replace('Dependencies Updates', '依赖更新')
+            content = await translate(content, 'zh-CN')
         }
         const today = new Date()
         content = `---\nversion: ${version}\ndate: ${today.getUTCFullYear()}-${(today.getUTCMonth() + 1).toString().padStart(2, '0')}-${today.getUTCDate().toString().padStart(2, '0')}\nlayout: changelog\n---\n${content}`
