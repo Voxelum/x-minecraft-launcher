813da97727dda3e3d34d5a172d1ef0a7a393aa00
CI010 <cijhn@hotmail.com>
2024-06-19 18:26:28 +0800

    chore: Fix deb after-install script

--- xmcl-electron-app/postinstall.ts
+++ xmcl-electron-app/postinstall.ts
@@ -13,11 +13,15 @@ if (process.platform === 'linux' || process.platform === 'openbsd' || process.pl
   // Overwrite the linux electron-builder js code
   const fpmTargetFilePath = './node_modules/app-builder-lib/out/targets/FpmTarget.js'
   const linuxTargetHelperFilePath = './node_modules/app-builder-lib/out/targets/LinuxTargetHelper.js'
+  const linuxAfterInstallShPath = './node_modules/app-builder-lib/templates/linux/after-install.tpl'
   writeFileSync(fpmTargetFilePath, readFileSync(fpmTargetFilePath, 'utf-8')
     // eslint-disable-next-line no-template-curly-in-string
     .replace('installPrefix}/${appInfo.sanitizedProductName}', 'installPrefix}/xmcl'), 'utf-8')
   writeFileSync(linuxTargetHelperFilePath, readFileSync(linuxTargetHelperFilePath, 'utf-8')
     // eslint-disable-next-line no-template-curly-in-string
     .replace('installPrefix}/${appInfo.sanitizedProductName}', 'installPrefix}/xmcl'), 'utf-8')
+  writeFileSync(linuxAfterInstallShPath, readFileSync(linuxAfterInstallShPath, 'utf-8')
+    // eslint-disable-next-line no-template-curly-in-string
+    .replace('${sanitizedProductName}', 'xmcl'), 'utf-8')
   console.log('Patched linux build target')
 }
