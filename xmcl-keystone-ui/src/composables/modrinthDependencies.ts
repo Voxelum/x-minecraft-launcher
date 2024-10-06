d2425bcfdd0332de78df762a6a8b08d33ab55fff
CI010 <cijhn@hotmail.com>
2024-10-06 20:32:36 +0800

    fix: Correctly resolve the mod dependencies

--- xmcl-keystone-ui/src/composables/modrinthDependencies.ts
+++ xmcl-keystone-ui/src/composables/modrinthDependencies.ts
@@ -23,32 +23,35 @@ type ResolvedDependency = {
   parent: Project
 }
 
-const visit = async (resolvedDep: ResolvedDependency, visited: Set<string>, config: IConfig): Promise<ResolvedDependency[]> => {
-  const { recommendedVersion: version } = resolvedDep
-  if (resolvedDep.relativeType === 'incompatible' || resolvedDep.type === 'embedded') {
+const visit = async (current: ResolvedDependency, visited: Set<string>, config: IConfig): Promise<ResolvedDependency[]> => {
+  const { recommendedVersion: version } = current
+  if (current.relativeType === 'incompatible' || current.type === 'embedded') {
     return []
   }
   if (visited.has(version.project_id)) {
     return []
   }
   visited.add(version.project_id)
-  const deps = await Promise.all(version.dependencies.map(async (dep) => {
-    const project = await swrvGet(getModrinthProjectKey(dep.project_id), () => clientModrinthV2.getProject(dep.project_id), config.cache!, config.dedupingInterval!)
-    const versions = await swrvGet(getModrinthVersionKey(dep.project_id, undefined, version.loaders, version.game_versions),
-        () => clientModrinthV2.getProjectVersions(dep.project_id, { loaders: version.loaders, gameVersions: version.game_versions }),
-        config.cache!, config.dedupingInterval!)
-    if (dep.version_id) {
-      const id = dep.version_id
-      const recommendedVersion = versions.find(v => v.id === id)!
-      const result = await visit({ project, versions, recommendedVersion, parent: resolvedDep.project, type: resolvedDep.type || dep.dependency_type, relativeType: dep.dependency_type }, visited, config)
-      return result
-    } else {
-      const result = await visit({ project, versions, parent: resolvedDep.project, recommendedVersion: versions[0], type: resolvedDep.type || dep.dependency_type, relativeType: dep.dependency_type }, visited, config)
-      return result
-    }
+  const deps = await Promise.all(version.dependencies.map(async (child) => {
+    const project = await swrvGet(getModrinthProjectKey(child.project_id), () => clientModrinthV2.getProject(child.project_id), config.cache!, config.dedupingInterval!)
+    const versions = await swrvGet(getModrinthVersionKey(child.project_id, undefined, version.loaders, version.game_versions),
+      () => clientModrinthV2.getProjectVersions(child.project_id, { loaders: version.loaders, gameVersions: version.game_versions }),
+      config.cache!, config.dedupingInterval!)
+    const recommendedVersion = child.version_id ? versions.find(v => v.id === child.version_id)! : versions[0]
+    const result = await visit({
+      project,
+      versions,
+      recommendedVersion,
+      parent: current.project,
+      type: child.dependency_type === 'required'
+        ? current.type === 'required' ? 'required' : current.type || child.dependency_type
+        : child.dependency_type,
+      relativeType: child.dependency_type,
+    }, visited, config)
+    return result
   }))
 
-  return [resolvedDep, ...deps.reduce((a, b) => [...a, ...b], [])]
+  return [current, ...deps.reduce((a, b) => [...a, ...b], [])]
 }
 
 export function getModrinthDependenciesModel(version: Ref<ProjectVersion | undefined>, config = injection(kSWRVConfig)): SWRVModel<ResolvedDependency[]> {
