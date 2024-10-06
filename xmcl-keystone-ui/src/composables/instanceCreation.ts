1cfb741597e33ab0f383dc1e4519d5e8d2a5d34f
CI010 <cijhn@hotmail.com>
2024-10-06 20:07:26 +0800

    fix: New instance memory setting should follow global setting

--- xmcl-keystone-ui/src/composables/instanceCreation.ts
+++ xmcl-keystone-ui/src/composables/instanceCreation.ts
@@ -32,12 +32,12 @@ export function useInstanceCreation(gameProfile: Ref<GameProfile>, instances: Re
     runtime: getNewRuntime(),
     version: '',
     java: '',
-    showLog: false,
-    hideLauncher: true,
+    showLog: undefined,
+    hideLauncher: undefined,
     vmOptions: [] as string[],
     mcOptions: [] as string[],
-    maxMemory: 0,
-    minMemory: 0,
+    maxMemory: undefined,
+    minMemory: undefined,
     author: gameProfile.value.name,
     fileApi: '',
     modpackVersion: '',
@@ -47,8 +47,8 @@ export function useInstanceCreation(gameProfile: Ref<GameProfile>, instances: Re
     icon: '',
     server: null,
     tags: [],
-    assignMemory: false,
-    fastLaunch: false,
+    assignMemory: undefined,
+    fastLaunch: undefined,
   })
   const files: Ref<InstanceFile[]> = ref([])
   const loading = ref(false)
@@ -60,8 +60,8 @@ export function useInstanceCreation(gameProfile: Ref<GameProfile>, instances: Re
       data.runtime = { ...template.runtime }
     }
     data.java = template.java ?? ''
-    data.showLog = template.showLog ?? false
-    data.hideLauncher = template.hideLauncher ?? true
+    data.showLog = template.showLog
+    data.hideLauncher = template.hideLauncher
     data.vmOptions = [...template.vmOptions ?? []]
     data.mcOptions = [...template.mcOptions ?? []]
     data.maxMemory = template.maxMemory ?? 0
