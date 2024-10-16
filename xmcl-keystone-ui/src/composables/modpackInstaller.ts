c339c4955f4b67866006ad49b97ea37300d3e9ce
CI010 <cijhn@hotmail.com>
2024-10-16 19:35:24 +0800

    fix: Sometime modpack does not install files

--- xmcl-keystone-ui/src/composables/modpackInstaller.ts
+++ xmcl-keystone-ui/src/composables/modpackInstaller.ts
@@ -1,52 +1,30 @@
 import { injection } from '@/util/inject'
-import { generateDistinctName } from '@/util/instanceName'
-import { CreateInstanceOption, InstallMarketOptions, InstanceServiceKey, ModpackServiceKey, waitModpackFiles } from '@xmcl/runtime-api'
-import { kInstanceFiles } from './instanceFiles'
-import { kInstanceVersion } from './instanceVersion'
+import { InstallMarketOptions, ModpackServiceKey, VersionServiceKey } from '@xmcl/runtime-api'
 import { kInstanceVersionInstall } from './instanceVersionInstall'
 import { kInstances } from './instances'
 import { kJavaContext } from './java'
 import { useService } from './service'
 
 export function useModpackInstaller() {
-  const { instances, selectedInstance } = injection(kInstances)
-  const { openModpack, installModapckFromMarket } = useService(ModpackServiceKey)
-  const { createInstance } = useService(InstanceServiceKey)
-  const { installFiles } = injection(kInstanceFiles)
-  const { getVersionHeader, getResolvedVersion } = injection(kInstanceVersion)
+  const { selectedInstance } = injection(kInstances)
+  const { importModpack, installModapckFromMarket } = useService(ModpackServiceKey)
+  const { resolveLocalVersion } = useService(VersionServiceKey)
   const { getInstanceLock, getInstallInstruction, handleInstallInstruction } = injection(kInstanceVersionInstall)
   const { all } = injection(kJavaContext)
   const { currentRoute, push } = useRouter()
   const installModpack = async (f: InstallMarketOptions) => {
     const [modpackFile] = await installModapckFromMarket(f)
-    const openedModpack = await openModpack(modpackFile)
-    const config = openedModpack.config
+    const { instancePath, version, runtime } = await importModpack(modpackFile)
 
-    if (!config) return
-    const name = generateDistinctName(config.name, instances.value.map(i => i.name))
-    const existed = getVersionHeader(config.runtime, '')
-    const options: CreateInstanceOption = {
-      ...config,
-      name,
-    }
-    if (existed) {
-      options.version = existed.id
-    }
-    const path = await createInstance(options)
-
-    selectedInstance.value = path
+    selectedInstance.value = instancePath
     if (currentRoute.path !== '/') {
       push('/')
     }
 
-    waitModpackFiles(openedModpack).then((files) => {
-      installFiles(path, files)
-    })
-
-    const lock = getInstanceLock(path)
+    const lock = getInstanceLock(instancePath)
     lock.write(async () => {
-      const resolved = existed ? await getResolvedVersion(existed) : undefined
-      const instruction = await getInstallInstruction(path, config.runtime, '', resolved, all.value)
+      const resolved = version ? await resolveLocalVersion(version) : undefined
+      const instruction = await getInstallInstruction(instancePath, runtime, '', resolved, all.value)
       await handleInstallInstruction(instruction)
     })
   }
