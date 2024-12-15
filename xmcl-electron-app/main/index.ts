fe93f0ece03cd7b2fcce0637bc2a79f1e130f4b3
paring <me@paring.moe>
2024-12-15 20:50:30 +0900

    fix: remove limit for event emitters for listeners not to be ignored

--- xmcl-electron-app/main/index.ts
+++ xmcl-electron-app/main/index.ts
@@ -3,5 +3,9 @@ require('graceful-fs').gracefulify(require('fs'))
 
 // eslint-disable-next-line import/first
 import ElectronLauncherApp from './ElectronLauncherApp'
+// eslint-disable-next-line import/first
+import { EventEmitter } from 'events'
+
+EventEmitter.setMaxListeners(0)
 
 new ElectronLauncherApp().start()
