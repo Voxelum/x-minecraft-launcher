1f908ee6b800d8f8a49993126b2785900a0e5d12
ci010 <cijhn@hotmail.com>
2025-02-02 17:35:02 +0800

    refactor: Update appinsightkey

--- xmcl-keystone-ui/src/telemetry.ts
+++ xmcl-keystone-ui/src/telemetry.ts
@@ -2,7 +2,7 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 const appInsights = new ApplicationInsights({
   config: {
-    connectionString: 'InstrumentationKey=294f3664-8208-4963-a2b0-62405ff9d48e;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/',
+    connectionString: 'InstrumentationKey=3db62cee-dd9b-4622-9884-e44d8403f2bc;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=be48ffb5-2543-4ab6-a75c-37ef9deda34a',
     disableCookiesUsage: true,
     disableFetchTracking: true,
     disableAjaxTracking: true,
