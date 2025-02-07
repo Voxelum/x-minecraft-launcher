824175bf13e68b73abca5a511a05431d8d063820
ci010 <cijhn@hotmail.com>
2025-02-07 17:32:59 +0800

    fix: Should follow logs

--- xmcl-keystone-ui/src/components/LogView.vue
+++ xmcl-keystone-ui/src/components/LogView.vue
@@ -1,29 +1,117 @@
 <template>
-  <div class="visible-scroll flex flex-col gap-1 rounded p-2 text-sm">
+  <div
+    ref="scroller" 
+    class="visible-scroll flex flex-col gap-1 rounded p-2 text-sm"
+    @wheel="onWheel"
+  >
     <div
-      v-for="(l, index) of logs"
-      :key="index"
-      :class="levelClasses[l.level]"
-      class="log-record"
+      ref="container"
+      :style="{
+        height: `${totalHeight}px`,
+        position: 'relative',
+        width: '100%',
+        marginTop: `${-offsetTop}px`,
+      }"  
     >
-      <div class="">
-        <span
-          class="level"
-        >{{ levelText[l.level] ? levelText[l.level] : l.level.toUpperCase() }}</span>
-        <span class="date">{{ l.date }}</span>
-        <span class="source">{{ l.source }}</span>
+      <div
+        v-for="virtualRow in virtualRows"
+        :key="logs[virtualRow.index].raw"
+        :ref="measureElement"
+        :data-index="virtualRow.index"
+        :style="{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          transform: `translateY(${virtualRow.start}px)`
+        }"
+      >
+        <div
+          :class="levelClasses[logs[virtualRow.index].level]"
+          class="log-record"
+        >
+          <div class="">
+            <span
+              class="level"
+            >{{ levelText[logs[virtualRow.index].level] ? levelText[logs[virtualRow.index].level] : logs[virtualRow.index].level.toUpperCase() }}</span>
+            <span class="date">{{ logs[virtualRow.index].date }}</span>
+            <span class="source">{{ logs[virtualRow.index].source }}</span>
+          </div>
+          <span class="content">{{ logs[virtualRow.index].content }}</span>
+        </div>
       </div>
-      <span class="content">{{ l.content }}</span>
+      <!-- <div
+        v-for="(l, index) of logs"
+        :key="index"
+        :class="levelClasses[l.level]"
+        class="log-record"
+      >
+        <div class="">
+          <span
+            class="level"
+          >{{ levelText[l.level] ? levelText[l.level] : l.level.toUpperCase() }}</span>
+          <span class="date">{{ l.date }}</span>
+          <span class="source">{{ l.source }}</span>
+        </div>
+        <span class="content">{{ l.content }}</span>
+      </div> -->
     </div>
+
+    <v-fab-transition>
+
+      <v-btn
+      v-if="locked"
+      class="z-10 absolute right-6 bottom-4"
+      elevation="2"
+      color="primary"
+      fab
+      @click="scrollToBottom"
+      >
+      <v-icon>arrow_downward</v-icon>
+    </v-btn>
+  </v-fab-transition>
   </div>
 </template>
 <script lang="ts" setup>
 import { injection } from '@/util/inject'
 import { LogRecord } from '../util/log'
 import { kTheme } from '@/composables/theme'
+import { useScroll } from '@vueuse/core';
+import { VirtualItem, VirtualizerOptions, useVirtualizer } from '@tanstack/vue-virtual';
 
-defineProps<{ logs: LogRecord[] }>()
+const props = defineProps<{ 
+  logs: LogRecord[]
+}>()
 const { isDark } = injection(kTheme)
+
+const container = ref<HTMLElement>()
+const offsetTop = ref(0)
+const scroller = ref<HTMLElement>()
+
+watch(container, container => {
+  if (container) {
+    nextTick().then(() => { offsetTop.value = container.offsetTop })
+  }
+})
+const virtualizerOptions = computed(() => ({
+  count: props.logs.length,
+  getScrollElement: () => scroller.value || null,
+  estimateSize: () => 56,
+  overscan: 10,
+  paddingStart: offsetTop.value,
+} satisfies Partial<VirtualizerOptions<HTMLElement, HTMLElement>>))
+const totalHeight = computed(() => virtualizer.value.getTotalSize())
+const virtualRows = computed(() => virtualizer.value.getVirtualItems() as (Omit<VirtualItem, 'key'> & { key: number })[])
+
+const measureElement = (el: any) => {
+  if (!el) return
+
+  virtualizer.value.measureElement(el)
+}
+
+const { arrivedState } = useScroll(scroller)
+
+const virtualizer = useVirtualizer(virtualizerOptions)
 
 const { t } = useI18n()
 const levelText: Record<string, string> = reactive({
@@ -36,6 +124,32 @@ const levelClasses: Record<string, string> = reactive({
   warn: computed(() => 'text-orange-600'),
   error: computed(() => 'text-rose-600'),
 })
+
+const locked = ref(true)
+watch(computed(() => arrivedState.bottom), (bottom) => {
+  if (bottom) {
+    locked.value = true
+  }
+})
+
+watch(() => props.logs.length, () => {
+  if (locked.value) {
+    nextTick(() => {
+      scrollToBottom()
+    })
+  }
+})
+
+function onWheel(e: WheelEvent) {
+  if (e.deltaY < 0) {
+    locked.value = false
+  }
+}
+
+function scrollToBottom() {
+  virtualizer.value.scrollToIndex(props.logs.length - 1)
+}
+
 </script>
 
 <style scoped>
@@ -57,7 +171,6 @@ const levelClasses: Record<string, string> = reactive({
 
 .log-record {
   @apply px-2 leading-7 border-l-3 border-current;
-  content-visibility: auto;
 }
 
 .log-record:hover {
--- xmcl-keystone-ui/src/windows/logger/App.vue
+++ xmcl-keystone-ui/src/windows/logger/App.vue
@@ -64,8 +64,6 @@
 
 <script lang=ts setup>
 import { set } from 'vue'
-
-import { baseService } from './baseService'
 import LogView from '@/components/LogView.vue'
 import { LogRecord, parseLog } from '@/util/log'
 import { kTheme } from '@/composables/theme'
