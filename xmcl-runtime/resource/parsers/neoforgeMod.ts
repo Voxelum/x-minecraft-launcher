b30052b7342ac11aa2220cf6087ef5faa6b9b3d8
ci010 <cijhn@hotmail.com>
2025-02-12 14:10:37 +0800

    fix: Should not emit neoforge metadata if it's not a neoforge mod

--- xmcl-runtime/resource/parsers/neoforgeMod.ts
+++ xmcl-runtime/resource/parsers/neoforgeMod.ts
@@ -14,6 +14,9 @@ export const neoforgeModParser: IResourceParser<NeoforgeMetadata> = ({
   },
   parseMetadata: async (fs, fileName) => {
     const mods = await readForgeModToml(fs, undefined, 'neoforge.mods.toml')
+    if (mods.length === 0) {
+      throw new Error()
+    }
     return {
       ...mods[0],
       children: mods.slice(1),
