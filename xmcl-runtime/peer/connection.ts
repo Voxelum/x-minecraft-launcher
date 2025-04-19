57595ef838d23b4b15a97f3788802634253511bd
ci010 <cijhn@hotmail.com>
2025-04-19 11:45:46 +0800

    refactor: Refine the peer download protocol

--- xmcl-runtime/peer/connection.ts
+++ xmcl-runtime/peer/connection.ts
@@ -3,7 +3,7 @@ import { createReadStream, existsSync } from 'fs'
 import debounce from 'lodash.debounce'
 import { createConnection } from 'net'
 import { join } from 'path'
-import { Readable, Writable } from 'stream'
+import { Readable, Writable, finished } from 'stream'
 import { PeerContext } from './PeerContext'
 import { ServerProxy } from './ServerProxy'
 import { MessageGetSharedManifestEntry, MessageShareManifestEntry } from './messages/download'
@@ -12,6 +12,7 @@ import { MessageIdentity, MessageIdentityEntry } from './messages/identity'
 import { MessageLanEntry } from './messages/lan'
 import { MessageEntry, MessageHandler, MessageType } from './messages/message'
 import { WorkerQueue } from '~/util/aggregator'
+import { DownloadChannel } from './DownloadChannel'
 
 const getRegistry = (entries: MessageEntry<any>[]) => {
   const reg: Record<string, MessageHandler<any>> = {}
@@ -51,39 +52,33 @@ export class PeerSession {
 
   #interval: ReturnType<typeof setInterval>
 
+  #channelPool: DownloadChannel[] = []
+
+  #getOrCreateDownloadChannel() {
+    let idel: DownloadChannel | undefined
+    for (const channel of this.#channelPool) {
+      if (!channel.isBusy) {
+        idel = channel
+        break
+      }
+    }
+    const channel = new DownloadChannel(this.connection.createDataChannel(`download-${this.#channelPool.length}`, {
+      ordered: true,
+      protocol: 'download',
+    }), this.createStream, this.connection.sctp?.maxMessageSize ?? 16 * 1024)
+    return channel
+  }
+
   #streamQueue = new WorkerQueue<{ file: string; destination: Writable }>(async ({ file, destination }) => {
-    return new Promise<void>((resolve) => {
-      const channel = this.connection.createDataChannel(file, {
-        protocol: 'download',
-        ordered: true,
-      })
-      const onError = (e: Error) => {
-        resolve()
-        channel.close()
-      }
-      channel.onclose = () => {
-        resolve()
-      }
-      channel.onmessage = (msg) => {
-        if (typeof msg.data === 'string') {
-          if (msg.data === 'end') {
-            destination.end()
-            channel.close()
-            resolve()
-          } else {
-            destination.destroy(new Error(msg.data))
-          }
-        } else {
-          destination.write(Buffer.from(msg.data))
-        }
-      }
-      destination.on('error', onError)
-      channel.onerror = (e) => {
-        resolve()
-        destination.destroy(new Error(e.type))
-      }
+    return new Promise<any>((resolve) => {
+      const channel = this.#getOrCreateDownloadChannel()
+      finished(destination, resolve)
+      channel.start(file, destination)
     })
-  }, 246)
+  }, 32, {
+    shouldRetry: () => false,
+    isEqual: () => false,
+  })
 
   constructor(
     /**
@@ -119,7 +114,6 @@ export class PeerSession {
       }
       this.#updateDescriptor()
     })
-    this.#streamQueue.workers = (this.connection.sctp?.maxChannels ?? 256) - 10
     this.connection.addEventListener('datachannel', (e) => {
       const channel = e.channel
       const label = channel.label
@@ -161,92 +155,69 @@ export class PeerSession {
         console.log('Metadata channel created')
       } else if (channel.protocol === 'download') {
         console.log(`Receive peer file request: ${channel.label}`)
-        const path = unescape(label)
-        const maxChunk = this.connection.sctp?.maxMessageSize ?? 16 * 1024
-        const createStream = (filePath: string) => {
-          if (filePath.startsWith('/sharing')) {
-            if (filePath === '/sharing') {
-              const man = this.context.getSharedInstance()
-              if (!man) {
-                return 'NOT_FOUND'
-              }
-              return new Readable({
-                read() {
-                  const encoder = new TextEncoder()
-                  const buf = encoder.encode(JSON.stringify(man))
-                  this.push(buf)
-                  this.push(null)
-                },
-                highWaterMark: maxChunk,
-              })
-            }
-            filePath = filePath.substring('/sharing'.length)
-            if (filePath.startsWith('/')) {
-              filePath = filePath.substring(1)
-            }
-            const man = this.context.getSharedInstance()
-            if (!man) {
-              return 'NO_PERMISSION'
-            }
-            if (!man.files.some(v => v.path === filePath)) {
-              return 'NO_PERMISSION'
-            }
-            const absPath = join(this.context.getShadedInstancePath(), filePath)
-            if (!existsSync(absPath)) {
-              return 'NOT_FOUND'
-            }
-            return createReadStream(absPath, {
-              highWaterMark: maxChunk,
-            })
-          } else if (filePath.startsWith('/image')) {
-            filePath = filePath.substring('/image'.length)
-            if (filePath.startsWith('/')) {
-              filePath = filePath.substring(1)
-            }
-            return createReadStream(this.context.getSharedImagePath(filePath), {
-              highWaterMark: 16 * 1024,
-            })
-          } else if (filePath.startsWith('/assets')) {
-            filePath = filePath.substring('/assets'.length)
-            return createReadStream(join(this.context.getSharedAssetsPath(), filePath), {
-              highWaterMark: 16 * 1024,
-            })
-          } else if (filePath.startsWith('/libraries')) {
-            filePath = filePath.substring('/libraries'.length)
-            return createReadStream(join(this.context.getSharedLibrariesPath(), filePath), {
-              highWaterMark: 16 * 1024,
-            })
-          }
-          return 'NOT_FOUND'
-        }
-        const result = createStream(path)
-        if (typeof result === 'string') {
-          // reject the file
-          console.log(`Reject peer file request ${path} due to ${result}`)
-          channel.send(result)
-          channel.close()
-        } else {
-          console.log(`Process peer file request: ${path}`)
-          result.on('data', (data: Buffer) => {
-            if (data.length > maxChunk) {
-              let pivot = 0
-              while (pivot < data.length) {
-                const sub = data.subarray(pivot, pivot + maxChunk)
-                channel.send(sub)
-                pivot += sub.length
-              }
-            } else {
-              channel.send(data.buffer)
-            }
-          })
-          result.on('end', () => {
-            channel.send('end')
-          })
-        }
+        this.#channelPool.push(new DownloadChannel(channel, this.createStream, this.connection.sctp?.maxMessageSize ?? 16 * 1024))
       } else {
         // TODO: emit error for unknown protocol
       }
     })
+  }
+
+  createStream = (filePath: string) => {
+    const maxChunk = this.connection.sctp?.maxMessageSize ?? 16 * 1024
+    if (filePath.startsWith('/sharing')) {
+      if (filePath === '/sharing') {
+        const man = this.context.getSharedInstance()
+        if (!man) {
+          return 'NOT_FOUND'
+        }
+        return new Readable({
+          read() {
+            const encoder = new TextEncoder()
+            const buf = encoder.encode(JSON.stringify(man))
+            this.push(buf)
+            this.push(null)
+          },
+          highWaterMark: maxChunk,
+        })
+      }
+      filePath = filePath.substring('/sharing'.length)
+      if (filePath.startsWith('/')) {
+        filePath = filePath.substring(1)
+      }
+      const man = this.context.getSharedInstance()
+      if (!man) {
+        return 'NO_PERMISSION'
+      }
+      if (!man.files.some(v => v.path === filePath)) {
+        return 'NO_PERMISSION'
+      }
+      const absPath = join(this.context.getShadedInstancePath(), filePath)
+      if (!existsSync(absPath)) {
+        return 'NOT_FOUND'
+      }
+      return createReadStream(absPath, {
+        highWaterMark: maxChunk,
+      })
+    } else if (filePath.startsWith('/image')) {
+      filePath = filePath.substring('/image'.length)
+      if (filePath.startsWith('/')) {
+        filePath = filePath.substring(1)
+      }
+      return createReadStream(this.context.getSharedImagePath(filePath), {
+        highWaterMark: 16 * 1024,
+      })
+    } else if (filePath.startsWith('/assets')) {
+      filePath = filePath.substring('/assets'.length)
+      return createReadStream(join(this.context.getSharedAssetsPath(), filePath), {
+        highWaterMark: 16 * 1024,
+      })
+    } else if (filePath.startsWith('/libraries')) {
+      filePath = filePath.substring('/libraries'.length)
+      return createReadStream(join(this.context.getSharedLibrariesPath(), filePath), {
+        highWaterMark: 16 * 1024,
+      })
+    }
+    return 'NOT_FOUND'
   }
 
   /**
