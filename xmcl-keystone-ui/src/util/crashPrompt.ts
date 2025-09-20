e2d106208036ff7b2b7c0f39ab4e0f6e39a70ddb
ci010 <cijhn@hotmail.com>
2025-09-20 18:06:13 +0800

    feat: Guide user to use AI to resolve the crash report

---
+++ xmcl-keystone-ui/src/util/crashPrompt.ts
@@ -0,0 +1,41 @@
+export function getCrashPrompt(useCN: boolean, crash: string, log: string, localeCode: string) {
+  if (useCN) {
+    return `你是Minecraft 崩溃报告（crash report）助手，帮助我分析crash造成的原因。你应当首先分析出游戏崩溃的主要原因（java，游戏原版library，mod，或者是其他原因）
+对于java问题或者游戏原版library，你应当直接提醒我。
+若是mod造成的崩溃，你应当通过日志分析出哪些mod可能造成这些错误，并提示我。
+
+如果实在分析不出具体原因，请告知我应当联系开发者。添加QQ群：858391850
+
+这是当前的崩溃报告:
+\`\`\`
+${crash}
+\`\`\`
+
+这是最新的日志作为补充：
+\`\`\`
+${log}
+\`\`\`
+`
+  } else {
+    return `
+You are a Minecraft crash report assistant, helping me analyze the causes of crashes. You should first identify the main reason for the game crash (Java issues, missing vanilla game libraries, mod-related problems, or other causes).
+
+For Java issues or missing vanilla game libraries, you should provide direct reminders.
+
+If the crash is caused by mods, you should analyze the logs to determine which mods may be responsible for the errors and notify me.
+
+If the specific cause cannot be determined, please inform me that I should contact the developers. Join discord: https://discord.gg/W5XVwYY7GQ
+
+I prefer you to response my with locale ${localeCode}.
+
+Here is the current crash report:
+\`\`\`
+${crash}
+\`\`\`
+Here is the latest log as supplementary information:
+\`\`\`
+${log}
+\`\`\`
+    `
+  }
+}
--- xmcl-keystone-ui/src/views/AppGameExitDialog.vue
+++ xmcl-keystone-ui/src/views/AppGameExitDialog.vue
@@ -26,23 +26,60 @@
           <v-icon>arrow_drop_down</v-icon>
         </v-btn>
       </v-toolbar>
-      <v-card-text class="flex flex-col overflow-auto">
-        <div
-          v-if="data.errorLog"
-          style="padding: 10px"
-        >
-          {{ data.launcherError ? t('launchFailed.failedToLaunch') : data.isCrash ? t(`launchFailed.crash`) : t(`launchFailed.description`) }}
+      <v-card-text class="grid grid-cols-12 overflow-auto gap-4">
+        <div class="col-span-9 flex flex-col overflow-auto">
+          <div
+            v-if="data.errorLog"
+            style="padding: 10px"
+          >
+            {{ data.launcherError ? t('launchFailed.failedToLaunch') : data.isCrash ? t(`launchFailed.crash`) : t(`launchFailed.description`) }}
+          </div>
+          <pre
+            v-if="data.errorLog"
+            class="overflow-auto min-h-[200px] rounded bg-[rgba(0,0,0,0.1)] p-5 hover:bg-[rgba(0,0,0,0.2)]"
+          >{{ data.errorLog }}</pre>
+          <div
+            style="padding: 10px"
+          >
+            {{ t(`launchFailed.latestLog`) }}
+          </div>
+          <pre class="overflow-auto rounded bg-[rgba(0,0,0,0.1)] p-5 hover:bg-[rgba(0,0,0,0.2)]">{{ data.log }}</pre>
         </div>
-        <pre
-          v-if="data.errorLog"
-          class="overflow-auto min-h-[200px] rounded bg-[rgba(0,0,0,0.1)] p-5 hover:bg-[rgba(0,0,0,0.2)]"
-        >{{ data.errorLog }}</pre>
-        <div
-          style="padding: 10px"
-        >
-          {{ t(`launchFailed.latestLog`) }}
+        <div class="col-span-3 mt-2 items-center justify-center flex flex-col gap-6 select-none">
+          <v-icon size="60">
+            hail
+          </v-icon>
+          <span class="text-lg">
+            {{ t('askAICrash.description') }}
+          </span>
+          <ol>
+            <li>
+              {{ t('askAICrash.copyPrompt') }}
+              <v-btn color="primary" :outlined="copied" small @click="onCopyPrompt">
+                <v-icon left>
+                  {{ copied ? 'check' : 'smart_toy'}}
+                </v-icon>
+                {{ t( 'copyClipboard.success' )}}
+              </v-btn>
+            </li>
+            <li>
+              {{ t('askAICrash.selectPlatform') }}
+              <ul>
+                <template v-if="useCNAI">
+                  <li><a href="https://chatglm.cn/share/kFiK3rVp" @click="onGMLClicked">GLM</a></li>
+                  <li><a href="https://doubao.com/chat">豆包</a></li>
+                  <li><a href="https://chat.deepseek.com/">Deepseek</a></li>
+                </template>
+                <template v-else>
+                  <li><a href="https://chat.openai.com">ChatGPT</a></li>
+                  <li><a href="https://gemini.google.com">Gemini</a></li>
+                  <li><a href="https://chat.deepseek.com/">Deepseek</a></li>
+                  <li><a href="https://chat.z.ai">z.ai</a></li>
+                </template>
+              </ul>
+            </li>
+          </ol>
         </div>
-        <pre class="overflow-auto rounded bg-[rgba(0,0,0,0.1)] p-5 hover:bg-[rgba(0,0,0,0.2)]">{{ data.log }}</pre>
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -50,8 +87,11 @@
 
 <script lang=ts setup>
 import { useService } from '@/composables'
+import { kEnvironment } from '@/composables/environment'
 import { kInstance } from '@/composables/instance'
 import { kInstanceLaunch } from '@/composables/instanceLaunch'
+import { kSettingsState } from '@/composables/setting'
+import { getCrashPrompt } from '@/util/crashPrompt'
 import { injection } from '@/util/inject'
 import { BaseServiceKey, InstanceLogServiceKey, LaunchServiceKey } from '@xmcl/runtime-api'
 
@@ -102,7 +142,6 @@ on('minecraft-exit', ({ code, signal, crashReport, crashReportLocation, errorLog
     return
   }
   if (code !== 0) {
-    console.log(errorLog)
     data.errorLog = errorLog || crashReport || ''
     if (crashReportLocation) {
       data.crashReportLocation = crashReportLocation
@@ -119,6 +158,28 @@ function openFolder() {
   } else {
     showLog(path.value, 'latest.log')
   }
+}
+
+const env = injection(kEnvironment)
+const useCNAI = computed(() => {
+  console.log(env.value?.region)
+  return env.value?.gfw || env.value?.region === 'zh-CN'
+})
+
+const copied = ref(false)
+const { state } = injection(kSettingsState)
+function onCopyPrompt() {
+  const useCN = useCNAI.value
+  const prompt = getCrashPrompt(useCN, data.errorLog, data.log, state.value?.locale || 'en-US')
+  windowController.writeClipboard(prompt)
+  copied.value = true
+  setTimeout(() => {
+    copied.value = false
+  }, 2000)
+}
+
+function onGMLClicked() {
+  windowController.writeClipboard(data.errorLog)
 }
 </script>
 
