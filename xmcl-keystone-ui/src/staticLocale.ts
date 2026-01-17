eda5a4e62f25e7d0d1d026baca974237c9e5e503
ci010 <cijhn@hotmail.com>
2026-01-17 12:05:14 +0800

    chore: cleanup unused i18n keys
    - Remove deprecated diagnose and task-related keys
    - Clean up unused UI string keys across all locales

--- xmcl-keystone-ui/src/staticLocale.ts
+++ xmcl-keystone-ui/src/staticLocale.ts
@@ -2,71 +2,10 @@
 function t(key) { /* noop */ }
 
 // tasks
-
-// Install assets
-t('installAssets.asset')
-t('installAssets.assetIndex')
-t('installAssets.name')
-
-// Install Fabric
-t('installFabric.name')
-
-// Install Forge
-t('installForge.name')
-t('installForge.library')
-t('installForge.postProcessing')
-
-// Install Optifine
-t('installOptifine.name')
-t('installOptifine.json')
-t('installOptifine.jar')
-t('installOptifine.download')
-
-// Install Instance
-t('installInstance.name')
-t('installInstance.file')
-t('installInstance.resolve')
-t('installInstance.unzip')
-t('installInstance.link')
-
-// Install Libraries
-t('installLibraries.library')
-t('installLibraries.name')
-
-// Install Authlib
-t('installAuthlibInjector')
-
-// Install curseforge file
-t('installCurseforgeFile')
-
-// Install Modpack
-t('installModpack.deploy')
-t('installModpack.download.file')
-t('installModpack.download.name')
-t('installModpack.name')
-t('installModpack.unpack')
-
-// Install Version
-t('installVersion.name')
-t('installVersion.jar')
-t('installVersion.json')
-
-// Install Modrinth
-t('installModrinthFile.name')
-
-// Install JRE
-t('installJre.name')
-t('installJre.download')
-t('installJre.decompress')
-
 // Upload Server
 t('server.upload')
 
 // Curseforge categories
-
-t('installLabyMod.asset')
-t('installLabyMod.json')
-t('installLabyMod.name')
 
 t('dependencies.incompatible')
 t('dependencies.optional')
