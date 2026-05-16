d749618448d92286f75d1731d8c9266c32d4e78d
ci010 <cijhn@hotmail.com>
2026-05-16 20:55:43 +0800

    refactor: modernize MarketProjectDetailModrinth layout

--- xmcl-keystone-ui/src/components/MarketProjectDetailModrinth.vue
+++ xmcl-keystone-ui/src/components/MarketProjectDetailModrinth.vue
@@ -5,7 +5,10 @@ import { useInCollection, useModrinthFollow } from '@/composables/modrinthAuthen
 import { getModrinthDependenciesModel } from '@/composables/modrinthDependencies'
 import { kModrinthInstaller } from '@/composables/modrinthInstaller'
 import { useModrinthProject } from '@/composables/modrinthProject'
-import { useModrinthProjectDetailData, useModrinthProjectDetailVersions } from '@/composables/modrinthProjectDetailData'
+import {
+  useModrinthProjectDetailData,
+  useModrinthProjectDetailVersions,
+} from '@/composables/modrinthProjectDetailData'
 import { getModrinthVersionModel, useModrinthTask } from '@/composables/modrinthVersions'
 import { useProjectDetailEnable, useProjectDetailUpdate } from '@/composables/projectDetail'
 import { useService } from '@/composables/service'
@@ -39,29 +42,55 @@ const emit = defineEmits<{
 
 // Project
 const projectId = computed(() => props.projectId)
-const { project, isValidating: isValidatingModrinth, refresh, error } = useModrinthProject(projectId)
+const {
+  project,
+  isValidating: isValidatingModrinth,
+  refresh,
+  error,
+} = useModrinthProject(projectId)
 const { lookupByModrinth } = useService(ProjectMappingServiceKey)
 
 const mapping = shallowRef(undefined as ProjectMapping | undefined)
 
-watch(projectId, async (id) => {
-  const result = await lookupByModrinth(id).catch(() => undefined)
-  if (id === projectId.value) {
-    mapping.value = result
-  }
-}, { immediate: true })
+watch(
+  projectId,
+  async (id) => {
+    const result = await lookupByModrinth(id).catch(() => undefined)
+    if (id === projectId.value) {
+      mapping.value = result
+    }
+  },
+  { immediate: true },
+)
 
-const model = useModrinthProjectDetailData(projectId, project, computed(() => props.modrinth), mapping)
+const model = useModrinthProjectDetailData(
+  projectId,
+  project,
+  computed(() => props.modrinth),
+  mapping,
+)
 const loading = useLoading(isValidatingModrinth, project, projectId)
 const modLoader = computed(() => props.loader)
 
 // Versions
 const { data: versions, isValidating: loadingVersions } = useSWRVModel(
-  getModrinthVersionModel(projectId, undefined, modLoader, computed(() => props.gameVersion ? [props.gameVersion] : undefined)),
-  inject(kSWRVConfig))
-const modVersions = useModrinthProjectDetailVersions(versions, computed(() => props.installed))
+  getModrinthVersionModel(
+    projectId,
+    undefined,
+    modLoader,
+    computed(() => (props.gameVersion ? [props.gameVersion] : undefined)),
+  ),
+  inject(kSWRVConfig),
+)
+const modVersions = useModrinthProjectDetailVersions(
+  versions,
+  computed(() => props.installed),
+)
 
-const selectedVersion = ref(modVersions.value.find(v => v.installed) ?? modVersions.value[0] as ProjectDetailVersion | undefined)
+const selectedVersion = ref(
+  modVersions.value.find((v) => v.installed) ??
+    (modVersions.value[0] as ProjectDetailVersion | undefined),
+)
 provide('selectedVersion', selectedVersion)
 
 const supportedVersions = computed(() => {
@@ -70,55 +99,73 @@ const supportedVersions = computed(() => {
 })
 
 // Dependencies
-const version = computed(() => versions.value?.find(v => v.id === selectedVersion.value?.id))
-const { data: deps, isValidating } = useSWRVModel(getModrinthDependenciesModel(version, modLoader), { revalidateOnFocus: false })
+const version = computed(() => versions.value?.find((v) => v.id === selectedVersion.value?.id))
+const { data: deps, isValidating } = useSWRVModel(
+  getModrinthDependenciesModel(version, modLoader),
+  { revalidateOnFocus: false },
+)
 const { tasks: taskManagerTasks } = injection(kTaskManager)
 const dependencies = computed(() => {
   if (!version.value) return []
   if (!deps.value) return []
 
-  return deps.value.map(({ recommendedVersion, versions, project, type, parent }) => {
-    // TODO: optimize this perf
-    const file = computed(() => {
-      for (const file of props.allFiles) {
-        if (file.modrinth?.versionId === recommendedVersion.id) {
-          return file
+  return (
+    deps.value.map(({ recommendedVersion, versions, project, type, parent }) => {
+      // TODO: optimize this perf
+      const file = computed(() => {
+        for (const file of props.allFiles) {
+          if (file.modrinth?.versionId === recommendedVersion.id) {
+            return file
+          }
         }
-      }
-      return undefined
-    })
-    const otherFile = computed(() => {
-      for (const file of props.allFiles) {
-        if (file.modrinth?.projectId === project.id && file.modrinth?.versionId !== recommendedVersion.id) {
-          return file
+        return undefined
+      })
+      const otherFile = computed(() => {
+        for (const file of props.allFiles) {
+          if (
+            file.modrinth?.projectId === project.id &&
+            file.modrinth?.versionId !== recommendedVersion.id
+          ) {
+            return file
+          }
         }
-      }
-      return undefined
-    })
-    const { progress, total } = useModrinthTask(taskManagerTasks, computed(() => recommendedVersion.id))
-    const dep: ProjectDependency = reactive({
-      id: project.id,
-      icon: project.icon_url,
-      title: project.title,
-      version: recommendedVersion.name,
-      description: recommendedVersion.files[0].filename,
-      type,
-      parent: parent?.title ?? '',
-      installedVersion: computed(() => file.value?.version),
-      installedDifferentVersion: computed(() => otherFile.value?.version),
-      progress: computed(() => total.value === -1 ? -1 : progress.value / total.value),
-    })
-    return dep
-  }) ?? []
+        return undefined
+      })
+      const { progress, total } = useModrinthTask(
+        taskManagerTasks,
+        computed(() => recommendedVersion.id),
+      )
+      const dep: ProjectDependency = reactive({
+        id: project.id,
+        icon: project.icon_url,
+        title: project.title,
+        version: recommendedVersion.name,
+        description: recommendedVersion.files[0].filename,
+        type,
+        parent: parent?.title ?? '',
+        installedVersion: computed(() => file.value?.version),
+        installedDifferentVersion: computed(() => otherFile.value?.version),
+        progress: computed(() => (total.value === -1 ? -1 : progress.value / total.value)),
+      })
+      return dep
+    }) ?? []
+  )
 })
 
 const innerUpdating = useProjectDetailUpdate()
-watch(() => props.modrinth, () => {
-  innerUpdating.value = false
-})
-watch(() => props.installed, () => {
-  innerUpdating.value = false
-}, { deep: true })
+watch(
+  () => props.modrinth,
+  () => {
+    innerUpdating.value = false
+  },
+)
+watch(
+  () => props.installed,
+  () => {
+    innerUpdating.value = false
+  },
+  { deep: true },
+)
 
 // Install
 const installing = ref(false)
@@ -127,14 +174,20 @@ const onInstall = async (v: ProjectDetailVersion) => {
   if (installing.value) return
   try {
     installing.value = true
-    await installWithDependencies(v.id, v.loaders, project.value?.icon_url, props.installed, deps.value ?? [])
+    await installWithDependencies(
+      v.id,
+      v.loaders,
+      project.value?.icon_url,
+      props.installed,
+      deps.value ?? [],
+    )
   } finally {
     installing.value = false
   }
 }
 const onInstallDependency = async (dep: ProjectDependency) => {
   if (installing.value || dep.installedVersion || dep.progress >= 0) return
-  const resolvedDep = deps.value?.find(d => d.project.id === dep.id)
+  const resolvedDep = deps.value?.find((d) => d.project.id === dep.id)
   if (!resolvedDep) return
   const version = resolvedDep.recommendedVersion
   try {
@@ -160,8 +213,8 @@ const { enabled, installed, hasInstalledVersion } = useProjectDetailEnable(
   selectedVersion,
   computed(() => props.installed),
   innerUpdating,
-  f => emit('enable', f),
-  f => emit('disable', f),
+  (f) => emit('enable', f),
+  (f) => emit('disable', f),
 )
 
 const onDelete = () => {
@@ -174,9 +227,13 @@ const onOpenDependency = (dep: ProjectDependency) => {
   push({ query: { ...currentRoute.value.query, id: `modrinth:${dep.id}` } })
 }
 
-const curseforgeId = computed(() => props.curseforge ||
-  props.allFiles.find(v => v.modrinth?.projectId === props.projectId && v.curseforge)?.curseforge?.projectId ||
-  mapping.value?.curseforgeId)
+const curseforgeId = computed(
+  () =>
+    props.curseforge ||
+    props.allFiles.find((v) => v.modrinth?.projectId === props.projectId && v.curseforge)
+      ?.curseforge?.projectId ||
+    mapping.value?.curseforgeId,
+)
 
 const isNotFound = computed(() => error.value?.status === 404)
 const { replace } = useRouter()
@@ -191,21 +248,56 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <Hint v-if="isNotFound" icon="warning" color="red" class="px-10" :size="100" :text="t('errors.NotFoundError')">
+  <Hint
+    v-if="isNotFound"
+    icon="warning"
+    color="red"
+    class="px-10"
+    :size="100"
+    :text="t('errors.NotFoundError')"
+  >
     <div>
-      <v-btn color="primary" v-if="curseforgeId" @click="goCurseforgeProject(curseforgeId)" variant="text">
+      <v-btn
+        color="primary"
+        v-if="curseforgeId"
+        @click="goCurseforgeProject(curseforgeId)"
+        variant="text"
+      >
         <v-icon start>xmcl:curseforge</v-icon>
         Curseforge
       </v-btn>
     </div>
   </Hint>
-  <MarketProjectDetail v-else :detail="model" :has-more="false" :enabled="enabled" :error="error"
-    :supported-versions="supportedVersions" :selected-installed="installed" :has-installed-version="hasInstalledVersion"
-    :versions="modVersions" :updating="innerUpdating || installing || updating" :loading-dependencies="isValidating"
-    :dependencies="dependencies" :loading="loading" :loading-versions="loadingVersions" :modrinth="projectId"
-    :curseforge="curseforgeId" :followed="isFollowed" :following="following" :collection="collectionId"
-    :loading-collections="loadingCollections" @collection="onAddOrRemove" current-target="modrinth"
-    @open-dependency="onOpenDependency" @install="onInstall" @enable="enabled = $event" @delete="onDelete"
-    @install-dependency="onInstallDependency" @select:category="emit('category', $event)" @refresh="refresh()"
-    @follow="onFollow" />
+  <MarketProjectDetail
+    v-else
+    :detail="model"
+    :has-more="false"
+    :enabled="enabled"
+    :error="error"
+    :supported-versions="supportedVersions"
+    :selected-installed="installed"
+    :has-installed-version="hasInstalledVersion"
+    :versions="modVersions"
+    :updating="innerUpdating || installing || updating"
+    :loading-dependencies="isValidating"
+    :dependencies="dependencies"
+    :loading="loading"
+    :loading-versions="loadingVersions"
+    :modrinth="projectId"
+    :curseforge="curseforgeId"
+    :followed="isFollowed"
+    :following="following"
+    :collection="collectionId"
+    :loading-collections="loadingCollections"
+    @collection="onAddOrRemove"
+    current-target="modrinth"
+    @open-dependency="onOpenDependency"
+    @install="onInstall"
+    @enable="enabled = $event"
+    @delete="onDelete"
+    @install-dependency="onInstallDependency"
+    @select:category="emit('category', $event)"
+    @refresh="refresh()"
+    @follow="onFollow"
+  />
 </template>
