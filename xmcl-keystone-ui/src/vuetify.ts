25a6998b1440fcb95106cfa2d7fd10889b8630ce
Areed Elahi <areedelahi@gmail.com>
2026-06-14 06:20:04 +0500

    feat: use native inset style for v-switch to match Material 3 guidelines

--- xmcl-keystone-ui/src/vuetify.ts
+++ xmcl-keystone-ui/src/vuetify.ts
@@ -50,6 +50,12 @@ export const vuetify = createVuetify({
     aliases,
     sets: { md, xmcl },
   },
+  defaults: {
+    VSwitch: {
+      inset: true,
+      color: 'primary',
+    },
+  },
   theme: {
     defaultTheme: 'dark',
     themes: {
