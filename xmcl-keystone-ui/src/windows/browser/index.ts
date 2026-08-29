ae018e9193feeb57740dbdb79f0b87314e0e872c
Shishir-Kc <shishir.khatri.kc@gmail.com>
2026-08-29 17:19:40 +0545

    fix(browser): provide user context dependencies

--- xmcl-keystone-ui/src/windows/browser/index.ts
+++ xmcl-keystone-ui/src/windows/browser/index.ts
@@ -1,12 +1,20 @@
 import { i18n } from '@/i18n'
 import { vuetify } from '@/vuetify'
 import 'virtual:uno.css'
-import { createApp, h } from 'vue'
+import { createApp, h, provide, ref } from 'vue'
+import { useUserContext, kUserContext } from '@/composables/user'
+import { kCompact } from '@/composables/scrollTop'
 import BrowseVue from './Browse.vue'
 import { BaseServiceKey } from '@xmcl/runtime-api'
 
 const app = createApp({
   setup() {
+    const userContext = useUserContext()
+    const compact = ref(false)
+
+    provide(kUserContext, userContext)
+    provide(kCompact, compact)
+
     const baseServiceChannel = serviceChannels.open(BaseServiceKey)
     baseServiceChannel.call('getSettings').then(state => state).then(state => {
       ;(i18n.global.locale as any).value = state.locale
